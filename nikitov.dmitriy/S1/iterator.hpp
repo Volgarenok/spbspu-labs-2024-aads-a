5ee910433824a0a437def8d632c47a4fc454a427
Nikitov Dmitriy <Dmitry5335@yandex.ru>
2024-02-24 15:29:57 +0300

    Primary realisation of iterator

--- nikitov.dmitriy/S1/iterator.hpp
+++ nikitov.dmitriy/S1/iterator.hpp
@@ -1,25 +1,59 @@
 #ifndef ITERATOR_HPP
 #define ITERATIR_HPP
 
+#include <iterator>
 #include "list.hpp"
 
 namespace nikitov
 {
   template< typename T >
+  class List;
+
+  template< typename T >
   class Iterator: public std::iterator< std::input_iterator_tag, T >
   {
-    friend class List;
+    friend class List< T >;
   public:
-    T& Iterator::reference operator*() const;
-    T* Iterator operator->() const;
-    Iterator& operator++();
-    Iterator operator++( int);
-    Iterator& operator--();
-    Iterator operator--(int);
-    bool operator==(const Iterator& other) const;
-    bool operator!=(const Iterator& other) const;
+    T& operator*() const
+    {
+      return node_.value;
+    }
+    T* operator->() const
+    {
+      return &node_.value;
+    }
+    Iterator& operator++()
+    {
+      node_->next_;
+      return *this;
+    }
+    Iterator operator++(int)
+    {
+      Iterator temp(*this);
+      node_->next_;
+      return temp;
+    }
+    Iterator& operator--()
+    {
+      node_->prev_;
+      return *this;
+    }
+    Iterator operator--(int)
+    {
+      Iterator temp(*this);
+      node_->prev_;
+      return temp;
+    }
+    bool operator==(const Iterator& other) const
+    {
+      return node_ == other.node_;
+    }
+    bool operator!=(const Iterator& other) const
+    {
+      return node_ != other.node_;
+    }
   private:
-    List< T >* node;
+    List< T >* node_;
   };
 }
 #endif
