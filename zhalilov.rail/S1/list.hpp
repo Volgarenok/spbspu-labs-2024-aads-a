225383a973308be002ff8ad7605c5c0af8b3e823
Rail <ksovfox@mail.ru>
2024-02-25 23:42:04 +0300

    Added pushing front method

--- zhalilov.rail/S1/list.hpp
+++ zhalilov.rail/S1/list.hpp
@@ -6,15 +6,13 @@
 namespace zhalilov
 {
   template < typename T >
-  struct Node
-  {
+  struct Node {
     T value;
     Node *prev;
     Node *next;
   };
 
-  class List
-  {
+  class List {
   public:
     List():
       m_size(0),
@@ -24,9 +22,10 @@ namespace zhalilov
 
     void push_back(T &&value)
     {
+      Node < T > *newTail = nullptr;
       try
       {
-        Node< T > *newTail = new < T > Node;
+        newTail = new Node < T >;
         newTail->value = value;
         newTail->prev = m_tail;
         newTail->next = nullptr;
@@ -42,12 +41,27 @@ namespace zhalilov
 
     void push_front(T &&value)
     {
-
+      Node < T > *newTail = nullptr;
+      try
+      {
+        newTail = new Node < T >;
+        newTail->value = value;
+        newTail->prev = nullptr;
+        newTail->next = m_head;
+        m_head->prev = newTail;
+        m_head = newTail;
+      }
+      catch (...)
+      {
+        delete newTail;
+        throw;
+      }
     }
+
   private:
     size_t m_size;
-    Node < T > *m_head{};
-    Node < T > *m_tail{};
+    Node < T > *m_head {};
+    Node < T > *m_tail {};
   };
 }
 #endif
