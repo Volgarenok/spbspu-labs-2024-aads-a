75e8a5dc38f766c834b2d8532ec7572688d40171
Dmitriy <Dmitry5335@yandex.ru>
2024-02-26 19:06:40 +0300

    Temp try/catch

--- nikitov.dmitriy/S1/main.cpp
+++ nikitov.dmitriy/S1/main.cpp
@@ -8,7 +8,14 @@
 int main()
 {
   using namespace nikitov;
-  List< std::pair< std::string, List< int >* >* > pairsList = inputList(std::cin);
-  outputList(pairsList, std::cout);
+  try
+  {
+    List< std::pair< std::string, List< int >* >* > pairsList = inputList(std::cin);
+    outputList(pairsList, std::cout);
+  }
+  catch(...)
+  {
+    return 1;
+  }
   return 0;
 }
