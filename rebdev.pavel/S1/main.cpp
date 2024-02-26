99c26323f38c7c4b8bd0d66957b142f21449dc59
User1235321 <pavreb1235321@gmail.com>
2024-02-26 13:07:20 +0300

    make template of end main file

--- rebdev.pavel/S1/main.cpp
+++ rebdev.pavel/S1/main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <utility>
+#include <string>
+
+#include "biList.hpp"
+#include "pairsFunction.hpp"
 
 int main()
 {
+  using pairs = std::pair < std::string, rebdev::BiList< size_t > >;
+  pairs * pairsArr = nullptr;
+  size_t numOfPairs = 0;
+  size_t maxNumber = 0;
 
+  while (!std::cout.eof())
+  {
+    std::string listName;
+    rebdev::BiList < size_t > numList;
+
+    try
+    {
+      rebdev::expandPairArr(pairsArr, numOfPairs);
+      std::getline(std::cin, listName, ' ');
+      size_t num = rebdev::inputNumbersList(std::cin, numList);
+      maxNumber = std::max(maxNumber, num);
+      pairsArr[numOfPairs++] = std::make_pair(listName, numList);
+    }
+    catch (const std::exception & e)
+    {
+      delete[] pairsArr;
+      std::cerr << e.what();
+      return 1;
+    }
+  }
+
+  for (size_t i = 0; i < numOfPairs; ++i)
+  {
+    std::cout << std::get < 0 > (pairsArr[i]) << ' ';
+  }
+  std::cout << '\n';
+  size_t sumOfNum[maxNumber] = {};
+  for (size_t i = 0; i < maxNumber; ++i)
+  {
+    for (size_t j = 0; j < numOfPairs; ++i)
+    {
+      try
+      {
+        size_t element = std::get < 1 > (pairsArr[j]).element(i);
+        std::cout << element << ' ';
+        sumOfNum[i] += element;
+      }
+      catch (...){}
+    }
+    std::cout << '\n';
+  }
+  for (size_t i = 0; i < maxNumber; ++i)
+  {
+    std::cout << sumOfNum[i] << ' ';
+  }
+
+  delete[] pairsArr;
   return 0;
 }
