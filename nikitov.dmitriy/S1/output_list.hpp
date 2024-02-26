daed4b5e06e52ec1f05233382ccaa439f1551930
Dmitriy <Dmitry5335@yandex.ru>
2024-02-26 15:06:26 +0300

    List output

---
+++ nikitov.dmitriy/S1/output_list.hpp
@@ -0,0 +1,11 @@
+#ifndef OUTPUT_LIST_HPP
+#define OUTPUT_LIST_HPP
+
+#include <fstream>
+#include "list.hpp"
+
+namespace nikitov
+{
+  void outputList(List< std::pair< std::string, List< int >* >* >* pairsList, std::ostream& output);
+}
+#endif
