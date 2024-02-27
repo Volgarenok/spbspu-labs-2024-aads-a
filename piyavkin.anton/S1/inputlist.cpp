13ffce85ecd8fb7496c60f7a119091f10fb16450
Piyavva <piyavkin05@bk.ru>
2024-02-27 14:30:49 +0300

    Replaced input(removed <string>)

--- piyavkin.anton/S1/inputlist.cpp
+++ piyavkin.anton/S1/inputlist.cpp
@@ -1,14 +1,13 @@
 #include "inputlist.hpp"
-#include <iostream>
-#include <cstring>
 #include <cmath>
 
 std::pair< std::string, piyavkin::List< unsigned int >* >* piyavkin::inputList(std::istream& in, size_t& size, size_t& max_size_list)
 {
   size_t max_size = 0;
-  std::string str = "";
   std::string name = "";
   size_t capacity = 10;
+  char symbol = 0;
+  unsigned int val = 0;
   std::pair< std::string, List< unsigned int >* >* pairs = new std::pair< std::string, List< unsigned int >* >[capacity] {};
   while (in >> name)
   {
@@ -25,46 +24,27 @@ std::pair< std::string, piyavkin::List< unsigned int >* >* piyavkin::inputList(s
     }
     List< unsigned int >* list = new List< unsigned int >;
     pairs[size].first = name;
-    std::getline(in, str);
-    str.erase(0, 1);
-    while (str != "")
+    in >> std::noskipws;
+    in >> symbol;
+    while (symbol != '\n')
     {
-      if (strchr(str.c_str(), ' ') != nullptr)
+      if (symbol == ' ')
       {
-        std::string str2 = str.substr(0, str.find(' '));
-        unsigned int val = 0;
-        try
-        {
-          val = std::stoi(str2);
-        }
-        catch (const std::out_of_range&)
+        in >> std::skipws;
+        in >> val;
+        if (!in)
         {
           delete list;
           delete[] pairs;
-          throw;
+          throw std::out_of_range("This number is too huge");
         }
+        in >> std::noskipws;
         list->push_back(val);
         ++max_size;
-        str.erase(0, str.find(' ') + 1);
       }
-      else
-      {
-        unsigned int val = 0;
-        try
-        {
-          val = std::stoi(str);
-        }
-        catch (const std::out_of_range&)
-        {
-          delete list;
-          delete[] pairs;
-          throw;
-        }
-        list->push_back(val);
-        ++max_size;
-        str = "";
-      }
+      in >> symbol;
     }
+    in >> std::skipws;
     pairs[size++].second = list;
     max_size_list = std::max(max_size, max_size_list);
     max_size = 0;
