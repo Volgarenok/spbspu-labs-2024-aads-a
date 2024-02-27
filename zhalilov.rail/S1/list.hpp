9bfbc5b4def66b7010534e420fe1cfccad317124
Rail <ksovfox@mail.ru>
2024-02-27 10:14:17 +0300

    Node вынесена в отдельный файл, изменен дизайн List

--- zhalilov.rail/S1/list.hpp
+++ zhalilov.rail/S1/list.hpp
@@ -2,66 +2,90 @@
 #define LIST_HPP
 
 #include <cstddef>
+#include <utility>
+
+#include "node.hpp"
 
 namespace zhalilov
 {
   template < typename T >
-  struct Node {
-    T value;
-    Node *prev;
-    Node *next;
-  };
-
   class List {
   public:
-    List():
-      m_size(0),
-      m_head(nullptr),
-      m_tail(nullptr)
-    {}
+    List();
+    ~List();
 
-    void push_back(T &&value)
-    {
-      Node < T > *newTail = nullptr;
-      try
-      {
-        newTail = new Node < T >;
-        newTail->value = value;
-        newTail->prev = m_tail;
-        newTail->next = nullptr;
-        m_tail->next = newTail;
-        m_tail = newTail;
-      }
-      catch (...)
-      {
-        delete newTail;
-        throw;
-      }
-    }
-
-    void push_front(T &&value)
-    {
-      Node < T > *newTail = nullptr;
-      try
-      {
-        newTail = new Node < T >;
-        newTail->value = value;
-        newTail->prev = nullptr;
-        newTail->next = m_head;
-        m_head->prev = newTail;
-        m_head = newTail;
-      }
-      catch (...)
-      {
-        delete newTail;
-        throw;
-      }
-    }
+    void push_back(T &&value);
+    void push_front(T &&value);
+    void clear();
 
   private:
     size_t m_size;
-    Node < T > *m_head {};
-    Node < T > *m_tail {};
+    Node < T > *m_head;
+    Node < T > *m_tail;
   };
+
+  template < typename T >
+  List < T >::List():
+    m_size(0),
+    m_head(nullptr),
+    m_tail(nullptr)
+  {}
+
+  template < typename T >
+  List < T >::~List()
+  {
+    clear();
+  }
+
+  template < typename T >
+  void List < T >::push_back(T &&value)
+  {
+    Node < T > *newTail = new Node < T >(std::forward < T >(value), m_tail, nullptr);
+    if (m_size > 0)
+    {
+      m_tail->next = newTail;
+      m_tail = newTail;
+    }
+    else
+    {
+      m_head = newTail;
+      m_tail = newTail;
+    }
+    m_size++;
+  }
+
+  template < typename T >
+  void List < T >::push_front(T &&value)
+  {
+    Node < T > *newHead = new Node < T >(std::forward < T >(value), m_head, nullptr);
+    if (m_size > 0)
+    {
+      m_head->prev = newHead;
+      m_head = newHead;
+    }
+    else
+    {
+      m_head = newHead;
+      m_tail = newHead;
+    }
+    m_size++;
+  }
+
+  template < typename T >
+  void List < T >::clear()
+  {
+    Node < T > *currNode = m_head;
+    Node < T > *nextNode = nullptr;
+    while (currNode)
+    {
+      nextNode = currNode->next;
+      delete currNode;
+      currNode = nextNode;
+    }
+    m_size = 0;
+    m_head = nullptr;
+    m_tail = nullptr;
+  }
 }
+
 #endif
---
+++ zhalilov.rail/S1/node.hpp
@@ -0,0 +1,26 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+namespace zhalilov
+{
+  template < typename T >
+  struct Node {
+    T value;
+    Node *prev;
+    Node *next;
+
+    Node():
+      value(T()),
+      prev(nullptr),
+      next(nullptr)
+    {}
+
+    Node(T &&newValue, Node *newPrev, Node *newNext):
+      value(newValue),
+      prev(newPrev),
+      next(newNext)
+    {}
+  };
+}
+
+#endif
