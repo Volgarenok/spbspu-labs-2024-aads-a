77e4e034026a7ac1ae05c8e4f520b3ee516f2ba1
Rail <ksovfox@mail.ru>
2024-02-27 17:17:59 +0300

    Добавлены методы pop

--- zhalilov.rail/S1/list.hpp
+++ zhalilov.rail/S1/list.hpp
@@ -4,43 +4,52 @@
 #include <cstddef>
 #include <utility>
 
+#include "iterator.hpp"
 #include "node.hpp"
 
 namespace zhalilov
 {
   template < typename T >
-  class List {
+  class List
+  {
   public:
+    using iterator = Iterator< T >;
     List();
     ~List();
 
-    void push_back(T &&value);
-    void push_front(T &&value);
+    void push_back(T &&);
+    void push_front(T &&);
+
+    void pop_back();
+    void pop_front();
     void clear();
+
+    iterator begin();
+    iterator end();
 
   private:
     size_t m_size;
-    Node < T > *m_head;
-    Node < T > *m_tail;
+    Node< T > *m_head;
+    Node< T > *m_tail;
   };
 
   template < typename T >
-  List < T >::List():
+  List< T >::List():
     m_size(0),
     m_head(nullptr),
     m_tail(nullptr)
   {}
 
   template < typename T >
-  List < T >::~List()
+  List< T >::~List()
   {
     clear();
   }
 
   template < typename T >
-  void List < T >::push_back(T &&value)
+  void List< T >::push_back(T &&value)
   {
-    Node < T > *newTail = new Node < T >(std::forward < T >(value), m_tail, nullptr);
+    Node< T > *newTail = new Node< T >(std::forward< T >(value), m_tail, nullptr);
     if (m_size > 0)
     {
       m_tail->next = newTail;
@@ -55,9 +64,9 @@ namespace zhalilov
   }
 
   template < typename T >
-  void List < T >::push_front(T &&value)
+  void List< T >::push_front(T &&value)
   {
-    Node < T > *newHead = new Node < T >(std::forward < T >(value), m_head, nullptr);
+    Node< T > *newHead = new Node< T >(std::forward< T >(value), m_head, nullptr);
     if (m_size > 0)
     {
       m_head->prev = newHead;
@@ -72,10 +81,26 @@ namespace zhalilov
   }
 
   template < typename T >
-  void List < T >::clear()
+  void List< T >::pop_back()
   {
-    Node < T > *currNode = m_head;
-    Node < T > *nextNode = nullptr;
+    Node< T > prev = m_tail->prev;
+    delete m_tail;
+    m_tail = prev;
+  }
+
+  template < typename T >
+  void List<T>::pop_front()
+  {
+    Node< T > next = m_head->next;
+    delete m_head;
+    m_head = next;
+  }
+
+  template < typename T >
+  void List< T >::clear()
+  {
+    Node< T > *currNode = m_head;
+    Node< T > *nextNode = nullptr;
     while (currNode)
     {
       nextNode = currNode->next;
@@ -85,6 +110,18 @@ namespace zhalilov
     m_size = 0;
     m_head = nullptr;
     m_tail = nullptr;
+  }
+
+  template < typename T >
+  typename List< T >::iterator List< T >::begin()
+  {
+    return iterator(m_head);
+  }
+
+  template < typename T >
+  typename List< T >::iterator List< T >::end()
+  {
+    return iterator(nullptr);
   }
 }
 
