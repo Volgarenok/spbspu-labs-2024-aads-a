fa785097de3b156c371a39129cde25ee9763e15c
Slivka <roman.strelyaeff@gmail.com>
2024-02-29 19:56:01 +0300

    list testing

---
+++ strelyaev.roman/S1/list.hpp
@@ -0,0 +1,32 @@
+#ifndef LIST_HPP
+#define LIST_HPP
+
+namespace strelyaev
+{
+  template< typename T >
+  struct Node
+  {
+    Node(T value):
+      value_(value),
+      next_(nullptr),
+      prev_(nullptr)
+    {}
+    ~Node() = default;
+    T value_;
+    Node* next_;
+    Node* prev_;
+  };
+
+  template< typename T >
+  class List
+  {
+    public:
+    List(Node< T >* head, Node< T >* tail):
+      head_(head),
+      tail_(tail)
+    {}
+     Node< T >* head_;
+     Node< T >* tail_;
+  };
+}
+#endif
--- strelyaev.roman/S1/main.cpp
+++ strelyaev.roman/S1/main.cpp
@@ -1,4 +1,44 @@
+#include "list.hpp"
+#include <iostream>
+
 int main()
 {
-
+  strelyaev::Node< int >* head = nullptr;
+  head = new strelyaev::Node< int >{0};
+  strelyaev::Node< int >* tail = head;
+  strelyaev::List< int >* test_list = new strelyaev::List< int >{head, tail};
+  for (int i = 1; i < 10; i++)
+  {
+    strelyaev::Node< int >* temp_node = new strelyaev::Node< int >{i};
+    test_list->tail_->next_ = temp_node;
+    temp_node->prev_ = tail;
+    test_list->tail_ = temp_node;
+  }
+  int i  =0;
+  while (test_list->head_->next_)
+  {
+    std::cout << "Node " << i << ":\n"
+              << "Value: " << test_list->head_->value_ << "\n"
+              << "Have previous?: ";
+    if (test_list->head_->prev_)
+    {
+      std::cout << "YES\n";
+    }
+    else
+    {
+      std::cout << "NO\n";
+    }
+    std::cout << "Have next?: ";
+    if (test_list->head_->next_)
+    {
+      std::cout << "YES\n";
+    }
+    else
+    {
+      std::cout << "NO\n";
+    }
+    i++;
+    test_list->head_ = test_list->head_->next_;
+    std::cout << "=====================================================================\n";
+  }
 }
