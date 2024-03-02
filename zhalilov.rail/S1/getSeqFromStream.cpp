7d608f47fd151b0c0dc9b25bee4eb0a225a92fca
Rail <ksovfox@mail.ru>
2024-03-02 19:15:43 +0300

    Исправлено условие выполнения цикла

--- zhalilov.rail/S1/getSeqFromStream.cpp
+++ zhalilov.rail/S1/getSeqFromStream.cpp
@@ -1,12 +1,12 @@
 #include "getSeqFromStream.hpp"
 
-#include <istream>
+#include <iostream>
 
 void zhalilov::getSeqFromStream(List< pair > &sequences, std::istream &input)
 {
   std::string seqName = "";
   size_t currNum = 0;
-  while (!input.eof())
+  while (input)
   {
     List< size_t > numbers;
     input >> seqName;
@@ -14,8 +14,11 @@ void zhalilov::getSeqFromStream(List< pair > &sequences, std::istream &input)
     {
       numbers.push_back(currNum);
     }
+    if (!input.eof())
+    {
+      input.clear();
+    }
     auto pair = std::make_pair(seqName, numbers);
     sequences.push_back(pair);
   }
-  input.clear();
 }
