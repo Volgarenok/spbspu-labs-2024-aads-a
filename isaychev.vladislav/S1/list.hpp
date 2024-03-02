3fb672151b491d897a57376002040df42f807b0a
Z0ltaan <gregnov69@gmail.com>
2024-03-02 21:50:41 +0300

    deleted list cpp file

--- isaychev.vladislav/S1/list.hpp
+++ isaychev.vladislav/S1/list.hpp
@@ -1,15 +1,10 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include "iterator.hpp"
+
 namespace isaychev
 {
-  template < typename T >
-  struct node_t
-  {
-    T data;
-    node_t< T > * next;
-  };
-
   template < typename T >
   class List
   {
@@ -17,9 +12,10 @@ namespace isaychev
     List();
     ~List();
 
-    //iterator
-    //constIterator
-
+    fwdIterator< T > begin();
+    fwdIterator< T > end();
+    //cbegin
+    //cend
     T & front();
     const T & front() const;
     bool empty();
@@ -31,6 +27,82 @@ namespace isaychev
    private:
     node_t< T > * head_;
   };
+
+  template < typename T >
+  List< T >::List():
+    head_(nullptr)
+  {}
+
+  template < typename T >
+  List< T >::~List()
+  {
+    clear();
+  }
+
+  template < typename T >
+  T & List< T >::front()
+  {
+    return head_->data;
+  }
+
+  template < typename T >
+  const T & List< T >::front() const
+  {
+    return head_->data;
+  }
+
+  template < typename T >
+  bool List< T >::empty()
+  {
+    return (head_ == nullptr);
+  }
+
+  template < typename T >
+  void List< T >::push(const T & obj)
+  {
+    if (empty())
+    {
+      head_ = new node_t< T >{obj, nullptr};
+    }
+    else
+    {
+      node_t< T > * temp = head_->next;
+      while (temp != nullptr)
+      {
+        temp = temp->next;
+      }
+      temp = new node_t< T >{obj, nullptr};
+    }
+  }
+
+  template < typename T >
+  void List< T >::pop()
+  {
+    if (!empty())
+    {
+      node_t< T > * temp = head_->next;
+      delete head_;
+      head_ = temp;
+    }
+  }
+
+  template < typename T >
+  void List< T >::clear()
+  {
+    while (!empty())
+    {
+      pop();
+    }
+  }
+
+  template < typename T >
+  void List< T >::swap(List< T > & rhs)
+  {
+    /*node_t< T > * tempHead = head_;
+    head_ = rhs.head_;
+    rhs.head_ = tempHead;*/
+    std::swap(head_, rhs.head);
+  }
 }
 
 #endif
