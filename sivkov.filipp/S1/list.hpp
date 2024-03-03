c77538047cc434b3a255f9feec8bca01d647aa6c
exxiiint <filipp.sivkov@yandex.ru>
2024-03-03 16:57:57 +0300

    edit list

--- sivkov.filipp/S1/list.hpp
+++ sivkov.filipp/S1/list.hpp
@@ -1,33 +1,91 @@
-#ifndef LIST_HPP
-#define LIST_HPP
+template< class T >
+class Node
+{
+public:
+  T data_;
+  Node* next_;
+  Node(const T& value, Node* const next);
+  Node(T&& value, Node* const next);
+  ~Node() = default;
+};
 
-template<typename T>
+template< class T >
+Node< T >::Node(const T& value, Node* const next) :
+  data_(value),
+  next_(next)
+{}
+
+template< class T >
+Node< T >::Node(T&& value, Node* const next) :
+  data_(value),
+  next_(next)
+{}
+
+
+template <class T>
 class List
 {
 public:
-  List();
-  ~List();
+	List();
+	~List();
 
-  void push_back(T data);
-  void delete_front();
-  int getSize() { return size; }
-  T& operator[](const int index);
+  bool empty() const;
+  void push_front(const T& value);
+  void pop_front();
+  void del();
+  void swap(List< T >& list);
+
 
 private:
-  class Node
-  {
-  public:
-    Node* pNext;
-    T data;
-
-    Node(T data = T(), Node* pNext = nullptr)
-    {
-      this->data = data;
-      this->pNext = pNext;
-    }
-  };
-  int size;
-  Node* head;
+  Node< T >* head_;
 };
 
-#endif
+
+
+template<class T>
+List<T>::List()
+{
+  head_(nullptr);
+}
+
+template<class T>
+List<T>::~List()
+{
+  del();
+}
+
+template<class T>
+bool List<T>::empty() const
+{
+  return !head_;
+}
+
+template<class T>
+void List<T>::push_front(const T& value)
+{
+  Node< T >* data = new Node< T >(value, head_);
+  head_ = data;
+}
+
+template<class T>
+void List<T>::pop_front()
+{
+  Node< T >* new_head = head_->next_;
+  delete head_;
+  head_ = new_head;
+}
+
+template<class T>
+void List<T>::del()
+{
+  while (head_)
+  {
+    pop_front();
+  }
+}
+
+template<class T>
+void List<T>::swap(List<T>& list)
+{
+  std::swap(head_, list.head_);
+}
