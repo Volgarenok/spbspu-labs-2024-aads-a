00ceecdab884107dd724d3676129115923169102
Rail <ksovfox@mail.ru>
2024-03-03 22:01:47 +0300

    Добавлена реализация всей программы в совокупности в main.cpp

--- zhalilov.rail/S1/main.cpp
+++ zhalilov.rail/S1/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <fstream>
 
 #include "list.hpp"
 #include "getSeqFromStream.hpp"
+#include "outputSeqs.hpp"
+#include "countSum.hpp"
 
 int main()
 {
   using namespace zhalilov;
   List< pair > sequences;
-  getSeqFromStream(sequences, std::cin);
+  std::ifstream file("test");
+  getSeqFromStream(sequences, file);
+  if (sequences.empty())
+  {
+    return 0;
+  }
+
+  try
+  {
+    outputSeqs(sequences, std::cout);
+  }
+  catch (const std::overflow_error &e)
+  {
+    std::cerr << "Error has occured: " << e.what();
+    return 1;
+  }
 }
