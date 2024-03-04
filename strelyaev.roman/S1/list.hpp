567b016354b446f1600007509eca38ff19a22519
Slivka <roman.strelyaeff@gmail.com>
2024-03-04 17:22:10 +0300

    fix

--- strelyaev.roman/S1/list.hpp
+++ strelyaev.roman/S1/list.hpp
@@ -61,6 +61,82 @@ namespace strelyaev
         tail_ = new_node;
       }
 
+      void push_front(const T& val)
+      {
+        Node< T >* new_node = new Node< T >(val);
+        new_node->next_ = head_;
+        if (head_)
+        {
+          head_->prev_ = new_node;
+        }
+        if (!tail_)
+        {
+          tail_ = new_node;
+        }
+        head_ = new_node;
+      }
+
+      void pop_front()
+      {
+        Node< T >* temp = head_->next_;
+        if (!temp)
+        {
+          delete head_;
+          head_ = nullptr;
+          return;
+        }
+        delete head_;
+        head_ = temp;
+      }
+
+      void pop_back()
+      {
+        Node< T >* temp = tail_->prev_;
+        if (!temp)
+        {
+          delete tail_;
+          tail_ = nullptr;
+          return;
+        }
+        delete tail_;
+        tail_ = temp;
+      }
+
+      T& back() const
+      {
+        if (tail_)
+        {
+          return tail_->value_;
+        }
+      }
+
+      T& front() const
+      {
+        if (head_)
+        {
+          return head_->value_;
+        }
+      }
+
+      void clear()
+      {
+        while (head_ != nullptr)
+        {
+          pop_front();
+        }
+      }
+
+      Iterator< T > begin()
+      {
+        return Iterator< T >(head_);
+      }
+
+      Iterator< T > end()
+      {
+        return Iterator< T >(nullptr);
+      }
+
+      
     private:
       Node< T >* head_;
       Node< T >* tail_;
