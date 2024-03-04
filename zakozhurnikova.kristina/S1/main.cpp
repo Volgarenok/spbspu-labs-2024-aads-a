94ee7fa0bd47a941e627b91d0223960718d3e0b7
kastiela <kristinazak150@gmail.com>
2024-03-04 23:27:29 +0300

    realization main

--- zakozhurnikova.kristina/S1/main.cpp
+++ zakozhurnikova.kristina/S1/main.cpp
@@ -1,2 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <limits>
+#include "List.hpp"
+#include "inputOperations.hpp"
+#include "print.hpp"
+
 int main()
-{}
+{
+  try {
+    List<pair> list;
+    readList<size_t>(list, std::cin);
+    printNames(list);
+    std::cout << '\n';
+    printByIndex(list);
+    printSums<size_t>(list);
+    std::cout << '\n';
+  }
+  catch (const std::range_error& e)
+  {
+  }
+  catch (const std::out_of_range& e)
+  {
+  }
+  return 0;
+}
