bc093e170f65e080b0ecea505d71c84e2953497c
kastiela <kristinazak150@gmail.com>
2024-03-04 19:58:46 +0300

    realization maxSize

---
+++ zakozhurnikova.kristina/S1/print.hpp
@@ -0,0 +1,21 @@
+#ifndef PRINT_HPP
+#define PRINT_HPP
+#include <iostream>
+#include "list.hpp"
+
+namespace zakozhurnikova
+{
+  using pair = std::pair<std::string, List<size_t>>;
+
+  template <typename T>
+  size_t getMaxSize(const List<pair>& l)
+  {
+    T max = 0;
+    for (pair p : l)
+    {
+      size_t size = p.second.size();
+      max = std::max(max, size);
+    }
+    return max;
+  }
+}
