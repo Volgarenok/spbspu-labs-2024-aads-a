ede7b6130e80be95507c93bd5b17392d98f6dc91
Slivka <roman.strelyaeff@gmail.com>
2024-03-04 15:17:45 +0300

    input working version

--- strelyaev.roman/S1/main.cpp
+++ strelyaev.roman/S1/main.cpp
@@ -6,35 +6,8 @@
 int main()
 {
   using namespace strelyaev;
-  List< int > a1;
-  a1.push_back(1);
-  a1.push_back(1);
-  a1.push_back(1);
-
-  List< int > a2;
-  a2.push_back(2);
-  a2.push_back(2);
-  a2.push_back(2);
-  a2.push_back(2);
-
-  List< int > a3;
-
-  List< int > a4;
-  a4.push_back(4);
-  a4.push_back(4);
-
-  auto b1 = std::make_pair("First", a1);
-  auto b2 = std::make_pair("Second", a2);
-  auto b3 = std::make_pair("Third", a3);
-  auto b4 = std::make_pair("Fourth", a4);
-
   List< std::pair< std::string, List < int > > > l;
-  l.push_back(b1);
-  l.push_back(b2);
-  l.push_back(b3);
-  l.push_back(b4);
-
-
+  makeSequence(std::cin, l);
   for (auto it = l.begin(); it != l.end(); it++)
   {
     std::cout << it->first << " ";
