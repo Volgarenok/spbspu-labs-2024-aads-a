37eb97d0cb0a88e4791eeb440bb3630c50202833
WarriorVortex <erohinvova01010111@gmail.com>
2024-03-06 00:51:39 +0300

    Normalized input of lists

--- erohin.vladimir/S1/main.cpp
+++ erohin.vladimir/S1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <utility>
 #include <string>
+#include <cctype>
 #include <stdexcept>
 #include "list.hpp"
 
@@ -15,12 +16,23 @@ int main()
     named_list & current_line = lines.front();
     std::cin.clear();
     std::cin >> current_line.first;
+    std::string string_number;
     int elem = 0;
-    while (std::cin >> elem)
+    while (std::cin.peek() != '\n' && !std::cin.eof())
     {
+      std::cin >> string_number;
       try
       {
+        elem = stoi(string_number);
         current_line.second.push_front(elem);
+      }
+      catch (const std::invalid_argument &)
+      {
+        std::cerr << "Input is not a number\n";
+      }
+      catch (const std::out_of_range &)
+      {
+        std::cerr << "Number is too big\n";
       }
       catch (const std::bad_alloc &)
       {
