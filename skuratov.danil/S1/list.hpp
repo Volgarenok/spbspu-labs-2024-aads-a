0791e57a581bcd2460ed07229bd82c4b5e622b5f
Skuratov09 <skyratovdaniil28@gmail.com>
2024-03-06 00:51:45 +0300

    Decided to start over

--- skuratov.danil/S1/list.hpp
+++ skuratov.danil/S1/list.hpp
@@ -1,47 +1,17 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
-#include <iostream>
-#include <vector>
-#include <cstddef>
-#include "node.hpp"
-
 namespace skuratov
 {
   template <typename T>
   class List
   {
   public:
-    List();
-    ~List();
-
-    using iterator = Node<T>*;
-    using const_iterator = const Node<T>*;
-
-    iterator begin();
-    iterator end();
-    const_iterator begin() const;
-    const_iterator end() const;
-
-    T& operator[](std::size_t index);
-    const T& operator[](std::size_t index) const;
-
-    bool empty() const;
-
-    void push_back(const T& value);
-
-    void clear();
-
-    std::size_t size() const;
-
-    void pop_back();
-    void assign(iterator first, iterator last);
-    void swap(List<T>& other);
-
-  private:
-    Node<T>* head_;
-    Node<T>* tail_;
     size_t size_;
+	Node< T >* head_;
+	Node< T >* tail_;
+  private:
+
   };
 }
 
--- skuratov.danil/S1/node.hpp
+++ skuratov.danil/S1/node.hpp
@@ -7,25 +7,16 @@ namespace skuratov
   class Node
   {
   public:
-    Node();
-    ~Node();
-
-    T& value();
-    const T& value() const;
-    Node<T>* next();
-    const Node<T>* next() const;
-    Node<T>* prev();
-    const Node<T>* prev() const;
-
-    void set_value(const T& value);
-    void set_next(Node<T>* next);
-    void set_prev(Node<T>* prev);
-
-  private:
-    Node<T>* prev_;
-    Node<T>* next_;
+    int data{};
+    Node<T> prev_;
+    Node<T> next_;
     T value_;
+  private:
+    Node(int data)
+    {
+      this->data = data;
+      this->prev_ = this->next_ = NULL;
+    }
   };
 }
-
 #endif
