db3fd7138ed1eddbb96f2e23b09ce37ea9db57ed
kastiela <kristinazak150@gmail.com>
2024-03-05 17:12:10 +0300

    probel in numbers

--- zakozhurnikova.kristina/S1/print.cpp
+++ zakozhurnikova.kristina/S1/print.cpp
@@ -18,17 +18,13 @@ void zakozhurnikova::printByIndex(const List<pair>& l)
   size_t maxSize = getMaxSize<size_t>(l);
   for (size_t i = 0; i < maxSize; ++i)
   {
-    for( auto it = l.begin(); it != l.end(); ++it)
+    for( auto it = l.cbegin(); it != l.cend(); ++it)
     {
       if (i >= it->second.size())
       {
         continue;
       }
-      std::cout << *(it->second.cbegin() + i);
-      if (it + 1 != l.end())
-      {
-        std::cout << ' ';
-      }
+      std::cout << *(it->second.cbegin() + i) << (it + 1 == l.cend() ? "" : " ");
     }
     std::cout << '\n';
   }
