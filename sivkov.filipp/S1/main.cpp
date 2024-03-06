7f907a3ffb4a12e2f09230b1a8cdec65f278015c
exxiiint <filipp.sivkov@yandex.ru>
2024-03-06 21:44:13 +0300

    edit main

--- sivkov.filipp/S1/main.cpp
+++ sivkov.filipp/S1/main.cpp
@@ -1,71 +1,31 @@
 #include <limits>
 #include "inputOutput.hpp"
 #include "list.hpp"
+#include "outputNums.hpp"
+
 int main()
 {
   List<std::pair<std::string, List<size_t>>> list;
   input(std::cin, list);
   list.reverse();
-  outputNames(list);
-
-  bool allData = true;
-  bool flag = false;
-  size_t sum = 0;
-  size_t counter = 0;
-  List<size_t> numbers;
-  while (allData)
+  List<size_t> sums;
+  try
   {
-    allData = false;
-    for (auto iteratorForPair = list.cbegin(); iteratorForPair != list.cend(); ++iteratorForPair)
-    {
-      auto iteratorForNums = iteratorForPair->second.cbegin();
-      for (size_t i = 0; i < counter; ++i)
-      {
-        if (iteratorForNums != iteratorForPair->second.cend())
-        {
-          ++iteratorForNums;
-        }
-      }
-      if (iteratorForNums != iteratorForPair->second.cend())
-      {
-        if (allData)
-        {
-          std::cout << " ";
-        }
-        if (std::numeric_limits<size_t>::max() - sum >= *iteratorForNums)
-        {
-          sum += *iteratorForNums;
-        }
-        else
-        {
-          flag = true;
-
-        }
-        std::cout << *iteratorForNums;
-        allData = true;
-      }
-    }
-    if (allData)
-    {
-      std::cout << "\n";
-      numbers.push_front(sum);
-      sum = 0;
-    }
-    ++counter;
+    outputNames(list);
+    addnum(list, sums);
+    sums.reverse();
+    outputSums(sums);
   }
-
-  if (flag == true)
+  catch (std::invalid_argument& e)
   {
-    std::cerr << "OverFlow\n";
+    std::cout << 0;
+    return 0;
+  }
+  catch (std::out_of_range& e)
+  {
+    std::cerr << "OverFlow";
     return 1;
   }
 
-  if (numbers.empty())
-  {
-    std::cout << 0 << "\n";
-    return 0;
-  }
-  numbers.reverse();
-  outputSums(numbers);
   return 0;
 }
