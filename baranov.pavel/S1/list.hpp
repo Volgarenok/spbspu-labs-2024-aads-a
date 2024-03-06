c4cc960da9499dff2ada1f66efc3bead271644b5
pashko2303 <baranov-p.b@yandex.ru>
2024-03-06 20:36:43 +0300

    begin and end

--- baranov.pavel/S1/list.hpp
+++ baranov.pavel/S1/list.hpp
@@ -2,6 +2,7 @@
 #define LIST_HPP
 
 #include "node.hpp"
+#include "iterator.hpp"
 
 namespace baranov
 {
@@ -10,17 +11,32 @@ namespace baranov
   {
     public:
       List();
+      Iterator< T > begin();
+      Iterator< T > end();
     private:
       Node< T > * head_;
       Node< T > * tail_;
   };
 
 
-  template < class T >
+  template< class T >
   List< T >::List():
     head_(nullptr),
     tail_(nullptr)
   {}
+
+  template< class T >
+  Iterator< T > List< T >::begin()
+  {
+    return Iterator< T >(head_);
+  }
+
+  template< class T >
+  Iterator< T > List< T >::end()
+  {
+    return Iterator< T >(nullptr);
+  }
+
 }
 
 #endif
