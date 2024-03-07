a2234a364dc73b191791359d7d3351b04d272575
pashko2303 <baranov-p.b@yandex.ru>
2024-03-07 07:23:30 +0300

    Empty and push methods

--- baranov.pavel/S1/list.hpp
+++ baranov.pavel/S1/list.hpp
@@ -13,6 +13,8 @@ namespace baranov
       List();
       Iterator< T > begin();
       Iterator< T > end();
+      bool empty() const;
+      void push(T data);
     private:
       Node< T > * head_;
       Node< T > * tail_;
@@ -35,6 +37,28 @@ namespace baranov
   Iterator< T > List< T >::end()
   {
     return Iterator< T >(nullptr);
+  }
+
+  template< class T >
+  bool List< T >::empty() const
+  {
+    return head_ == nullptr || tail_ == nullptr;
+  }
+
+  template< class T >
+  void List< T >::push(T data)
+  {
+    Node< T > * topush = new Node< T >{ data };
+    if (empty())
+    {
+      head_ = topush;
+      tail_ = topush;
+    }
+    else
+    {
+      tail_->next_ = topush;
+      tail_ = topush;
+    }
   }
 
 }
