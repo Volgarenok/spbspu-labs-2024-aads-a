1a4a26dded326f3ce1de3ec12554807ee8bdfb01
pashko2303 <baranov-p.b@yandex.ru>
2024-03-08 07:45:59 +0300

    Input function

---
+++ baranov.pavel/S1/inputList.cpp
@@ -0,0 +1,17 @@
+#include "inputList.hpp"
+
+void baranov::inputList(std::istream & input, ds_t & list)
+{
+  size_t number = 0;
+  std::string name = "";
+  while (input)
+  {
+    input >> name;
+    List < size_t > in_lst;
+    while (input >> number)
+    {
+      in_lst.push_back(number);
+    }
+    list.push_back(std::make_pair(name, in_lst));
+  }
+}
--- baranov.pavel/S1/main.cpp
+++ baranov.pavel/S1/main.cpp
@@ -1,3 +1,11 @@
+#include "list.hpp"
+#include "inputList.hpp"
+#include <iostream>
+
 int main()
-{}
+{
+  using namespace baranov;
+  List< std::pair< std::string, List< size_t > > > list;
+  inputList(std::cin, list);
+}
 
