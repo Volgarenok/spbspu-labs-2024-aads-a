f75d22e7280dbcc832e1455c7235cea8adcf334d
WarriorVortex <erohinvova01010111@gmail.com>
2024-03-09 10:22:29 +0300

    Removed unused functions

--- erohin.vladimir/S1/output_named_list.cpp
+++ erohin.vladimir/S1/output_named_list.cpp
@@ -41,46 +41,6 @@ std::ostream & erohin::printNumList(std::ostream & output, const List< size_t > 
   return output;
 }
 
-std::ostream & erohin::printElementsAndSums(std::ostream & output, const List< named_list > & list)
-{
-  using iterator_list = List< ListConstIterator< size_t > >;
-  iterator_list iters;
-  auto current = list.cbegin();
-  auto last = list.cend();
-  size_t max_count = 0;
-  while (current != last)
-  {
-    iters.push_front(current->second.cbegin());
-    ++current;
-  }
-  iters.reverse();
-  List< size_t > sum;
-  auto iter_end = list.front().second.cend();
-  while (!iters.empty())
-  {
-    sum.push_front(0);
-    size_t count = 0;
-    for (auto & cur_iter: iters)
-    {
-      if (cur_iter != iter_end)
-      {
-        output << (count ? " " : "") << *cur_iter;
-        sum.front() += *cur_iter;
-        ++cur_iter;
-        ++count;
-      }
-    }
-    iters.remove(iter_end);
-    if (count < max_count || !iters.empty())
-    {
-      output << "\n";
-    }
-  }
-  sum.reverse();
-  printNumList(output, sum);
-  return output;
-}
-
 void erohin::formOrderedNumLists(List < List< size_t > > & result, const List< named_list > & list)
 {
   result.clear();
--- erohin.vladimir/S1/output_named_list.hpp
+++ erohin.vladimir/S1/output_named_list.hpp
@@ -9,7 +9,6 @@ namespace erohin
 {
   std::ostream & printNames(std::ostream & output, const List< named_list > & list);
   std::ostream & printSums(std::ostream & output, const List< size_t > & sum);
-  std::ostream & printElementsAndSums(std::ostream & output, const List< named_list > & list);
   std::ostream & printNumList(std::ostream & output, const List< size_t > & list);
   void formOrderedNumLists(List < List< size_t > > & result, const List< named_list > & list);
   void formSumList(List< size_t > & result, const List < List< size_t > > & list);
