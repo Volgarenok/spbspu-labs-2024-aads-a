0c42b8dc1706b4c2702c89da6c5e9b96730642a9
Skuratov09 <skyratovdaniil28@gmail.com>
2024-03-09 15:43:35 +0300

    Created sequence.hpp

---
+++ skuratov.danil/S1/sequence.hpp
@@ -0,0 +1,24 @@
+#ifndef SEQUENCE_HPP
+#define SEQUENCE_HPP
+
+#include <string>
+#include <vector>
+
+namespace skuratov
+{
+  class Sequence
+  {
+  public:
+    Sequence(const std::string& name): name_(name)
+    {}
+    const std::string& name() const { return name_; }
+    std::vector<int>& numbers() { return numbers_; }
+    const std::vector<int>& numbers() const { return numbers_; }
+
+  private:
+    std::string name_;
+    std::vector<int> numbers_;
+  };
+}
+
+#endif
