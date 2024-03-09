d869b5ce50a91e9f9dd9d9b936567fbe25f03df6
Skuratov09 <skyratovdaniil28@gmail.com>
2024-03-09 17:26:53 +0300

    Fixed CG

--- skuratov.danil/S1/main.cpp
+++ skuratov.danil/S1/main.cpp
@@ -7,74 +7,71 @@
 
 int main()
 {
-    using namespace skuratov;
-    std::vector<Sequence> sequences;
-    std::string line;
+  using namespace skuratov;
+  std::vector<Sequence> sequences;
+  std::string line;
 
-    while (std::getline(std::cin, line))
+  while (std::getline(std::cin, line))
+  {
+    if (!line.empty())
     {
-        if (!line.empty())
-        {
-            std::istringstream iss(line);
-            std::string name;
-            iss >> name;
-            Sequence sequence(name);
-            int num = {};
-            while (iss >> num)
-            {
-                sequence.numbers().push_back(num);
-            }
-            sequences.push_back(sequence);
-        }
+      std::istringstream iss(line);
+      std::string name;
+      iss >> name;
+      Sequence sequence(name);
+      int num = {};
+      while (iss >> num)
+      {
+        sequence.numbers().push_back(num);
+      }
+      sequences.push_back(sequence);
     }
+  }
 
+  for (const auto& seq : sequences)
+  {
+    std::cout << seq.name() << " ";
+  }
+  std::cout << '\n';
+
+  size_t maxSize = 0;
+  for (const auto& seq : sequences)
+  {
+    maxSize = std::max(maxSize, seq.numbers().size());
+  }
+
+  for (size_t i = 0; i < maxSize; ++i)
+  {
     for (const auto& seq : sequences)
     {
-        std::cout << seq.name() << " ";
+      const auto& numbers = seq.numbers();
+      if (i < numbers.size())
+      {
+        std::cout << numbers[i] << " ";
+      }
     }
     std::cout << '\n';
+  }
 
-    size_t maxSize = 0;
+  std::vector<int> column_sums(maxSize, 0);
+
+  for (size_t i = 0; i < maxSize; ++i)
+  {
     for (const auto& seq : sequences)
     {
-        maxSize = std::max(maxSize, seq.numbers().size());
+      const auto& numbers = seq.numbers();
+      if (i < numbers.size())
+      {
+        column_sums[i] += numbers[i];
+      }
     }
+  }
 
-    // Обходим последовательности по индексам
-    for (size_t i = 0; i < maxSize; ++i) {
-        // Для каждого индекса выводим элементы из всех последовательностей
-        for (const auto& seq : sequences) {
-            const auto& numbers = seq.numbers();
-            if (i < numbers.size()) {
-                std::cout << numbers[i] << " ";
-            }
-        }
-        std::cout << '\n';
-    }
+  for (const auto& sum : column_sums)
+  {
+    std::cout << sum << " ";
+  }
+  std::cout << '\n';
 
-
-    // Вектор, хранящий суммы элементов по колонкам
-    std::vector<int> column_sums(maxSize, 0);
-
-    // Суммируем элементы по колонкам
-    for (size_t i = 0; i < maxSize; ++i)
-    {
-        for (const auto& seq : sequences)
-        {
-            const auto& numbers = seq.numbers();
-            if (i < numbers.size())
-            {
-                column_sums[i] += numbers[i];
-            }
-        }
-    }
-
-    // Выводим суммы элементов по колонкам
-    for (const auto& sum : column_sums)
-    {
-        std::cout << sum << " ";
-    }
-    std::cout << '\n';
-
-    return 0;
+  return 0;
 }
