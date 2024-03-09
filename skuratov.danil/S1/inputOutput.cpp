2a72612a860ee8a815f6514997c4158b5210aa07
Skuratov09 <skyratovdaniil28@gmail.com>
2024-03-09 22:43:23 +0300

    Fixed the mistake about spaces

--- skuratov.danil/S1/inputOutput.cpp
+++ skuratov.danil/S1/inputOutput.cpp
@@ -5,68 +5,74 @@
 
 void skuratov::inputOutput()
 {
-    std::vector<Sequence> sequences;
-    std::string line;
+  std::vector<Sequence> sequences;
+  std::string line;
 
-    while (std::getline(std::cin, line))
+  while (std::getline(std::cin, line))
+  {
+    if (!line.empty())
     {
-        if (!line.empty())
-        {
-            std::istringstream iss(line);
-            std::string name;
-            iss >> name;
-            Sequence sequence(name);
-            int num = {};
-            while (iss >> num)
-            {
-                sequence.numbers().push_back(num);
-            }
-            sequences.push_back(sequence);
-        }
+      std::istringstream iss(line);
+      std::string name;
+      iss >> name;
+      Sequence sequence(name);
+      int num = {};
+      while (iss >> num)
+      {
+        sequence.numbers().push_back(num);
+      }
+      sequences.push_back(sequence);
     }
+  }
 
+  for (const auto& seq : sequences)
+  {
+    std::cout << seq.name() << " ";
+  }
+  std::cout << '\n';
+
+  size_t maxSize = 0;
+  for (const auto& seq : sequences)
+  {
+    maxSize = std::max(maxSize, seq.numbers().size());
+  }
+
+  for (size_t i = 0; i < maxSize; ++i)
+  {
+    bool isFirst = true;
     for (const auto& seq : sequences)
     {
-        std::cout << seq.name() << " ";
+      const auto& numbers = seq.numbers();
+      if (i < numbers.size())
+      {
+        if (!isFirst)
+        {
+          std::cout << " ";
+        }
+        std::cout << numbers[i];
+        isFirst = false;
+      }
     }
     std::cout << '\n';
+  }
 
-    size_t maxSize = 0;
+  std::vector<int> column_sums(maxSize, 0);
+
+  for (size_t i = 0; i < maxSize; ++i)
+  {
     for (const auto& seq : sequences)
     {
-        maxSize = std::max(maxSize, seq.numbers().size());
+      const auto& numbers = seq.numbers();
+      if (i < numbers.size())
+      {
+        column_sums[i] += numbers[i];
+      }
     }
+  }
 
-    for (size_t i = 0; i < maxSize; ++i)
-    {
-        for (const auto& seq : sequences)
-        {
-            const auto& numbers = seq.numbers();
-            if (i < numbers.size())
-            {
-                std::cout << numbers[i] << " ";
-            }
-        }
-        std::cout << '\n';
-    }
-
-    std::vector<int> column_sums(maxSize, 0);
-
-    for (size_t i = 0; i < maxSize; ++i)
-    {
-        for (const auto& seq : sequences)
-        {
-            const auto& numbers = seq.numbers();
-            if (i < numbers.size())
-            {
-                column_sums[i] += numbers[i];
-            }
-        }
-    }
-
-    for (const auto& sum : column_sums)
-    {
-        std::cout << sum << " ";
-    }
-    std::cout << '\n';
+  for (const auto& sum : column_sums)
+  {
+    std::cout << sum << " ";
+  }
+  std::cout << '\n';
 }
