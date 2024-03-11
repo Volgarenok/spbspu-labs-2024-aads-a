d63812e8a382f7008c4d8db409aa6bed3e572846
mordw1n <lolpovar0@gmail.com>
2024-03-11 23:02:41 +0300

    fix iterator

--- spiridonov.mikhail/S1/iterator.hpp
+++ spiridonov.mikhail/S1/iterator.hpp
@@ -28,9 +28,79 @@ namespace spiridonov
   };
 
   template <typename T>
-  iterator<T>::iterator()
+  iterator<T>::iterator() {}
+
+  template <typename T>
+  iterator<T>::iterator(Node<T>* node) : current(node)
   {}
 
+  template <typename T>
+  iterator<T>::iterator(const iterator& other) : current(other.current)
+  {}
+
+  template <typename T>
+  iterator<T>& iterator<T>::operator++()
+  {
+    if (current) current = current->next;
+    return *this;
+  }
+
+  template <typename T>
+  iterator<T>& iterator<T>::operator--()
+  {
+    if (current)
+    {
+      current = current->prev;
+    }
+
+    return *this;
+  }
+
+  template <typename T>
+  iterator<T>& iterator<T>::operator+(size_t n)
+  {
+    iterator temp = *this;
+    for (size_t i = 0; i < n; ++i)
+    {
+      ++temp;
+    }
+    return temp;
+  }
+
+  template <typename T>
+  iterator<T>& iterator<T>::operator-(size_t n)
+  {
+    iterator temp = *this;
+    for (size_t i = 0; i < n; ++i)
+    {
+      --temp;
+    }
+    return temp;
+  }
+
+  template <typename T>
+  bool iterator<T>::operator==(const iterator& other) const
+  {
+    return current == other.current;
+  }
+
+  template <typename T>
+  bool iterator<T>::operator!=(const iterator& other) const
+  {
+    return current != other.current;
+  }
+
+  template <typename T>
+  T& iterator<T>::operator*() const
+  {
+    return current->data;
+  }
+
+  template <typename T>
+  T* iterator<T>::operator->() const
+  {
+    return &(current->data);
+  }
 }
 
 #endif
