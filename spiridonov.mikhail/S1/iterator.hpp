f12dede9cb06f9ab96856e0c91c98ab6ba9a1a47
mordw1n <lolpovar0@gmail.com>
2024-03-11 22:49:00 +0300

    fix operator

--- spiridonov.mikhail/S1/iterator.hpp
+++ spiridonov.mikhail/S1/iterator.hpp
@@ -12,9 +12,19 @@ namespace spiridonov
 
   public:
     iterator();
-
+    iterator(Node<T>* node);
+    iterator(const iterator& other);
     ~iterator() = default;
 
+    iterator& operator++();
+    iterator& operator--();
+    iterator& operator+(size_t n);
+    iterator& operator-(size_t n);
+
+    bool operator==(const iterator& other) const;
+    bool operator!=(const iterator& other) const;
+    T& operator*() const;
+    T* operator->() const;
   };
 
   template <typename T>
