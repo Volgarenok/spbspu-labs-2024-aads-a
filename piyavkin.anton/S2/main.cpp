e58d1ee80eb5c330aba0fbe95050ba4c86d5ad30
Piyavva <piyavkin05@bk.ru>
2024-03-17 23:12:12 +0300

    Added queue(methos: empty, drop, push, size)

--- piyavkin.anton/S2/main.cpp
+++ piyavkin.anton/S2/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include "stack.hpp"
+#include "queue.hpp"
 
 int main()
 {
   using namespace piyavkin;
-  Stack< char > stack;
+  Queue< char > stack;
   char a = 0;
   char b = 0;
   std::cin >> a >> b;
   stack.push(a);
   stack.push(b);
-  std::cout << stack.list.front() << ' ' << stack.list.back();
-  stack.pop();
-  stack.pop();
-  std::cout << stack.empty();
+  std::cout << stack.list.front() << ' ' << stack.list.back() << ' ';
 }
--- piyavkin.anton/S2/queue.hpp
+++ piyavkin.anton/S2/queue.hpp
@@ -1,27 +1,31 @@
-#ifndef STACK_HPP
-#define STACK_HPP
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
 #include <list.hpp>
 
 namespace piyavkin
 {
   template< class T >
-  class Stack
+  class Queue
   {
   public:
-    Stack() = default;
+    Queue() = default;
     void push(const T& value)
     {
       list.push_back(value);
     }
     T drop()
     {
-      T temp = list.back();
-      list.pop_back();
+      T temp = list.front();
+      list.pop_front();
       return temp;
     }
     bool empty() const
     {
       return list.empty();
+    }
+    size_t size() const
+    {
+      return list.size();
     }
   // private:
     List< T > list;
