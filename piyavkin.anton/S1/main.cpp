6ffa781314598f026f2f576e9fb3dcd3da664cfb
Piyavva <piyavkin05@bk.ru>
2024-03-17 19:04:33 +0300

    Returned by worker main

--- piyavkin.anton/S1/main.cpp
+++ piyavkin.anton/S1/main.cpp
@@ -6,38 +6,30 @@
 int main()
 {
   using namespace piyavkin;
-  // size_t size = 0;
-  // size_t max_size_list = 0;
-  // std::pair< std::string, piyavkin::List< unsigned long long >* >* pairs = nullptr;
-  // try
-  // {
-  //   pairs = inputList(std::cin, size, max_size_list);
-  //   output(std::cout, pairs, size, max_size_list);
-  // }
-  // catch (const std::invalid_argument& e)
-  // {
-  //   std::cerr << e.what() << '\n';
-  //   freeMemory(pairs, size);
-  //   return 1;
-  // }
-  // catch (const std::logic_error& e)
-  // {
-  //   std::cout << 0 << '\n';
-  //   return 0;
-  // }
-  // catch (const std::exception& e)
-  // {
-  //   std::cerr << e.what() << '\n';
-  //   return 1;
-  // }
-  // std::cout << '\n';
-  // freeMemory(pairs, size);
-  List< std::pair<int,char> > mylist;
-  mylist.emplace( mylist.cbegin(), 100, 'x' );
-  mylist.emplace( mylist.cbegin(), 200, 'y' );
-  std::cout << "mylist contains:";
-  for (auto& x: mylist)
-    std::cout << " (" << x.first << "," << x.second << ")";
-
+  size_t size = 0;
+  size_t max_size_list = 0;
+  std::pair< std::string, piyavkin::List< unsigned long long >* >* pairs = nullptr;
+  try
+  {
+    pairs = inputList(std::cin, size, max_size_list);
+    output(std::cout, pairs, size, max_size_list);
+  }
+  catch (const std::invalid_argument& e)
+  {
+    std::cerr << e.what() << '\n';
+    freeMemory(pairs, size);
+    return 1;
+  }
+  catch (const std::logic_error& e)
+  {
+    std::cout << 0 << '\n';
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
   std::cout << '\n';
+  freeMemory(pairs, size);
 }
