76d06d45a7106da28b5897539384909327062757
Z0ltaan <gregnov69@gmail.com>
2024-03-18 15:27:20 +0300

    added one-methoded Stack and Queue

---
+++ isaychev.vladislav/S2/queue.hpp
@@ -0,0 +1,15 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+namespace isaychev
+{
+  template < typename T >
+  class Queue
+  {
+   public:
+    void push(T & rhs);
+    //etc
+  }
+}
+
+#endif
---
+++ isaychev.vladislav/S2/stack.hpp
@@ -0,0 +1,15 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+namespace isaychev
+{
+  template < typename T >
+  class Stack
+  {
+   public:
+    void push(T & rhs);
+    //etc
+  }
+}
+
+#endif
