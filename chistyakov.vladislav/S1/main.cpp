47f6e4b8fbc16e3349ae7def5d308c8a23e5f20c
jiggerman <liggerman@mail.ru>
2024-03-21 13:53:10 +0300

    Add main

--- chistyakov.vladislav/S1/main.cpp
+++ chistyakov.vladislav/S1/main.cpp
@@ -1,2 +1,32 @@
+#include <iostream>
+#include <limits>
+#include "list.hpp"
+#include "iterator.hpp"
+#include "constIterator.hpp"
+#include "inputList.hpp"
+
 int main()
-{}
+{
+  using namespace chistyakov;
+  List< std::pair< std::string, List< int > > > list;
+
+  try
+  {
+    inputList(std::cin, list);
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << "\n";
+  }
+
+  if (list.empty())
+  {
+    return 0;
+  }
+
+  for (auto element = list.begin(); element != list.end(); ++element)
+  {
+    std::cout << element->first;
+  }
+
+}
