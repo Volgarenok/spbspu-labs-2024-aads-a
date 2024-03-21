65f1de401e137f4a8ae643d0c3ea395a3d8dbf98
Dmitriy <Dmitry5335@yandex.ru>
2024-03-21 20:25:07 +0300

    Add common folder with list

---
+++ nikitov.dmitriy/common/node.hpp
@@ -0,0 +1,38 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <utility>
+
+namespace nikitov
+{
+  namespace detail
+  {
+    template< class T >
+    struct Node
+    {
+    public:
+      Node(): Node(T()){ };
+      explicit Node(const T& value);
+      Node(T&& value);
+      ~Node() = default;
+      T value_;
+      Node* prev_;
+      Node* next_;
+    };
+
+    template< class T >
+    Node< T >::Node(const T& value):
+      value_(value),
+      prev_(nullptr),
+      next_(nullptr)
+    {}
+
+    template< class T >
+    Node< T >::Node(T&& value):
+      value_(std::move(value)),
+      prev_(nullptr),
+      next_(nullptr)
+    {}
+  }
+}
+#endif
