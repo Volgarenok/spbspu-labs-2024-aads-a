8ffe3d6a996c4367ba13527fdf4ce8bc6197ed51
Dmitriy <Dmitry5335@yandex.ru>
2024-03-21 20:51:41 +0300

    Queue constructors and destructor

--- nikitov.dmitriy/S2/queue.hpp
+++ nikitov.dmitriy/S2/queue.hpp
@@ -1,7 +1,7 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
-#include "stack.hpp"
+#include <list.hpp>
 
 namespace nikitov
 {
@@ -9,8 +9,15 @@ namespace nikitov
   class Queue
   {
   public:
+    Queue() = default;
+    Queue(const Queue< T >& other) = default;
+    ~Queue() = default;
+
+    Queue< T >& operator=(const Queue< T >&) = default;
+
     void push(const T& value);
     T& drop();
+
   private:
     List< T > data_;
   };
