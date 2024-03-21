908461d051138ef9879aaf935f463fba20f999e2
jiggerman <liggerman@mail.ru>
2024-03-22 01:04:03 +0300

    Complete task

--- chistyakov.vladislav/S1/main.cpp
+++ chistyakov.vladislav/S1/main.cpp
@@ -37,4 +37,72 @@ int main()
 
   std::cout << "\n";
 
+  List< std::pair< int, List< int > > > listSumAndNums;
+  int nowSize = 0;
+  while (true)
+  {
+    List< int > nums;
+    int sum = 0;
+
+    for (auto element = list.begin(); element != list.end(); ++element)
+    {
+      List< int > nowList = element->second;
+      int index = 0;
+
+      for (auto numList = (element->second).begin(); numList != (element->second).end(); ++numList)
+      {
+        if (index == nowSize)
+        {
+          sum += *numList;
+          nums.push_back(*numList);
+          break;
+        }
+        index++;
+      }
+    }
+
+    if (!nums.empty())
+    {
+      listSumAndNums.push_back({sum, nums});
+    }
+    else
+    {
+      break;
+    }
+
+    nowSize++;
+  }
+
+  if (listSumAndNums.empty())
+  {
+    std::cerr << "Can't count sum, list is empty\n";
+    return 1;
+  }
+
+  for (auto element = listSumAndNums.begin(); element != listSumAndNums.end(); ++element)
+  {
+    for (auto num = (element->second).begin(); num != (element->second).end(); ++num)
+    {
+      std::cout << *num;
+      auto next = num;
+      if (next.get_BiList()->next_ != nullptr)
+      {
+        std::cout << " ";
+      }
+    }
+    std::cout << "\n";
+  }
+
+  for (auto element = listSumAndNums.begin(); element != listSumAndNums.end(); ++element)
+  {
+    std::cout << element->first;
+    auto next = element;
+    if (++next != listSumAndNums.end())
+    {
+      std::cout << " ";
+    }
+  }
+
+  std::cout << "\n";
+  return 0;
 }
