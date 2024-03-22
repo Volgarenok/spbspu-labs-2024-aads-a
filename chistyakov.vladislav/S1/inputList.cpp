ac6fce649ba348da159414807421bbcab918ace8
jiggerman <liggerman@mail.ru>
2024-03-22 17:49:40 +0300

    Remove foo in input

--- chistyakov.vladislav/S1/inputList.cpp
+++ chistyakov.vladislav/S1/inputList.cpp
@@ -12,20 +12,11 @@ void chistyakov::inputList(std::istream & input, List< std::pair< std::string, L
   {
     name = line;
     List< int > nums;
-    inputNums(input, nums);
+    while (input >> line && std::isdigit(line[0]))
+    {
+      std::size_t num = std::stoull(line);
+      nums.push_back(num);
+    }
     list.push_back({name, nums});
   }
-}
-
-void chistyakov::inputNums(std::istream & input, List< int > & nums)
-{
-  std::string digits = "";
-
-  while (input >> digits && std::isdigit(digits[0]))
-  {
-    std::size_t num = std::stoull(digits);
-    nums.push_back(num);
-  }
-
-  input.clear();
 }
