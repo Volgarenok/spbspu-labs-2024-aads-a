1c3e6550ac6f3f7d7d9d6e5a13039e26f6f61b6f
Dmitriy <Dmitry5335@yandex.ru>
2024-03-23 15:26:08 +0300

    Base input to list of queues from std::cin

--- nikitov.dmitriy/S2/main.cpp
+++ nikitov.dmitriy/S2/main.cpp
@@ -1,2 +1,65 @@
+#include <iostream>
+#include <string>
+#include <list.hpp>
+#include "queue.hpp"
+#include "expression_type.hpp"
+
 int main()
-{}
+{
+  using namespace nikitov;
+  List< Queue< ExpressionType > > expressionList;
+
+  std::cin >> std::noskipws >> std::fixed;
+  while (std::cin)
+  {
+    Queue< ExpressionType > expression;
+    std::string line = {};
+    char symb = {};
+    while (std::cin >> symb)
+    {
+      ExpressionType type;
+      if (symb == ' ' || symb == '\n')
+      {
+        if (std::isdigit(line[0]))
+        {
+          type.value.num = std::stoi(line);
+          type.storedType = 1;
+        }
+        else
+        {
+          type.value.symb = line[0];
+          type.storedType = 2;
+        }
+        line = {};
+        expression.push(type);
+
+        if (symb == '\n')
+        {
+          expressionList.push_front(expression);
+          break;
+        }
+      }
+      else
+      {
+        line += symb;
+      }
+    }
+  }
+
+  for (auto i = expressionList.begin(); i != expressionList.end(); ++i)
+  {
+    while(!((*i).empty()))
+    {
+      auto value = (*i).drop();
+      if (value.storedType == 1)
+      {
+        std::cout << value.value.num;
+      }
+      else
+      {
+        std::cout << value.value.symb;
+      }
+    }
+    std::cout << '\n';
+  }
+}
