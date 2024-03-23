9ca9dcfa4bd087fd7ad8a640aa9c6e1cf03d0ea0
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-03-23 17:41:13 +0300

    Exspression's tokens added

---
+++ zaitsev.vladimir/S2/token.hpp
@@ -0,0 +1,25 @@
+#ifndef TOKENS_HPP
+#define TOKENS_HPP
+#include "operators.hpp"
+
+namespace zaitsev
+{
+  enum struct token_type { bracket = 0, binary_operator, value };
+  enum struct bracket_type { round_open = 1 , round_close = -1 };
+
+  template<typename T>
+  union TokenValue
+  {
+    bracket_type bracket;
+    BinaryOperator<T> bin_operator;
+    T value;
+  };
+
+  template<typename T>
+  struct Token
+  {
+    token_type type;
+    TokenValue<T> token;
+  };
+}
+#endif
