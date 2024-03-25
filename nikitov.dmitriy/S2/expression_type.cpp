01279c068262a6fc02bb63ce73bb28f92cfa9e98
Dmitriy <Dmitry5335@yandex.ru>
2024-03-25 19:16:03 +0300

    Delete extra variable

--- nikitov.dmitriy/S2/expression_type.cpp
+++ nikitov.dmitriy/S2/expression_type.cpp
@@ -2,13 +2,12 @@
 
 bool nikitov::detail::Operation::operator<=(const detail::Operation& other) const
 {
-  char otherSymb = other.symb;
   if (symb == '+' || symb == '-')
   {
     return true;
   }
   else
   {
-    return otherSymb == '*' || otherSymb == '/' || otherSymb == '%';
+    return other.symb == '*' || other.symb == '/' || other.symb == '%';
   }
 }
