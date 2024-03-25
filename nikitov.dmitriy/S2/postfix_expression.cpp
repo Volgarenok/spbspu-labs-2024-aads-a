860cc32c86aed587d56c5d077c8d9905d1e6a885
Dmitriy <Dmitry5335@yandex.ru>
2024-03-25 20:42:20 +0300

    Overload of arithmetic operators for postfix expressions

--- nikitov.dmitriy/S2/postfix_expression.cpp
+++ nikitov.dmitriy/S2/postfix_expression.cpp
@@ -1,5 +1,53 @@
 #include "postfix_expression.hpp"
 
+void inputOperator(nikitov::PostfixExpression& expression, char symb, long long value)
+{
+  nikitov::PostfixType operation;
+  operation.typeName = nikitov::TypeName::operation;
+  operation.operation.symb = symb;
+  expression.add(operation);
+
+  nikitov::PostfixType operand;
+  operand.typeName = nikitov::TypeName::operand;
+  operand.operand.num = value;
+  expression.add(operand);
+}
+
+nikitov::PostfixExpression nikitov::PostfixExpression::operator+(long long value) const
+{
+  PostfixExpression newExpression(*this);
+  inputOperator(newExpression, '+', value);
+  return newExpression;
+}
+
+nikitov::PostfixExpression nikitov::PostfixExpression::operator-(long long value) const
+{
+  PostfixExpression newExpression(*this);
+  inputOperator(newExpression, '-', value);
+  return newExpression;
+}
+
+nikitov::PostfixExpression nikitov::PostfixExpression::operator*(long long value) const
+{
+  PostfixExpression newExpression(*this);
+  inputOperator(newExpression, '*', value);
+  return newExpression;
+}
+
+nikitov::PostfixExpression nikitov::PostfixExpression::operator/(long long value) const
+{
+  PostfixExpression newExpression(*this);
+  inputOperator(newExpression, '/', value);
+  return newExpression;
+}
+
+nikitov::PostfixExpression nikitov::PostfixExpression::operator%(long long value) const
+{
+  PostfixExpression newExpression(*this);
+  inputOperator(newExpression, '%', value);
+  return newExpression;
+}
+
 void nikitov::PostfixExpression::add(PostfixType& value)
 {
   data.push(value);
