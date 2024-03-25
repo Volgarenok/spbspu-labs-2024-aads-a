93b81b031cc7990a19079f71482fab6853bce019
Piyavva <piyavkin05@bk.ru>
2024-03-25 14:10:12 +0300

    Add new operations, brackets and operands

--- piyavkin.anton/S2/partexpression.hpp
+++ piyavkin.anton/S2/partexpression.hpp
@@ -3,5 +3,26 @@
 
 namespace piyavkin
 {
+  namespace detail
+  {
+    struct Operand
+    {
+      unsigned long long number;
+    };
+    struct Operation
+    {
+      char operation;
+    };
+    struct Bracket
+    {
+      char bracket;
+    };
+    enum Type
+    {
+      operand = 0;
+      operation = 1;
+      bracket = 2;
+    };
+  }
 }
 #endif
