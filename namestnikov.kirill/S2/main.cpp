5c8a3d55666ba4eb9d0a236905a176984e457eb9
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-03-27 17:29:48 +0300

    added queue and trying simple input with it

--- namestnikov.kirill/S2/main.cpp
+++ namestnikov.kirill/S2/main.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
 int main()
-{}
+{
+  std::queue<int> temp;
+  int number = 0;
+  while (true)
+  {
+    std::cin >> number;
+    if (number == 0)
+    {
+      break;
+    }
+    temp.push(number);
+  }
+  std::cout << temp.size();
+  while (!temp.empty())
+  {
+    //int ans = temp.front();
+    //std::cin << ans;
+    temp.pop();
+  }
+}
