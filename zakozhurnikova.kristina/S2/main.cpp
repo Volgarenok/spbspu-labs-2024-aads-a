d84e179cd84805992ca7fd47ab91605d95fb743d
kastiela <kristinazak150@gmail.com>
2024-03-30 14:21:58 +0300

    shablonchik for input in main for file or std::cin

--- zakozhurnikova.kristina/S2/main.cpp
+++ zakozhurnikova.kristina/S2/main.cpp
@@ -6,4 +6,19 @@
 int main(int argc, char* argv[])
 {
   using namespace zakozhurnikova;
+  try
+  {
+    if (argc == 2)
+    {
+    }
+    else
+    {
+    }
+
+  }
+  catch ()
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
 }
