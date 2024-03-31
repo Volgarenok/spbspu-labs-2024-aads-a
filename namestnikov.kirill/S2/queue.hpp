5054ce02ca2eb0007fc6d214bfa553063a9bad22
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-03-31 19:50:17 +0300

    constructors queue

--- namestnikov.kirill/S2/queue.hpp
+++ namestnikov.kirill/S2/queue.hpp
@@ -11,6 +11,19 @@ namespace namestnikov
     using queue_t = Queue< T >;
   public:
     Queue() = default;
+    Queue(const queue_t & other) = default;
+    queue_t & operator=(const queue_t & other) = default;
+    Queue(queue_t && other) noexcept:
+      data_(std::move(other.data_))
+    {}
+    queue_t & operator=(queue_t && other)
+    {
+      if (std::addressof(other) != this)
+      {
+        std::swap(data_, other.data_);
+      }
+      return *this;
+    }
     ~Queue() = default;
   private:
     std::list< T > data_;
