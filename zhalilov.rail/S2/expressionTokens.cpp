2ec5e1298a7259d1a8a838a73aaed327f8d322b2
Rail <ksovfox@mail.ru>
2024-03-31 20:06:51 +0300

    Дополнена реализация токенов

--- zhalilov.rail/S2/expressionTokens.cpp
+++ zhalilov.rail/S2/expressionTokens.cpp
@@ -4,37 +4,129 @@ zhalilov::InfixToken::InfixToken():
   type_(PrimaryType::Undefined)
 {}
 
-zhalilov::InfixToken::InfixToken(PrimaryType type):
-  type_(type)
+zhalilov::InfixToken::InfixToken(const InfixToken &tkn):
+  type_(PrimaryType::Undefined)
 {}
+
+zhalilov::InfixToken::InfixToken(BinOperator aBinOperator):
+  type_(PrimaryType::BinOperator),
+  binOperator(aBinOperator)
+{}
+
+zhalilov::InfixToken::InfixToken(Operand aOperand):
+  type_(PrimaryType::Operand),
+  operand(aOperand)
+{}
+
+zhalilov::InfixToken::InfixToken(Bracket aBracket):
+  type_(bracket.getType()),
+  bracket(aBracket)
+{}
+
+zhalilov::InfixToken &zhalilov::InfixToken::operator=(const InfixToken &tkn)
+{
+  assigner(tkn);
+  return *this;
+}
 
 zhalilov::PrimaryType zhalilov::InfixToken::getType() const
 {
   return type_;
 }
 
+void zhalilov::InfixToken::assigner(InfixToken tkn)
+{
+  switch (tkn.type_)
+  {
+  case PrimaryType::Operand:
+    type_ = PrimaryType::Operand;
+    operand = tkn.operand;
+  case PrimaryType::BinOperator:
+    type_ = PrimaryType::BinOperator;
+    binOperator = tkn.binOperator;
+  case PrimaryType::CloseBracket:
+    type_ = PrimaryType::CloseBracket;
+    bracket = Bracket(PrimaryType::CloseBracket);
+  case PrimaryType::OpenBracket:
+    type_ = PrimaryType::OpenBracket;
+    bracket = Bracket(PrimaryType::CloseBracket);
+  }
+}
+
 zhalilov::PostfixToken::PostfixToken():
   type_(PrimaryType::Undefined)
 {}
 
-zhalilov::PostfixToken::PostfixToken(PrimaryType type):
-  type_(type)
+zhalilov::PostfixToken::PostfixToken(const PostfixToken &tkn)
+{
+  assigner(tkn);
+}
+
+zhalilov::PostfixToken::PostfixToken(BinOperator aBinOperator):
+  type_(PrimaryType::BinOperator),
+  binOperator(aBinOperator)
 {}
+
+zhalilov::PostfixToken::PostfixToken(Operand aOperand):
+  type_(PrimaryType::Operand),
+  operand(aOperand)
+{}
+
+zhalilov::PostfixToken &zhalilov::PostfixToken::operator=(const PostfixToken &tkn)
+{
+  assigner(tkn);
+  return *this;
+}
 
 zhalilov::PrimaryType zhalilov::PostfixToken::getType() const
 {
   return type_;
 }
 
+void zhalilov::PostfixToken::assigner(PostfixToken tkn)
+{
+  switch (tkn.type_)
+  {
+  case PrimaryType::Operand:
+    type_ = PrimaryType::Operand;
+    operand = tkn.operand;
+  case PrimaryType::BinOperator:
+    type_ = PrimaryType::BinOperator;
+    binOperator = tkn.binOperator;
+  }
+}
+
 zhalilov::TransferToken::TransferToken():
   type_(PrimaryType::Undefined)
 {}
 
-zhalilov::TransferToken::TransferToken(PrimaryType type):
-  type_(type)
+zhalilov::TransferToken::TransferToken(const TransferToken &tkn)
+{
+  assigner(tkn);
+}
+
+zhalilov::TransferToken::TransferToken(BinOperator aBinOperator):
+  type_(PrimaryType::BinOperator),
+  binOperator(aBinOperator)
 {}
 
-zhalilov::PrimaryType zhalilov::TransferToken::getType() const
+zhalilov::TransferToken::TransferToken(Bracket aBracket):
+  type_(aBracket.getType()),
+  bracket(aBracket)
+{}
+
+void zhalilov::TransferToken::assigner(TransferToken tkn)
 {
-  return type_;
+  switch (tkn.type_)
+  {
+  case PrimaryType::BinOperator:
+    type_ = PrimaryType::BinOperator;
+    binOperator = tkn.binOperator;
+  case PrimaryType::CloseBracket:
+    type_ = PrimaryType::CloseBracket;
+    bracket = Bracket(PrimaryType::CloseBracket);
+  case PrimaryType::OpenBracket:
+    type_ = PrimaryType::OpenBracket;
+    bracket = Bracket(PrimaryType::CloseBracket);
+  }
 }
--- zhalilov.rail/S2/expressionTokens.hpp
+++ zhalilov.rail/S2/expressionTokens.hpp
@@ -8,6 +8,8 @@
 
 namespace zhalilov
 {
+  struct TransferToken;
+
   struct InfixToken
   {
     union
@@ -18,12 +20,19 @@ namespace zhalilov
     };
 
     InfixToken();
-    explicit InfixToken(PrimaryType type);
+    InfixToken(const InfixToken &tkn);
+    explicit InfixToken(BinOperator aBinOperator);
+    explicit InfixToken(Operand aOperand);
+    explicit InfixToken(Bracket aBracket);
+
+    InfixToken &operator=(const InfixToken &tkn);
 
     PrimaryType getType() const;
 
   private:
     PrimaryType type_;
+
+    void assigner(InfixToken tkn);
   };
 
   struct PostfixToken
@@ -35,12 +44,18 @@ namespace zhalilov
     };
 
     PostfixToken();
-    explicit PostfixToken(PrimaryType type);
+    PostfixToken(const PostfixToken &tkn);
+    explicit PostfixToken(BinOperator aBinOperator);
+    explicit PostfixToken(Operand aOperand);
+
+    PostfixToken &operator=(const PostfixToken &tkn);
 
     PrimaryType getType() const;
 
   private:
     PrimaryType type_;
+
+    void assigner(PostfixToken tkn);
   };
 
   struct TransferToken
@@ -52,12 +67,18 @@ namespace zhalilov
     };
 
     TransferToken();
-    explicit TransferToken(PrimaryType type);
+    TransferToken(const TransferToken &tkn);
+    explicit TransferToken(BinOperator aBinOperator);
+    explicit TransferToken(Bracket aBracket);
+
+    TransferToken &operator=(const TransferToken &tkn);
 
     PrimaryType getType() const;
 
   private:
     PrimaryType type_;
+
+    void assigner(TransferToken tkn);
   };
 }
 
