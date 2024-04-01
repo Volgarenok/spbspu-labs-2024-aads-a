3b6bb133327256cceb38bc1829bf398f13dc131a
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-04-01 17:41:13 +0300

    Added functions: empty, pop, push, size, constuctors, Assignment operators

--- novokhatskiy.danil/S2/queue.hpp
+++ novokhatskiy.danil/S2/queue.hpp
@@ -5,12 +5,64 @@
 
 namespace novokhatskiy
 {
-  template < typename T > class Queue
+  template < typename T >
+  class Queue
   {
+  public:
+    Queue() = default;
+    Queue(const Queue< T >& other) = default;
+    Queue(Queue< T >&& other) noexcept;
+    Queue< T >& operator=(const Queue< T >& other) = default;
+    Queue< T >& operator=(Queue< T >&& other) noexcept;
 
+    void push(const T& value);
+    void pop();
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    ~Queue() = default;
   private:
     ForwardList< T > data_;
   };
+
+  template < class T >
+  Queue< T >::Queue(Queue< T >&& other) noexcept :
+    data_(other.data_)
+  {}
+
+  template < class T >
+  Queue< T >& Queue< T >::operator=(Queue< T >&& other) noexcept
+  {
+    if (std::addressof(other) != this)
+    {
+      std::swap(data_, other.data_);
+      other.data_.clear;
+    }
+    return *this;
+  }
+
+  template < class T >
+  void Queue< T >::push(const T& value)
+  {
+    data_.push_back(value);
+  }
+
+  template < class T >
+  void Queue< T >::pop()
+  {
+    data_.pop_front();
+  }
+
+  template < class T >
+  bool Queue< T >::empty() const noexcept
+  {
+    return data_.empty();
+  }
+
+  template < class T >
+  size_t Queue< T >::size() const noexcept
+  {
+    return data_.max_size();
+  }
 }
 
 #endif
--- novokhatskiy.danil/S2/stack.hpp
+++ novokhatskiy.danil/S2/stack.hpp
@@ -5,10 +5,16 @@
 
 namespace novokhatskiy
 {
-  template < class T > class Stack
+  template < class T > 
+  class Stack
   {
   public:
     Stack() = default;
+    Stack(const Stack< T >& other) = default;
+    Stack(Stack< T >&& other) noexcept;
+    Stack< T >& operator=(const Stack< T >& other) = default;
+    Stack< T >& operator=(Stack< T >&& other) noexcept;
+
     T& top() const;
     bool empty() const noexcept;
     size_t size() const noexcept;
@@ -20,29 +26,50 @@ namespace novokhatskiy
     ForwardList< T > data_;
   };
 
-  template < class T > T& Stack< T >::top() const
+  template < class T > 
+  T& Stack< T >::top() const
   {
     return data_.front();
   }
 
-  template < class T > bool Stack< T >::empty() const noexcept
+  template < class T > 
+  bool Stack< T >::empty() const noexcept
   {
     return data_.empty();
   }
 
-  template < class T > size_t Stack< T >::size() const noexcept
+  template < class T > 
+  size_t Stack< T >::size() const noexcept
   {
     return data_.max_size();
   }
 
-  template < class T > void Stack< T >::push(const T& value)
+  template < class T > 
+  void Stack< T >::push(const T& value)
   {
     data_.push_front(value);
   }
 
-  template < class T > void Stack< T >::pop()
+  template < class T > 
+  void Stack< T >::pop()
   {
     data_.pop_front();
+  }
+
+  template < class T >
+  Stack< T >::Stack(Stack< T >&& other) noexcept:
+    data_(std::move(other.data))
+  {}
+
+  template < class T >
+  Stack< T >& Stack< T >::operator=(Stack< T >&& other) noexcept
+  {
+    if (std::addressof(other) != this)
+    {
+      std::swap(data_, other.data_);
+      other.data_.clear();
+    }
+    return *this;
   }
 }
 
