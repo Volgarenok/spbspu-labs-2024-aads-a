b924838fec260efe394e5acab70c21b1a9b703aa
kastiela <kristinazak150@gmail.com>
2024-04-01 17:22:05 +0300

    fix output probels

--- zakozhurnikova.kristina/S2/outputResult.cpp
+++ zakozhurnikova.kristina/S2/outputResult.cpp
@@ -6,12 +6,12 @@ void outputResult(zakozhurnikova::Stack< ll >& result)
   unsigned long long quantity = 0;
   while(!result.empty())
   {
-    std::cout << result.top();
-    result.drop();
-    if(quantity != 0 && !result.empty())
+    if(quantity != 0)
     {
       std::cout << " ";
     }
+    std::cout << result.top();
+    result.drop();
     ++quantity;
   }
 }
