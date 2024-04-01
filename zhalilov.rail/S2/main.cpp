25f10f9f5f9449509c0cedb0b04de38ed5818c89
Rail <ksovfox@mail.ru>
2024-04-01 15:50:27 +0300

    Реализована логика задания

--- zhalilov.rail/S2/main.cpp
+++ zhalilov.rail/S2/main.cpp
@@ -5,23 +5,25 @@
 #include "queue.hpp"
 #include "getInfix.hpp"
 #include "infixToPostfix.hpp"
+#include "calculateExpr.hpp"
 
 int main(int argc, char *argv[])
 {
   using namespace zhalilov;
   Stack< Queue< InfixToken > > infixes;
-  if (argc == 2)
-  {
-    std::ifstream file(argv[1]);
-    getInfixesFromStream(infixes, file);
-  }
-  else
-  {
-    getInfixesFromStream(infixes, std::cin);
-  }
-  Stack< Queue< PostfixToken > > postfixes;
+  Queue< long long > results;
   try
   {
+    if (argc == 2)
+    {
+      std::ifstream file(argv[1]);
+      getInfixesFromStream(infixes, file);
+    }
+    else
+    {
+      getInfixesFromStream(infixes, std::cin);
+    }
+    Stack< Queue< PostfixToken > > postfixes;
     while (!infixes.empty())
     {
       Queue< PostfixToken > expression;
@@ -29,10 +31,18 @@ int main(int argc, char *argv[])
       postfixes.push(expression);
       infixes.pop();
     }
+    while (!postfixes.empty())
+    {
+      results.push(calculateExpr(postfixes.top()));
+    }
   }
   catch (const std::invalid_argument &e)
   {
     std::cerr << e.what();
     return 1;
+  }
+  while (!results.empty())
+  {
+    std::cout << results.front() << '\n';
   }
 }
