baa19c3eafe1ea5e675087ae3ab3722ecb59f20e
WarriorVortex <erohinvova01010111@gmail.com>
2024-04-02 01:50:00 +0300

    Added input of simple char and numbers sequence

--- erohin.vladimir/S2/main.cpp
+++ erohin.vladimir/S2/main.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
+
 int main()
-{}
+{
+  long long operand = 0ll;
+  char operation = 0;
+  while (!std::cin.eof())
+  {
+    if (std::cin)
+    {
+      std::cin.clear();
+      std::cin >> operation;
+    }
+    std::cin >> operand;
+  }
+  std::cout << operation << " " << operand << "\n";
+}
