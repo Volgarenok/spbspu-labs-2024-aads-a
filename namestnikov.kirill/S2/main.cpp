040f5b4c844f429e74b17f92568e56bbc10514f5
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-04-03 15:13:51 +0300

    output correction

--- namestnikov.kirill/S2/main.cpp
+++ namestnikov.kirill/S2/main.cpp
@@ -26,51 +26,9 @@ int main()
     results.push(calculatePostfixExpression(postfixes.top()));
     postfixes.pop();
   }
-  std::cout << results.top();
-  /*std::string temp = "";
-  std::getline(std::cin, temp, '\n');
-  std::queue< std::string > infix;
-  std::queue< std::string > postfix;
-  inputExpression(temp, infix);
-  convertToPostfix(infix, postfix);
-  std::cout << calculatePostfixExpression(postfix);*/
-  /*std::queue<std::string> prefixExpression;
-  std::queue<std::string> postfixExpression;
-  std::stack<std::string> operationStack;
-  std::string inString = "";
-  while (std::cin >> inString)
+  while (!results.empty())
   {
-    if (std::isdigit(inString[0]))
-    {
-      postfixExpression.push(inString);      
-    }
-    else
-    {
-      if (!operationStack.empty())
-      {
-        postfixExpression.push(operationStack.top());
-        operationStack.pop();
-      }
-      operationStack.push(inString);
-    }
-  }*/
-  /*std::stack< std::string > processStack;
-  std::queue< std::string > currentQueue;
-  std::queue< std::string > resultQueue;
-  std::string inputString = "";
-  while ((!std::cin.eof()) && (std::cin >> inputString))
-  {
-    currentQueue.push(inputString);
+    (results.size() != 1) ? std::cout << results.top() << " " : std::cout << results.top() << "\n";
+    results.pop();
   }
-  //std::cout << currentQueue.size();
-  namestnikov::convertToPostfix(currentQueue, resultQueue);
-  std::string result = "";
-  //while (!resultQueue.empty())
-  //{
-    //std::cout << resultQueue.front() << "\t";
-  //  result += resultQueue.front();
-  //  resultQueue.pop();
-  //}
-  //std::cout << result << "\n";
-  std::cout << namestnikov::calculatePostfixExpression(resultQueue);*/
 }
