7c3858a2553d1b9e5a506a64ad38c24a34107348
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-04-03 11:45:11 +0300

    continue working on input

--- namestnikov.kirill/S2/input_expressions.cpp
+++ namestnikov.kirill/S2/input_expressions.cpp
@@ -1,7 +1,36 @@
 #include "input_expressions.hpp"
 
 void namestnikov::inputExpressions(std::istream & in, std::stack< std::queue< std::string > > & expressionsStack)
-{}
+{
+  std::string s = "";
+  while (std::getline(in, s, '\n'))
+  {
+    if (s.size() != 0)
+    {
+      std::queue< std::string > expression;
+      inputExpression(s, expression);
+      expressionsStack.push(expression);
+    }
+  }
+}
 
 void namestnikov::inputExpression(std::string s, std::queue< std::string > & expression)
-{}
+{
+  /*std::string delimeter = " ";
+  std::string value = "";
+  size_t pos = 0;
+  while ((pos = s.find(delimeter)) != std::string::npos)
+  {
+    value = s.substr(0, pos);
+    expression.push(value);
+    s.erase(0, pos + delimeter.length());
+  }*/
+  size_t i = 0;
+  while (i < s.size())
+  {
+    if (!std::isspace(s[i]))
+    {
+      expression.push(std::to_string(s[i]));
+    }
+  }
+}
--- namestnikov.kirill/S2/main.cpp
+++ namestnikov.kirill/S2/main.cpp
@@ -5,9 +5,35 @@
 #include "stack.hpp"
 #include "convert_to_postfix.hpp"
 #include "calculate_postfix_expression.hpp"
+#include "input_expressions.hpp"
 
 int main()
 {
+  using namespace namestnikov;
+  /*std::stack< std::queue< std::string > > infixes;
+  std::stack< int > results;
+  inputExpressions(std::cin, infixes);
+  std::stack< std::queue< std::string > > postfixes;
+  while (!infixes.empty())
+  {
+    std::queue< std::string > expression;
+    convertToPostfix(infixes.top(), expression);
+    postfixes.push(expression);
+    infixes.pop();
+  }
+  while (!postfixes.empty())
+  {
+    results.push(calculatePostfixExpression(postfixes.top()));
+    postfixes.pop();
+  }
+  std::cout << results.top();*/
+  /*std::string temp = "";
+  std::getline(std::cin, temp, '\n');
+  std::queue< std::string > infix;
+  std::queue< std::string > postfix;
+  inputExpression(temp, infix);
+  convertToPostfix(infix, postfix);
+  std::cout << calculatePostfixExpression(postfix);*/
   /*std::queue<std::string> prefixExpression;
   std::queue<std::string> postfixExpression;
   std::stack<std::string> operationStack;
@@ -37,15 +63,15 @@ int main()
     std::cout << inputString << "|";
     currentQueue.push(inputString);
   }
-  std::cout << currentQueue.size();
+  //std::cout << currentQueue.size();
   namestnikov::convertToPostfix(currentQueue, resultQueue);
-  /*std::string result = "";
+  std::string result = "";
   while (!resultQueue.empty())
   {
     //std::cout << resultQueue.front() << "\t";
     result += resultQueue.front();
     resultQueue.pop();
   }
-  std::cout << result << "\n";*/
-  std::cout << namestnikov::calculatePostfixExpression(resultQueue);
+  std::cout << result << "\n";
+  //std::cout << namestnikov::calculatePostfixExpression(resultQueue);
 }
