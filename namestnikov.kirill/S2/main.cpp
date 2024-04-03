baa7d88a5546c0f7df57e623be331e825dc94713
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-04-03 17:27:20 +0300

    try catch

--- namestnikov.kirill/S2/main.cpp
+++ namestnikov.kirill/S2/main.cpp
@@ -13,43 +13,50 @@ int main(int argc, char * argv[])
   using namespace namestnikov;
   std::stack< std::queue< std::string > > infixes;
   std::stack< long long > results;
-  inputExpressions(std::cin, infixes);
   std::stack< std::queue< std::string > > postfixes;
-  if (argc == 1)
+  try
   {
-    inputExpressions(std::cin, infixes);
-  }
-  else if (argc == 2)
-  {
-    std::ifstream in(argv[1]);
-    if (!in.is_open())
+    if (argc == 1)
     {
-      std::cerr << "Can not open file\n";
+      inputExpressions(std::cin, infixes);
+    }
+    else if (argc == 2)
+    {
+      std::ifstream in(argv[1]);
+      if (!in.is_open())
+      {
+        std::cerr << "Can not open file\n";
+        return 1;
+      }
+      inputExpressions(in, infixes);
+      in.close();
+    }
+    else
+    {
+      std::cerr << "Wrong count of parameters\n";
       return 1;
     }
-    inputExpressions(in, infixes);
-    in.close();
+    while (!infixes.empty())
+    {
+      std::queue< std::string > expression;
+      convertToPostfix(infixes.top(), expression);
+      postfixes.push(expression);
+      infixes.pop();
+    }
+    while (!postfixes.empty())
+    {
+      results.push(calculatePostfixExpression(postfixes.top()));
+      postfixes.pop();
+    }
+    while (!results.empty())
+    {
+      (results.size() != 1) ? std::cout << results.top() << " " : std::cout << results.top() << "\n";
+      results.pop();
+    }
   }
-  else
+  catch (const std::exception & e)
   {
-    std::cerr << "Wrong count of parameters\n";
+    std::cerr << e.what() << "\n";
     return 1;
-  }
-  while (!infixes.empty())
-  {
-    std::queue< std::string > expression;
-    convertToPostfix(infixes.top(), expression);
-    postfixes.push(expression);
-    infixes.pop();
-  }
-  while (!postfixes.empty())
-  {
-    results.push(calculatePostfixExpression(postfixes.top()));
-    postfixes.pop();
-  }
-  while (!results.empty())
-  {
-    (results.size() != 1) ? std::cout << results.top() << " " : std::cout << results.top() << "\n";
-    results.pop();
   }
 }
