3094e1425babf742bb4451e07e42cdab795ac1ef
User1235321 <pavreb1235321@gmail.com>
2024-04-05 23:54:04 +0300

    Add some special methods. I think now lab is ready for 1 point

--- rebdev.pavel/S2/myQueue.hpp
+++ rebdev.pavel/S2/myQueue.hpp
@@ -10,14 +10,29 @@ namespace rebdev
   class Queue
   {
     public:
+      Queue() = default;
+      Queue(const Queue & queue) = default;
+      Queue(Queue && queue) = default;
+
+      ~Queue() = default;
+
+      Queue & operator=(const Queue & queue) = default;
+      Queue & operator=(Queue && rhQueue) = default;
+
       void push(T data)
       {
         dataBase_.push_back(data);
       }
       T drop()
       {
+        if (dataBase_.size() > 0)
+        {
+          throw std::logic_error("Try to take and delete element from empty queue!");
+        }
+
         T backData = dataBase_.front();
         dataBase_.pop_front();
+
         return backData;
       }
       size_t size() const noexcept
--- rebdev.pavel/S2/myStack.hpp
+++ rebdev.pavel/S2/myStack.hpp
@@ -10,14 +10,29 @@ namespace rebdev
   class Stack
   {
     public:
+      Stack() = default;
+      Stack(const Stack & stack) = default;
+      Stack(Stack && stack) = default;
+
+      ~Stack() = default;
+
+      Stack & operator=(const Stack & stack) = default;
+      Stack & operator=(Stack && rhStack) = default;
+
       void push(T data)
       {
         dataBase_.push_back(data);
       }
       T drop()
       {
+        if (dataBase_.size() > 0)
+        {
+          throw std::logic_error("Try to take and delete element from empty stack!");
+        }
+
         T backData = dataBase_.back();
         dataBase_.pop_back();
+
         return backData;
       }
       size_t size() const noexcept
