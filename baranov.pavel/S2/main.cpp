0339a7c935eb9409b54214012f10b35cd51f9284
pashko2303 <baranov-p.b@yandex.ru>
2024-04-05 07:58:12 +0300

    Standart stack and queue

--- baranov.pavel/S2/main.cpp
+++ baranov.pavel/S2/main.cpp
@@ -1,3 +1,31 @@
+#include <iostream>
+#include "stack.hpp"
+#include "queue.hpp"
+
 int main()
-{}
+{
+  using namespace baranov;
+  int a = 0;
+
+  Stack< int > stack;
+  Queue< int > queue;
+
+  while (std::cin >> a)
+  {
+    stack.push(a);
+    queue.push(a);
+  }
+
+  while (!stack.empty())
+  {
+  std::cout << stack.top() << '\n';
+  stack.pop();
+  }
+
+  while (!queue.empty())
+  {
+  std::cout << queue.front() << '\n';
+  queue.pop();
+  }
+}
 
