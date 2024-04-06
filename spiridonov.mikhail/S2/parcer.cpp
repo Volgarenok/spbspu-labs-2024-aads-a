c1846a73ca46c7c4afc7a36bf4661860717b35b1
mordw1n <lolpovar0@gmail.com>
2024-04-06 23:40:44 +0300

    fix parcer

---
+++ spiridonov.mikhail/S2/parcer.cpp
@@ -0,0 +1,13 @@
+#include "parser.hpp"
+
+namespace spiridonov
+{
+  int Parser::parseExpression(const std::string& expression)
+  {
+  }
+
+  void Parser::applyOperation(Stack<int>& operands, char op)
+  {
+  }
+
+}
