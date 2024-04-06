8768bb6ad4aa559b9d19143dc3d0107e5f48b26e
mordw1n <lolpovar0@gmail.com>
2024-04-06 22:38:27 +0300

    detectSym hpp add

---
+++ spiridonov.mikhail/S2/detectSym.hpp
@@ -0,0 +1,21 @@
+#ifndef DETECT_SYM_HPP
+#define DETECT_SYM_HPP
+#include <string>
+
+namespace spiridonov
+{
+  bool isNumber(const std::string& s);
+  bool isOp(const std::string& s);
+  bool isLeftBracket(const std::string& s);
+  bool isRightBracket(const std::string& s);
+  size_t evalPriority(char op);
+  bool priorityEqualOrGreater(char op1, char op2);
+  int opPlus(int a, int b);
+  int opMinus(int a, int b);
+  int opMultiply(int a, int b);
+  int opDiv(int a, int b);
+  int opMod(int a, int b);
+}
+
+
+#endif
