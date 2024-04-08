91539f2a299f1ab804e0d5b11d4082ffd3a04569
mordw1n <lolpovar0@gmail.com>
2024-04-08 15:13:26 +0300

    reallocate for tests

---
+++ spiridonov.mikhail/S1/S2/node.hpp
@@ -0,0 +1,16 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+namespace spiridonov
+{
+  template < typename T >
+
+  struct Node
+  {
+    T data_;
+    Node< T >* next_;
+  };
+
+}
+
+#endif
---
+++ spiridonov.mikhail/S1/S2/parcer.hpp
@@ -0,0 +1,20 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+#include "detectSym.hpp"
+#include "stack.hpp"
+#include <string>
+#include <stdexcept>
+
+namespace spiridonov
+{
+  class Parser
+  {
+  public:
+    static int parseExpression(const std::string& expression);
+
+  private:
+    static void applyOperation(Stack<int>& operands, char op);
+  };
+}
+
+#endif
