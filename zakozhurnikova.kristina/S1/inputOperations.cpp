068c4b1ebc3538f6c449d47a139ed7b359639a79
kastiela <kristinazak150@gmail.com>
2024-04-10 12:14:31 +0300

    fix + while...

--- zakozhurnikova.kristina/S1/inputOperations.cpp
+++ zakozhurnikova.kristina/S1/inputOperations.cpp
@@ -3,24 +3,20 @@
 void zakozhurnikova::readList(List< pair >& list, std::istream& in)
 {
   std::string stringName;
-  std::cin >> stringName;
-  if (!std::cin)
+  while (in)
   {
-    return;
+    in >> stringName;
+	  size_t number = 0;
+	  List< size_t > numbers;
+	  while (in >> number)
+	  {
+	    numbers.push_back(number);
+	  }
+	  if (!in.eof())
+	  {
+	    in.clear();
+	  }
+	  auto pair = std::make_pair(stringName, numbers);
+	  list.push_back(pair);
   }
-  std::string input;
-  std::getline(std::cin, input);
-
-  size_t number = 0;
-  List< size_t > numbers;
-  while (in >> number)
-  {
-    numbers.push_back(number);
-  }
-  if (!in.eof())
-  {
-    in.clear();
-  }
-  auto pair = std::make_pair(stringName, numbers);
-  list.push_back(pair);
 }
