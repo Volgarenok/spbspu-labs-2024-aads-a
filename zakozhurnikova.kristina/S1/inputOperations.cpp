f222e53ab71ea8ef5189f0c0c72adf99f09e8bfd
kastiela <kristinazak150@gmail.com>
2024-04-10 12:06:18 +0300

    fix rm while

--- zakozhurnikova.kristina/S1/inputOperations.cpp
+++ zakozhurnikova.kristina/S1/inputOperations.cpp
@@ -2,28 +2,25 @@
 
 void zakozhurnikova::readList(List< pair >& list, std::istream& in)
 {
-  while (!in.eof())
+  std::string stringName;
+  std::cin >> stringName;
+  if (!std::cin)
   {
-    std::string stringName;
-    std::cin >> stringName;
-    if (!std::cin)
-    {
-      return;
-    }
-    std::string input;
-    std::getline(std::cin, input);
-
-    size_t number = 0;
-    List< size_t > numbers;
-    while (in >> number)
-    {
-      numbers.push_back(number);
-    }
-    if (!in.eof())
-    {
-      in.clear();
-    }
-    auto pair = std::make_pair(stringName, numbers);
-    list.push_back(pair);
+    return;
   }
+  std::string input;
+  std::getline(std::cin, input);
+
+  size_t number = 0;
+  List< size_t > numbers;
+  while (in >> number)
+  {
+    numbers.push_back(number);
+  }
+  if (!in.eof())
+  {
+    in.clear();
+  }
+  auto pair = std::make_pair(stringName, numbers);
+  list.push_back(pair);
 }
