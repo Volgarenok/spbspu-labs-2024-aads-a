95ebc8404d0c97ca08b30760fe35087b8ab6434f
kastiela <kristinazak150@gmail.com>
2024-04-10 12:19:30 +0300

    CG 1.11

--- zakozhurnikova.kristina/S1/inputOperations.cpp
+++ zakozhurnikova.kristina/S1/inputOperations.cpp
@@ -6,17 +6,17 @@ void zakozhurnikova::readList(List< pair >& list, std::istream& in)
   while (in)
   {
     in >> stringName;
-	  size_t number = 0;
-	  List< size_t > numbers;
-	  while (in >> number)
-	  {
-	    numbers.push_back(number);
-	  }
-	  if (!in.eof())
-	  {
-	    in.clear();
-	  }
-	  auto pair = std::make_pair(stringName, numbers);
-	  list.push_back(pair);
+    size_t number = 0;
+    List< size_t > numbers;
+    while (in >> number)
+    {
+      numbers.push_back(number);
+    }
+    if (!in.eof())
+    {
+      in.clear();
+    }
+    auto pair = std::make_pair(stringName, numbers);
+    list.push_back(pair);
   }
 }
