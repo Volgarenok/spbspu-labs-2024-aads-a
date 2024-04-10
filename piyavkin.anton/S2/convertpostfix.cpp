bc560d213b06b93b57b5609bce3551ab69eac26d
Piyavva <piyavkin05@bk.ru>
2024-04-10 22:43:39 +0300

    Change the conversion to a postfix expression

--- piyavkin.anton/S2/convertpostfix.cpp
+++ piyavkin.anton/S2/convertpostfix.cpp
@@ -9,63 +9,57 @@ void piyavkin::convertPostfix(infix_t& infix, postfix_t& postfix)
     Queue< Postfix > q;
     while (!it->empty())
     {
-      if (it->front().type == detail::operand)
+      if (it->front().getType() == detail::operand)
       {
-        Postfix postfix_val;
-        postfix_val.type = detail::operand;
-        postfix_val.symbol.operand = it->drop().symbol.operand;
+        Postfix postfix_val(it->front().getOperand());
+        it->pop();
         q.push(postfix_val);
       }
       else
       {
-        if (it->front().symbol.bracket.bracket == '(')
+        if (it->front().getBraket() == '(')
         {
-          ConversionExpressionType stack_val;
-          stack_val.type = detail::bracket;
-          stack_val.symbol.bracket = it->drop().symbol.bracket;
+          ConversionExpressionType stack_val(it->front().getBraket(), detail::bracket);
+          it->pop();
           stack.push(stack_val);
         }
-        else if (it->front().symbol.bracket.bracket == ')')
+        else if (it->front().getBraket() == ')')
         {
-          while (stack.top().symbol.bracket.bracket != '(')
+          while (stack.top().getBraket() != '(')
           {
             if (stack.empty())
             {
               throw std::logic_error("The parentheses are not placed correctly");
             }
-            Postfix postfix_val;
-            postfix_val.type = detail::operation;
-            postfix_val.symbol.operation = stack.drop().symbol.operation;
+            Postfix postfix_val(stack.top().getOperation());
+            stack.pop();
             q.push(postfix_val);
           }
-          stack.drop();
-          it->drop();
+          stack.pop();
+          it->pop();
         }
         else
         {
-          while (!stack.empty() && !(it->front().symbol.operation > stack.top().symbol.operation))
+          while (!stack.empty() && !(it->front().getOperation() > stack.top().getOperation()))
           {
-            if (stack.top().type == detail::bracket)
+            if (stack.top().getType() == detail::bracket)
             {
               break;
             }
-            Postfix postfix_val;
-            postfix_val.type = detail::operation;
-            postfix_val.symbol.operation = stack.drop().symbol.operation;
+            Postfix postfix_val(stack.top().getOperation());
+            stack.pop();
             q.push(postfix_val);
           }
-          ConversionExpressionType stack_val;
-          stack_val.type = detail::operation;
-          stack_val.symbol.operation = it->drop().symbol.operation;
+          ConversionExpressionType stack_val(it->front().getOperation(), detail::operation);
+          it->pop();
           stack.push(stack_val);
         }
       }
     }
     while (!stack.empty())
     {
-      Postfix postfix_val;
-      postfix_val.type = detail::operation;
-      postfix_val.symbol.operation = stack.drop().symbol.operation;
+      Postfix postfix_val(stack.top().getOperation());
+      stack.pop();
       q.push(postfix_val);
     }
     postfix.push_back(q);
