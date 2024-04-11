7ca691de3f10cdfda5962346d9da03475d7df2af
osakasonyboy@gmail.com <sensh0god@gmail.com>
2024-04-11 13:16:19 +0300

    changed input format

--- arakelyan.daniil/S1/inputData.cpp
+++ arakelyan.daniil/S1/inputData.cpp
@@ -8,17 +8,33 @@
 
 void arakelyan::inputData(std::istream &input, BinList < std::pair < std::string, BinList< size_t > > > &listOfLists)
 {
-  std::string line = "";
+  // std::string line = "";
   std::string name = "";
-  input >> line;
+  // input >> line;
+  // while (!input.eof())
+  // {
+  //   name = line;
+  //   BinList< size_t > temp;
+  //   while (input >> line && std::isdigit(line[0]))
+  //   {
+  //     size_t val = std::stoull(line);
+  //     temp.push_back(val);
+  //   }
+  //   listOfLists.push_back({name, temp});
+  // }
   while (!input.eof())
   {
-    name = line;
-    BinList< size_t > temp;
-    while (input >> line && std::isdigit(line[0]))
+    if (!input)
     {
-      size_t val = std::stoull(line);
-      temp.push_back(val);
+      input.clear();
+      input.setstate(std::ios::goodbit);
+    }
+    BinList< size_t > temp;
+    input >> name;
+    size_t nums = 0;
+    while (input >> nums)
+    {
+      temp.push_back(nums);
     }
     listOfLists.push_back({name, temp});
   }
