1378ecc283a2784aa81c656a702760ef0f9f49f0
marlions <marishin.lv@edu.spbstu.ru>
2024-04-14 13:38:29 +0300

    refactor input

--- marishin.leonid/S1/input.cpp
+++ marishin.leonid/S1/input.cpp
@@ -1,114 +1,24 @@
 #include "input.hpp"
 
-void marishin::readInput(LinkedList< std::pair< std::string, LinkedList< size_t >>>& pairs)
+void marishin::inputList(LinkedList< std::pair< std::string, LinkedList< size_t > > > & pairs, std::istream & input)
 {
   std::string str;
-  std::istream& input = std::cin;
-  input >> str;
+  size_t num = 0;
   while (input)
   {
-    pairs.push_back({ str, LinkedList< size_t >() });
     input >> str;
-    while (input && std::isdigit(str[0]))
-    {
-      pairs.back().second.push_back(std::stoull(str));
-     input >> str;
-    }
-  }
-}
-
-void marishin::printPairs(const LinkedList< std::pair< std::string, LinkedList< size_t >>>& pairs)
-{
-  auto iter = pairs.begin();
-  auto end = pairs.end();
-
-  while (iter != end)
-  {
-    std::cout << (*iter).first;
-    if (++iter != end)
-    {
-      std::cout << " ";
-    }
-  }
-
-  if (!pairs.empty())
-  {
-    std::cout << "\n";
-  }
-}
-
-void marishin::calculateSums(
-  const LinkedList< std::pair< std::string, LinkedList< size_t >>>& pairs,
-  bool& overflow,
-  LinkedList< size_t >& lst
-)
-{
-  constexpr size_t maxvalue = std::numeric_limits< size_t >::max();
-  while (!pairs.empty())
-  {
-    size_t sum = 0;
-    bool emptyList = false;
-
-    for (auto it = pairs.begin(); it != pairs.end(); ++it)
-    {
-      auto& pair = *it;
-      if (!pair.second.empty())
-      {
-        if (emptyList)
-        {
-          std::cout << " ";
-        }
-        emptyList = true;
-        auto frontValue = pair.second.front();
-        std::cout << frontValue;
-        if (maxvalue - sum > frontValue)
-        {
-          sum += frontValue;
-          pair.second.pop_front();
-        }
-        else
-        {
-          overflow = true;
-          sum = std::numeric_limits< size_t >::max();
-          pair.second.pop_front();
-        }
-      }
-    }
-
-    if (!emptyList)
+    if (str.empty())
     {
       break;
     }
-
-    lst.push_back(sum);
-    std::cout << "\n";
-  }
-}
-
-void marishin::printResults(const LinkedList< size_t >& lst, bool overflow)
-{
-  if (overflow)
-  {
-    throw std::overflow_error("Overflow!");
-  }
-  else
-  {
-    for (auto& sum : lst)
+    pairs.push_back({ str, LinkedList< size_t >() });
+    while (input >> num)
     {
-      std::cout << sum;
-      if (&sum != &lst.back())
-      {
-        std::cout << " ";
-      }
-      if (&sum == &lst.back())
-      {
-        std::cout << "\n";
-      }
+      pairs.back().second.push_back(num);
     }
-  }
-
-  if (lst.empty())
-  {
-    std::cout << "0\n";
+    if (!input.eof())
+    {
+      input.clear();
+    }
   }
 }
--- marishin.leonid/S1/input.hpp
+++ marishin.leonid/S1/input.hpp
@@ -1,15 +1,13 @@
 #ifndef INPUT_HPP
 #define INPUT_HPP
 
+#include <string>
+#include <istream>
 #include "linkedList.hpp"
-#include <limits>
 
 namespace marishin
 {
-  void readInput(LinkedList< std::pair< std::string, LinkedList< size_t >>>& pairs);
-  void printPairs(const LinkedList< std::pair< std::string, LinkedList< size_t >>>& pairs);
-  void calculateSums(const LinkedList< std::pair< std::string, LinkedList< size_t >>>& pairs, bool& overflow, LinkedList< size_t >& lst);
-  void printResults(const LinkedList< size_t >& lst, bool overflow);
+  void inputList(LinkedList< std::pair< std::string, LinkedList< size_t > > > & pairs, std::istream & input);
 }
 
 #endif
