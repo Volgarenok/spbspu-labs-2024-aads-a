37866728fb3f27016cc42dd616aa2f45f476ea19
Piyavva <piyavkin05@bk.ru>
2024-04-14 14:30:15 +0300

    Add treeiterator.hpp

---
+++ piyavkin.anton/S4/treeiterator.hpp
@@ -0,0 +1,15 @@
+#ifndef TREEITERATOR_HPP
+#define TREEITERATOR_HPP
+#include <iterator>
+#include "treenode.hpp"
+
+namespace piyavkin
+{
+  template< class Key, class T, class Compare = std::less< Key > >
+  class Tree;
+  template< class Key, class T, class Compare = std::less< Key > >
+  class TreeIterator: public std::iterator< std::bidirectional_iterator_tag, T >
+  {
+  };
+}
+#endif
