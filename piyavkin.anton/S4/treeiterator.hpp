ae2c54516eac8dd91eab24fe210f4a6bfbd25af8
Piyavva <piyavkin05@bk.ru>
2024-04-14 23:19:25 +0300

    Add operators ++ for iterators

--- piyavkin.anton/S4/treeiterator.hpp
+++ piyavkin.anton/S4/treeiterator.hpp
@@ -10,6 +10,35 @@ namespace piyavkin
   template< class Key, class T, class Compare = std::less< Key > >
   class TreeIterator: public std::iterator< std::bidirectional_iterator_tag, T >
   {
+    friend class Tree< Key, T, Compare >;
+  public:
+    TreeIterator():
+      node_(nullptr)
+    {}
+    TreeIterator(const TreeIterator< Key, T, Compare >&) = default;
+    TreeIterator< Key, T, Compare >& operator=(const TreeIterator< Key, T, Compare >&) = default;
+    ~TreeIterator() = default;
+    TreeIterator< Key, T, Compare >& operator++()
+    {
+      if (node_->right_)
+      {
+        node_ = node_->right_;
+        return *this;
+      }
+      while (!node_->right_ && node_->parent_->right_ == node_)
+      {
+        node_ = node_->parent_;
+      }
+      return *this;
+    }
+    TreeIterator< Key, T, Compare > operator++(int)
+    {
+      TreeIterator< Key, T, Compare > result(*this);
+      ++(*this);
+      return result;
+    }
+  private:
+    detail::Node< Key, T >* node_; 
   };
 }
 #endif
