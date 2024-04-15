96be1038f5956d00d12eed8df4ce11d0cfca35af
exxiiint <filipp.sivkov@yandex.ru>
2024-04-15 23:17:56 +0300

    fix stack

--- sivkov.filipp/S2/stack.hpp
+++ sivkov.filipp/S2/stack.hpp
@@ -5,68 +5,105 @@
 
 namespace sivkov
 {
-  template<typename T>
+  template< typename T >
   class Stack
   {
   public:
-    Stack()
-    {}
-
-    Stack(const Stack& other):
-      list(other.list)
-    {}
-
-    Stack(Stack&& other) noexcept:
-      list(std::move(other.list))
-    {}
+    Stack();
+    Stack(const Stack& other);
+    Stack(Stack&& other) noexcept;
+    ~Stack() = default;
 
     void push(const T& value);
     void pop();
     T& top();
+    T drop();
     bool empty() const;
     size_t size() const;
-    Stack< T >& operator=(const Stack< T >&);
+    void swap(Stack< T >& other);
+
+    Stack< T >& operator=(const Stack< T >& other);
     Stack< T >& operator=(Stack< T >&& other) noexcept;
 
   private:
-    List<T> list;
+    List< T > list_;
   };
 
-  template<typename T>
-  void Stack<T>::push(const T& value)
+  template< typename T >
+  Stack< T >::Stack() :
+    list_()
+  {}
+
+  template< typename T >
+  Stack< T >::Stack(const Stack& other):
+    list_(other.list_)
+  {}
+
+  template< typename T >
+  Stack< T >::Stack(Stack&& other) noexcept:
+    list_(std::move(other.list_))
   {
-    list.push_front(value);
+    other.list_ = list_();
   }
 
-  template<typename T>
-  void Stack<T>::pop()
+  template< typename T >
+  void Stack< T >::push(const T& value)
   {
-    if (!list.empty())
-    {
-      list.pop_front();
-    }
+    list_.push_front(value);
   }
 
-  template<typename T>
-  T& Stack<T>::top()
+  template< typename T >
+  void Stack< T >::pop()
   {
-    if (!list.empty())
-    {
-      return list.front();
-    }
-    throw std::out_of_range("Stack is empty");
+    list_.pop_front();
   }
 
-  template<typename T>
-  bool Stack<T>::empty() const
+  template< typename T >
+  T& Stack< T >::top()
   {
-    return list.empty();
+    return list_.front();
   }
 
-  template<typename T>
-  size_t Stack<T>::size() const
+  template< typename T >
+  T Stack< T >::drop()
   {
-    return list.getSize();
+    T data = list_.front();
+    list_.pop_front();
+    return data;
+  }
+
+  template< typename T >
+  bool Stack< T >::empty() const
+  {
+    return list_.empty();
+  }
+
+  template< typename T >
+  size_t Stack< T >::size() const
+  {
+    return list_.getSize();
+  }
+
+  template< typename T >
+  void Stack< T >::swap(Stack< T >& other)
+  {
+    std::swap(list_, other.list_());
+  }
+
+  template< typename T >
+  Stack< T >& Stack< T >::operator=(const Stack< T >& other)
+  {
+    list_ = other.list_;
+    return *this;
+  }
+
+  template< typename T >
+  Stack< T >& Stack< T >::operator=(Stack< T >&& other) noexcept
+  {
+    list_ = std::move(other.list_);
+    other.list_ = list_();
+    return *this;
   }
 }
 #endif
+
