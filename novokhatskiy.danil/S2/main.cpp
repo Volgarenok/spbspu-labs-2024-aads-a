4dc78bb77668bac711a70ad70c7da706591c5e41
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-04-15 09:53:06 +0300

    Need to fix CG

--- novokhatskiy.danil/S2/main.cpp
+++ novokhatskiy.danil/S2/main.cpp
@@ -1,15 +1,35 @@
-#include <exception>
-#include <fstream>
-#include <iostream>
-#include <list>
-#include <string>
 #include "calculate_expression.hpp"
 #include "convert_expression.hpp"
 #include "input_infix.hpp"
 #include "queue.hpp"
 #include "stack.hpp"
 #include "tokens.hpp"
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <string>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+  using namespace novokhatskiy;
+  Queue<Queue<InfixType>> infixQueue;
+  try {
+    if (argc == 2) {
+      std::ifstream in(argv[1]);
+      inputInfix(infixQueue, in);
+    } else if (argc == 1) {
+      inputInfix(infixQueue, std::cin);
+    } else {
+      std::cerr << "Wrong input arguments\n";
+      return 1;
+    }
+    while (!infixQueue.empty()) {
+      Queue<InfixType> tmp = infixQueue.drop();
+      Queue<Postfix> res = convertExpression(std::move(tmp));
+      std::cout << calculatePostExp(std::move(res)) << '\n';
+    }
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
 }
--- novokhatskiy.danil/S2/tokens.hpp
+++ novokhatskiy.danil/S2/tokens.hpp
@@ -1,33 +1,30 @@
 #ifndef EXPRESSION_HPP
 #define EXPRESSION_HPP
 
-#include <cstddef>
 #include "basicTypes.hpp"
+#include <cstddef>
 
-namespace novokhatskiy
-{
-  struct InfixType
-  {
-    TokenType type;
-    union {
-      Operand operand;
-      Operation operation;
-      Bracket bracket;
-    };
+namespace novokhatskiy {
+struct InfixType {
+  TokenType type;
+  union {
+    Operand operand;
+    Operation operation;
+    Bracket bracket;
   };
+};
 
-  struct Postfix
-  {
-    TokenType type;
-    union {
-      Operand operand;
-      Operation operation;
-    };
-    Postfix() = default;
-    Postfix(InfixType&& inf);
-    Postfix(TokenType inType, Operation inOperation);
-    Postfix convertToPostfix(const InfixType& inf);
+struct Postfix {
+  TokenType type;
+  union {
+    Operand operand;
+    Operation operation;
   };
-}
+  Postfix() = default;
+  Postfix(InfixType &&inf);
+  Postfix(TokenType inType, Operation inOperation);
+  Postfix convertToPostfix(const InfixType &inf);
+};
+} // namespace novokhatskiy
 
 #endif
