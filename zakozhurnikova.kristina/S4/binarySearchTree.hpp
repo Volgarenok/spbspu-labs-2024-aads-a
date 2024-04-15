53937def47ab5ae863c784c776deb4b68ef41034
kastiela <kristinazak150@gmail.com>
2024-04-15 23:27:14 +0300

    жестко пушим ноды по самое не хочу без балансировки см и регистрации

--- zakozhurnikova.kristina/S4/binarySearchTree.hpp
+++ zakozhurnikova.kristina/S4/binarySearchTree.hpp
@@ -1,6 +1,7 @@
 #ifndef BINARYSEARCHTREE_HPP
 #define BINARYSEARCHTREE_HPP
 #include "treeNode.hpp"
+#include <cstddef>
 
 namespace zakozhurnikova
 {
@@ -15,11 +16,47 @@ namespace zakozhurnikova
       return this->size;
     }
 
+    void push(Key key, Value val)
+    {
+      if (this->root)
+      {
+        this->put(key, val, this->root);
+      }
+      else
+      {
+        this->root = new TreeNode< Key, Value >(key, val);
+      }
+      this->size = this->size + 1;
+    }
+
+    void put(Key key, Value val, TreeNode< Key, Value >* currentNode)
+    {
+      if (key < currentNode->key)
+      {
+        if (currentNode->hasLeftChild())
+        {
+          this->put(key, val, currentNode->leftChild);
+        }
+        else
+        {
+          currentNode->leftChild = new TreeNode< Key, Value >(key, val, currentNode);
+        }
+      }
+      else
+      {
+        if (currentNode->hasRightChild()){
+            this->_put(key, val, currentNode->rightChild);
+        }
+        else
+        {
+          currentNode->rightChild = new TreeNode< Key, Value >(key, val, currentNode);
+        }
+      }
+    }
   private:
     TreeNode< Key, Value >* root;
-    int size;
+    size_t size;
   };
-
 }
 
 #endif
