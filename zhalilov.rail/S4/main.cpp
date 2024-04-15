76ff208202764914f5e5a32b2eb3784d8f1baf30
Rail <ksovfox@mail.ru>
2024-04-15 12:54:54 +0300

    Добавлен вывод символа конца строки

--- zhalilov.rail/S4/main.cpp
+++ zhalilov.rail/S4/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     }
     catch (const std::invalid_argument &e)
     {
-      std::cout << "<INVALID COMMAND>";
+      std::cout << "<INVALID COMMAND>\n";
     }
     catch (const std::exception &e)
     {
