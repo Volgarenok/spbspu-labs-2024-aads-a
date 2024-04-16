614928ab0d5b086476f89ffb4fbcfcf4e8545900
kastiela <kristinazak150@gmail.com>
2024-04-16 23:58:37 +0300

    навалила кринжа, разберемся с булевыми бетодами чуть позже, пусть будет, хотя половина из них мб уйдёт, если и не все

--- zakozhurnikova.kristina/S4/treeNode.hpp
+++ zakozhurnikova.kristina/S4/treeNode.hpp
@@ -11,6 +11,46 @@ namespace zakozhurnikova
     TreeNode *leftChild;
     TreeNode *rightChild;
     TreeNode *parent;
+
+    TreeNode* hasLeftChild()
+    {
+      return this->leftChild;
+    }
+
+    TreeNode *hasRightChild()
+    {
+      return this->rightChild;
+    }
+
+    bool isLeftChild()
+    {
+      return this->parent && this->parent->leftChild == this;
+    }
+
+    bool isRightChild()
+    {
+      return this->parent && this->parent->rightChild == this;
+    }
+
+    bool isRoot()
+    {
+      return !this->parent;
+    }
+
+    bool isLeaf()
+    {
+      return !(this->rightChild || this->leftChild);
+    }
+
+    bool hasAnyChildren()
+    {
+      return this->rightChild || this->leftChild;
+    }
+
+    bool hasBothChildren()
+    {
+      return this->rightChild && this->leftChild;
+    }
   };
 }
 
