927997eb212438a5bbe3e9759dd9354b4cdebe3f
NovokhatskiyDanil <karuk.vova2005@mail.ru>
2024-04-16 14:48:11 +0300

    add stack output

--- novokhatskiy.danil/S2/input_infix.cpp
+++ novokhatskiy.danil/S2/input_infix.cpp
@@ -1,15 +1,14 @@
 #include <string>
 #include "input_infix.hpp"
 
-void novokhatskiy::inputInfix(Queue< Queue< InfixType > >& infixQueue, std::istream& in)
+void novokhatskiy::inputInfix(Queue<Queue<InfixType>> &infixQueue, std::istream &in)
 {
   in >> std::noskipws;
-  while (!in.eof())
+  while (in)
   {
     char symb{};
-    Queue< InfixType > currQ;
-
-    while (in >> symb && symb != '\n')
+    Queue<InfixType> currQ;
+    while (in >> symb && symb != '\n' && !in.eof())
     {
       InfixType res;
       switch (symb)
@@ -20,12 +19,12 @@ void novokhatskiy::inputInfix(Queue< Queue< InfixType > >& infixQueue, std::istr
       case '/':
       case '%':
         res.type = TokenType::OPERATION;
-        res.operation = static_cast< Operation >(symb);
+        res.operation = static_cast<Operation>(symb);
         break;
       case '(':
       case ')':
         res.type = TokenType::BRACKET;
-        res.bracket = static_cast< Bracket >(symb);
+        res.bracket = static_cast<Bracket>(symb);
         break;
       default:
         if (std::isdigit(symb))
--- novokhatskiy.danil/S2/main.cpp
+++ novokhatskiy.danil/S2/main.cpp
@@ -10,10 +10,10 @@
 #include "stack.hpp"
 #include "tokens.hpp"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   using namespace novokhatskiy;
-  Queue< Queue< InfixType > > infixQueue;
+  Queue<Queue<InfixType>> infixQueue;
   try
   {
     if (argc == 2)
@@ -30,14 +30,20 @@ int main(int argc, char** argv)
       std::cerr << "Wrong input arguments\n";
       return 1;
     }
+    Stack<long long> stack;
     while (!infixQueue.empty())
     {
-      Queue< InfixType > tmp = infixQueue.drop();
-      Queue< Postfix > res = convertExpression(std::move(tmp));
-      std::cout << calculatePostExp(std::move(res)) << '\n';
+      Queue<InfixType> tmp = infixQueue.drop();
+      Queue<Postfix> res = convertExpression(std::move(tmp));
+      stack.push(calculatePostExp(std::move(res)));
+      // std::cout << calculatePostExp(std::move(res)) << '\n';
+    }
+    while (!stack.empty())
+    {
+      std::cout << stack.drop() << ' ';
     }
   }
-  catch (const std::exception& e)
+  catch (const std::exception &e)
   {
     std::cerr << e.what() << '\n';
     return 1;
