c1ab04b802d61f1b1efa9e55e2ba461592438610
Dmitriy <Dmitry5335@yandex.ru>
2024-04-17 15:53:08 +0300

    Input parameters check

--- nikitov.dmitriy/S4/main.cpp
+++ nikitov.dmitriy/S4/main.cpp
@@ -1,2 +1,16 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+  if (argc == 2)
+  {
+    std::ifstream input(argv[1]);
+  }
+  else
+  {
+    std::cerr << "Error: Wrong input parameters" << '\n';
+    return 1;
+  }
+  return 0;
+}
