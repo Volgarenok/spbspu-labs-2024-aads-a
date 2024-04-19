1fb723dc4155c8ae093a9fe22e3f2887077d7f5c
exxiiint <filipp.sivkov@yandex.ru>
2024-04-19 18:29:28 +0300

    update main.cpp

--- sivkov.filipp/S2/main.cpp
+++ sivkov.filipp/S2/main.cpp
@@ -1,3 +1,30 @@
-int main()
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <string>
+#include "infixToPostfix.hpp"
+#include "utilities.hpp"
+#include "calcultPostfix.hpp"
+
+int main(int argc, char* argv[])
 {
+  std::queue<char> numb;
+  std::stack<int> infix;
+
+  if (argc == 2)
+  {
+    std::ifstream inputFile(argv[1]);
+    infixToPostFix(inputFile, numb);
+  }
+  else
+  {
+    infixToPostFix(std::cin, numb);
+  }
+
+  int rez = 0;
+  rez = calcult(numb);
+
+  std::cout << rez;
+  return 0;
 }
