ed059b02ea63f76dc9111e00b3af703761ef4691
seep-code <seepox7@gmail.com>
2024-04-22 23:03:29 +0300

    main

--- ishmuratov.eric/S2/main.cpp
+++ ishmuratov.eric/S2/main.cpp
@@ -43,11 +43,108 @@ int calculate(int n1, int n2, const std::string& opr)
   return 0;
 }
 
+void input_infix(std::string line, ishmuratov::Queue< std::string > & input_queue)
+{
+  std::string token;
+  for (char i : line)
+  {
+    if (!std::isspace(i))
+    {
+      token += i;
+    }
+    else
+    {
+      input_queue.push(token);
+      token = "";
+    }
+  }
+  input_queue.push(token);
+}
+
+void topostfix(ishmuratov::Queue< std::string > & process_queue, ishmuratov::Queue< std::string > & result_queue)
+{
+  ishmuratov::Stack< std::string > process_stack;
+  while (!process_queue.empty())
+  {
+    std::string temp = process_queue.drop();
+    if (std::isdigit(temp[0]))
+    {
+      result_queue.push(temp);
+    }
+    else if (temp == "(")
+    {
+      process_stack.push(temp);
+    }
+    else if (temp == ")")
+    {
+      while (process_stack.top() != "(")
+      {
+        result_queue.push(process_stack.drop());
+      }
+      process_stack.drop();
+    }
+    else if ((temp == "+") || (temp == "-"))
+    {
+      if (process_stack.empty())
+      {
+        process_stack.push(temp);
+      }
+      else
+      {
+        while (!process_stack.empty() && low_priority(process_stack.top()))
+        {
+          result_queue.push(process_stack.drop());
+        }
+        process_stack.push(temp);
+      }
+    }
+    else if (temp == "*" || temp == "/" || temp == "%")
+    {
+      if (process_stack.empty())
+      {
+        process_stack.push(temp);
+      }
+      else
+      {
+        while (!process_stack.empty() && high_priority(process_stack.top()))
+        {
+          result_queue.push(process_stack.drop());
+        }
+        process_stack.push(temp);
+      }
+    }
+  }
+  while(!process_stack.empty())
+  {
+    result_queue.push(process_stack.drop());
+  }
+}
+
+void calculate_postfix(ishmuratov::Queue< std::string > & result_queue, ishmuratov::Stack< int > & operands)
+{
+  while (!result_queue.empty())
+  {
+    std::string temp = result_queue.drop();
+    if (isdigit(temp[0]))
+    {
+      operands.push(std::stoi(temp));
+    }
+    else if (isOperator(temp))
+    {
+      int n2 = operands.drop();
+      int n1 = operands.drop();
+      int res = calculate(n1, n2, temp);
+      operands.push(res);
+    }
+  }
+}
+
 int main(int argc, char * argv[])
 {
   using namespace ishmuratov;
   std::istream * input;
   std::ifstream input_file;
+
   if (argc == 1)
   {
     input = &std::cin;
@@ -63,100 +160,13 @@ int main(int argc, char * argv[])
   Queue< std::string > result_queue;
   Stack< int > operands;
   Stack< int > results;
-
   std::string line;
+
   while (std::getline(*input, line))
   {
-    //read infix
-    std::string token;
-    for (char i : line)
-    {
-      if (!std::isspace(i))
-      {
-        token += i;
-      }
-      else
-      {
-        process_queue.push(token);
-        token = "";
-      }
-    }
-    process_queue.push(token);
-
-
-    //to postfix
-    while (!process_queue.empty())
-    {
-      std::string temp = process_queue.drop();
-      if (std::isdigit(temp[0]))
-      {
-        result_queue.push(temp);
-      }
-      else if (temp == "(")
-      {
-        process_stack.push(temp);
-      }
-      else if (temp == ")")
-      {
-        while (process_stack.top() != "(")
-        {
-          result_queue.push(process_stack.drop());
-        }
-        process_stack.drop();
-      }
-      else if ((temp == "+") || (temp == "-"))
-      {
-        if (process_stack.empty())
-        {
-          process_stack.push(temp);
-        }
-        else
-        {
-          while (!process_stack.empty() && low_priority(process_stack.top()))
-          {
-            result_queue.push(process_stack.drop());
-          }
-          process_stack.push(temp);
-        }
-      }
-      else if (temp == "*" || temp == "/" || temp == "%")
-      {
-        if (process_stack.empty())
-        {
-          process_stack.push(temp);
-        }
-        else
-        {
-          while (!process_stack.empty() && high_priority(process_stack.top()))
-          {
-            result_queue.push(process_stack.drop());
-          }
-          process_stack.push(temp);
-        }
-      }
-    }
-    while(!process_stack.empty())
-    {
-      result_queue.push(process_stack.drop());
-    }
-
-    //result
-    while (!result_queue.empty())
-    {
-      std::string temp = result_queue.drop();
-      if (isdigit(temp[0]))
-      {
-        operands.push(std::stoi(temp));
-      }
-      else if (isOperator(temp))
-      {
-        int n2 = operands.drop();
-        int n1 = operands.drop();
-        int res = calculate(n1, n2, temp);
-        operands.push(res);
-      }
-    }
-
+    input_infix(line, process_queue);
+    topostfix(process_queue, result_queue);
+    calculate_postfix(result_queue, operands);
     results.push(operands.top());
   }
 
