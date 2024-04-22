296c2a8042b576651f09e097b49595ee2202dc93
Steuger7 <steuger7@mail.ru>
2024-04-22 21:42:16 +0300

    add queue

--- grechishnikov.roman/S2/main.cpp
+++ grechishnikov.roman/S2/main.cpp
@@ -1,17 +1,17 @@
-#include "iostream"
-#include "stack.hpp"
+#include <iostream>
+#include "queue.hpp"
 
 int main()
 {
   using namespace grechishnikov;
-  Stack< int > s1;
+  Queue< int > s1;
   s1.push(10);
   s1.push(20);
-  Stack< int > s2(s1);
+  Queue< int > s2(s1);
   std::cout << s2.top() << '\n';
   s2.pop();
   std::cout << s2.top() << '\n';
-  Stack< int > s3(std::move(s1));
+  Queue< int > s3(std::move(s1));
   std::cout << s3.top() << '\n';
   s3.pop();
   std::cout << s3.top() << '\n';
