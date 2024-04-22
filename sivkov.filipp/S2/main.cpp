c91452caef73bd58401c08f3fbdfba78549b959e
exxiiint <filipp.sivkov@yandex.ru>
2024-04-22 23:42:05 +0300

    edit new maim

--- sivkov.filipp/S2/main.cpp
+++ sivkov.filipp/S2/main.cpp
@@ -7,41 +7,62 @@
 #include "utilities.hpp"
 #include "calcultPostfix.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::string in = "";
-  std::queue< std::string > infix1;
-  std::string token = "";
-  std::stack< std::string > oper;
   std::queue <std::queue< std::string >> postfix;
+  std::istream* input;
+  std::ifstream file;
 
-  while (std::getline(std::cin, in))
+  if (argc == 1)
   {
-    if (in == "")
-    {
-      continue;
-    }
-    for (size_t i = 0; i < in.length(); ++i)
-    {
-      token = "";
-      while (i < in.length() && !std::isspace(in[i]))
-      {
-        token += in[i];
-        ++i;
-      }
-      infix1.push(token);
-    }
-    postfix.push(infixToPostfix(infix1));
+    input = &std::cin;
+  }
+  else if (argc == 2)
+  {
+    file.open(argv[1]);
+    input = &file;
+  }
+  else
+  {
+    std::cerr << "Error comand line\n";
+    return 2;
   }
 
+  std::string in = "";
+  std::string token = "";
+  std::queue< std::string > infix1;
   std::queue<long long> res;
-
-  while (!postfix.empty())
+  try
   {
-    res.push(calcult(postfix.front()));
-    postfix.pop();
+    while (std::getline(*input, in))
+    {
+      if (in == "")
+      {
+        continue;
+      }
+      for (size_t i = 0; i < in.length(); ++i)
+      {
+        token = "";
+        while (i < in.length() && !std::isspace(in[i]))
+        {
+          token += in[i];
+          ++i;
+        }
+        infix1.push(token);
+      }
+      postfix.push(infixToPostfix(infix1));
+    }
+    while (!postfix.empty())
+    {
+      res.push(calcult(postfix.front()));
+      postfix.pop();
+    }
   }
-
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
   bool second = true;
   while (!res.empty())
   {
@@ -56,3 +77,4 @@ int main()
   std::cout << "\n";
   return 0;
 }
+
