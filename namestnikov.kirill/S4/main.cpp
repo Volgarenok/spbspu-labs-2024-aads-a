1e0df0832cac4c86082b89795fe3e4fb470a7c62
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-04-24 10:30:00 +0300

    trying simple output with map

--- namestnikov.kirill/S4/main.cpp
+++ namestnikov.kirill/S4/main.cpp
@@ -1,2 +1,14 @@
+#include <iostream>
+#include <string>
+#include <map>
+
 int main()
-{}
+{
+  std::map< char, int > letters{{'a', 27}, {'b', 3}, {'c', 1}};
+  letters['b'] = 57;
+  letters['c'] = 24;
+  for (const auto & pair : letters)
+  {
+    std::cout << "{" << pair.first << ";" << pair.second << "}";
+  }
+}
