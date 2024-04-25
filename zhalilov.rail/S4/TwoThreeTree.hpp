c4a28267ded119b26f96e59206786b71244ea357
Rail <ksovfox@mail.ru>
2024-04-25 12:27:31 +0300

    Добавлены методы для создания узлов. Начата реализация вставки

--- zhalilov.rail/S4/TwoThreeTree.hpp
+++ zhalilov.rail/S4/TwoThreeTree.hpp
@@ -3,9 +3,9 @@
 
 #include <functional>
 
-#include "node.hpp"
-#include "iterator.hpp"
 #include "const_iterator.hpp"
+#include "iterator.hpp"
+#include "node.hpp"
 
 namespace zhalilov
 {
@@ -34,15 +34,43 @@ namespace zhalilov
     const_iterator find(const Key &) const;
 
   private:
-    detail::Node < MapPair > *head_;
+    using Node = detail::Node < MapPair >;
+    Node *head_;
     size_t size_;
+
+    void doInsert(const Key &, const T &);
+    Node *createTwoNode(Node *, const MapPair &);
+    Node *createThreeNode(Node *, const MapPair &, const MapPair &);
   };
 
   template < class Key, class T, class Compare >
-  TwoThree < Key, T, Compare >::TwoThree():
-    head_(nullptr),
-    size_(0)
+  TwoThree < Key, T, Compare >::TwoThree() :
+    head_(nullptr), size_(0)
   {}
-}
+
+  template < class Key, class T, class Compare >
+  void TwoThree < Key, T, Compare >::doInsert(const Key &key, const T &value)
+  {
+    MapPair newPair = std::make_pair(key, value);
+    if (empty())
+    {
+      head_ = createTwoNode(nullptr, MapPair());
+      Node newNode = createTwoNode(head_, newPair);
+      head_->childs[0] = newNode;
+    }
+  }
+
+  template < class Key, class T, class Compare >
+  typename TwoThree < Key, T, Compare >::Node *TwoThree < Key, T, Compare >::createTwoNode(Node *parent, const MapPair &pair)
+  {
+    return new Node{ parent, { nullptr, nullptr, nullptr }, { pair, pair }, detail::NodeType::Two };
+  }
+
+  template < class Key, class T, class Compare >
+  typename TwoThree < Key, T, Compare >::Node *TwoThree < Key, T, Compare >::createThreeNode(Node *parent, const MapPair &pair1, const MapPair &pair2)
+  {
+    return new Node{ parent, { nullptr, nullptr, nullptr }, { pair1, pair2 }, detail::NodeType::Three };
+  }
+} // namespace zhalilov
 
 #endif
