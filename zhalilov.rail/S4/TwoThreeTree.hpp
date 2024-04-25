fa72f44af0ee275b129cfa9265c436a2cdd8b2aa
Rail <ksovfox@mail.ru>
2024-04-25 11:02:38 +0300

    Добавлены объявления константных методов

--- zhalilov.rail/S4/TwoThreeTree.hpp
+++ zhalilov.rail/S4/TwoThreeTree.hpp
@@ -5,6 +5,7 @@
 
 #include "node.hpp"
 #include "iterator.hpp"
+#include "const_iterator.hpp"
 
 namespace zhalilov
 {
@@ -14,10 +15,12 @@ namespace zhalilov
   public:
     using MapPair = std::pair < Key, T >;
     using iterator = TwoThreeIterator < MapPair >;
+    using const_iterator = TwoThreeIterator < MapPair >;
+
     TwoThree();
 
     T &at(const Key &);
-    const T &at(const Key &);
+    const T &at(const Key &) const;
     T &operator[](const Key &);
     T &operator[](Key &&);
 
@@ -28,6 +31,7 @@ namespace zhalilov
     void swap();
 
     iterator find(const Key &);
+    const_iterator find(const Key &) const;
 
   private:
     detail::Node < MapPair > *head_;
