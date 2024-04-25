84b3ba95f229be41de7ff7c1030d5e714440cb3f
Rail <ksovfox@mail.ru>
2024-04-25 10:44:59 +0300

    Добавлены объявления основных методов

--- zhalilov.rail/S4/TwoThreeTree.hpp
+++ zhalilov.rail/S4/TwoThreeTree.hpp
@@ -4,6 +4,7 @@
 #include <functional>
 
 #include "node.hpp"
+#include "iterator.hpp"
 
 namespace zhalilov
 {
@@ -11,10 +12,25 @@ namespace zhalilov
   class TwoThree
   {
   public:
+    using MapPair = std::pair < Key, T >;
+    using iterator = TwoThreeIterator < MapPair >;
     TwoThree();
 
+    T &at(const Key &);
+    const T &at(const Key &);
+    T &operator[](const Key &);
+    T &operator[](Key &&);
+
+    bool empty();
+    size_t size();
+
+    void clear();
+    void swap();
+
+    iterator find(const Key &);
+
   private:
-    detail::Node < T > *head_;
+    detail::Node < MapPair > *head_;
     size_t size_;
   };
 }
