9aa749e09017e30c000eb1f0ecd6be035fe574cd
NovokhatskiyDanil <karuk.vova2005@mail.ru>
2024-04-25 12:18:21 +0300

    why

--- novokhatskiy.danil/S1/forward_list.hpp
+++ novokhatskiy.danil/S1/forward_list.hpp
@@ -449,7 +449,7 @@ namespace novokhatskiy
       }
       catch (...)
       {
-        freeNode(new_head);
+        novokhatskiy::detail::Node::freeNode(new_head);
         throw;
       }
       return new_head;
@@ -468,7 +468,7 @@ namespace novokhatskiy
       }
       catch (...)
       {
-        freeNode(new_node);
+        novokhatskiy::detail::Node::freeNode(new_node);
         throw;
       }
       return new_node;
