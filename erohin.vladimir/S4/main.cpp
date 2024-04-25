731e8c236df75002b8e075b3874ab8c8feadf5dd
WarriorVortex <erohinvova01010111@gmail.com>
2024-04-26 01:36:47 +0300

    Changed command system specification

--- erohin.vladimir/S4/main.cpp
+++ erohin.vladimir/S4/main.cpp
@@ -2,18 +2,18 @@
 #include <fstream>
 #include <map>
 #include <string>
-#include "dictionary.hpp"
+#include <functional>
 
 int main(int argc, char ** argv)
 {
-  using namespace erohin;
   if (argc != 2)
   {
     std::cerr << "Wrong CLA number\n";
     return 0;
   }
   std::fstream file(argv[1]);
-  std::map< std::string, std::map< size_t, std::string > > collection;
+  using dictionary = std::map< size_t, std::string >;
+  std::map< std::string, dictionary > context;
   std::string dict_name;
   file >> dict_name;
   while (!file.eof())
@@ -22,7 +22,7 @@ int main(int argc, char ** argv)
     std::string value;
     while (file >> key >> value)
     {
-      collection[dict_name][key] = value;
+      context[dict_name][key] = value;
     }
     file.clear();
     file >> dict_name;
@@ -32,14 +32,6 @@ int main(int argc, char ** argv)
     }
   }
   file.close();
-  std::map< std::string, void (*)(Dictionary &, const Dictionary &, const Dictionary &) > command;
+  std::map< std::string, std::function< void(dictionary &, const dictionary &, const dictionary &) > > command;
   command.empty();
-  for (auto & dict: collection)
-  {
-    for (auto & record: dict.second)
-    {
-      std::cout << record.first << ": " << record.second << ", ";
-    }
-    std::cout << "\n";
-  }
 }
