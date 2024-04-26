9d1090bb81d20c730961b5566af595dacb333de1
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-04-26 18:15:24 +0300

    input in different func

--- namestnikov.kirill/S4/main.cpp
+++ namestnikov.kirill/S4/main.cpp
@@ -52,6 +52,27 @@ std::map< size_t, std::string > makeUnion(const std::map< size_t, std::string > 
   return res;
 }
 
+void inputMaps(std::ifstream & in, std::map< std::string, std::map< size_t, std::string > > & myMap)
+{
+  std::map< size_t, std::string > tempMap;
+  std::string mapName = "";
+  in >> mapName;
+  while (!in.eof())
+  {
+    size_t keyNumber = 0;
+    std::string value = "";
+    while (in >> keyNumber >> value)
+    {
+      tempMap.insert(std::make_pair(keyNumber, value));
+    }
+    if (!in.eof())
+    {
+      in.clear();
+    }
+    myMap.insert(std::make_pair(mapName, tempMap));
+  }
+}
+
 int main(int argc, char * argv[])
 {
   std::map< std::string, std::map< size_t, std::string > > myMap;
@@ -63,23 +84,7 @@ int main(int argc, char * argv[])
       std::cerr << "Can not open the file\n";
       return 1;
     }
-    std::map< size_t, std::string > tempMap;
-    std::string mapName = "";
-    in >> mapName;
-    while (!in.eof())
-    {
-      size_t keyNumber = 0;
-      std::string value = "";
-      while (in >> keyNumber >> value)
-      {
-        tempMap.insert(std::make_pair(keyNumber, value));
-      }
-      if (!in.eof())
-      {
-        in.clear();
-      }
-      myMap.insert(std::make_pair(mapName, tempMap));
-    }
+    inputMaps(in, myMap);
     in.close();
   }
   for (const auto & pair : myMap)
