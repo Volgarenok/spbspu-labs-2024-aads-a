40fa4d44c631f987b7c55f681eae5c6698d781e5
User1235321 <pavreb1235321@gmail.com>
2024-04-28 19:10:02 +0300

    Add: begin, end, cbegin, cend, empty, size and find

--- rebdev.pavel/S4/main.cpp
+++ rebdev.pavel/S4/main.cpp
@@ -5,5 +5,8 @@
 int main()
 {
   rebdev::AVLTree< int, int > firstTree;
+  std::cout << firstTree.size();
+  std::cout << firstTree.empty();
+  auto iter = firstTree.end();
   return 0;
 }
--- rebdev.pavel/S4/myTree.hpp
+++ rebdev.pavel/S4/myTree.hpp
@@ -16,19 +16,79 @@ namespace rebdev
     using node = TreeNode< pair >;
 
     public:
-      using iteartor = BaseIter< Key, Value, Compare, pair, node >;
+      using iterator = BaseIter< Key, Value, Compare, pair, node >;
       using const_iterator = BaseIter< Key, Value, Compare, const pair, const node >;
 
       AVLTree():
-        node_(new node{nullptr, nullptr, nullptr, pair{}})
+        headNode_(new node{nullptr, nullptr, nullptr, pair{}}),
+        size_(0)
       {}
       ~AVLTree() noexcept
       {
-        delete node_;
+        delete headNode_;
+      }
+
+      iterator begin()
+      {
+        node * beginNode = headNode_;
+        while ((beginNode->left) != (nullptr))
+        {
+          beginNode = beginNode->left;
+        }
+        return iterator(beginNode);
+      }
+      iterator end()
+      {
+        node * beginNode = headNode_;
+        while ((beginNode->right) != (nullptr))
+        {
+          beginNode = beginNode->right;
+        }
+        return iterator(beginNode);
+      }
+      const_iterator cbegin() const
+      {
+        return const_iterator(*begin());
+      }
+      const_iterator cend() const
+      {
+        return const_iterator(*end());
+      }
+
+      bool empty() const noexcept
+      {
+        return (size_ == 0);
+      }
+      size_t size() const noexcept
+      {
+        return size_;
+      }
+
+      iterator find(const Key & k)
+      {
+        return iterator(findBase(k));
+      }
+      const_iterator find(const Key & k) const
+      {
+        return const_iterator(findBase(k));
       }
 
     private:
-      node * node_;
+      node * headNode_;
+      size_t size_;
+
+      node * findBase(const Key & k)
+      {
+        iterator it = begin();
+        while (it != end())
+        {
+          if ((*it.first) == k)
+          {
+            return &it;
+          }
+        }
+        return nullptr;
+      }
   };
 }
 
