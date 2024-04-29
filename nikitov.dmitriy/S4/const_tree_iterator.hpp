0cf1c190eb10c775fd633dee0911a4f052d35b6c
Dmitriy <Dmitry5335@yandex.ru>
2024-04-29 20:39:26 +0300

    Const iterator declaration and default constructor for it

---
+++ nikitov.dmitriy/S4/const_tree_iterator.hpp
@@ -0,0 +1,28 @@
+#ifndef CONST_TREE_ITERATOR_HPP
+#define CONST_TREE_ITERATOR_HPP
+
+#include "tree_node.hpp"
+
+namespace nikitov
+{
+  template< class Key, class T, class Compare >
+  class Tree;
+
+  template< class Key, class T, class Compare = std::less< Key > >
+  class ConstTreeIterator
+  {
+    friend class Tree< Key, T, Compare >;
+  public:
+    explicit ConstTreeIterator(TreeNode< Key, T, Compare >* node);
+    ~ConstTreeIterator() = default;
+
+  private:
+    TreeNode< Key, T, Compare >* node_;
+  };
+
+  template< class Key, class T, class Compare >
+  ConstTreeIterator< Key, T, Compare >::ConstTreeIterator(TreeNode< Key, T, Compare >* node):
+    node_(node)
+  {}
+}
+#endif
