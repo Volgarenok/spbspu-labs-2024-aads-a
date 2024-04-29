79ca513716147a2431f8e1a5711f2db24f62bea5
Dmitriy <Dmitry5335@yandex.ru>
2024-04-29 14:01:31 +0300

    Tree node forward constructor and destructor

--- nikitov.dmitriy/S4/tree.hpp
+++ nikitov.dmitriy/S4/tree.hpp
@@ -3,9 +3,14 @@
 
 namespace nikitov
 {
-  template< class T >
+  template< class T, class Compare >
   class Tree
   {
+  public:
+    template< class... Args >
+    Tree(Args&&... args);
+    ~Tree() = default;
+
   private:
     T firstValue_;
     T secondValue_;
@@ -14,5 +19,16 @@ namespace nikitov
     Tree< T >* right_;
     Tree< T >* parent_;
   };
+
+  template< class T, class Compare >
+  template< class... Args >
+  Tree< T, Compare >::Tree(Args&&... args):
+    firstValue_(std::forward< Args >(args)...),
+    secondValue_(T()),
+    left_(nullptr),
+    middle_(nullptr),
+    right_(nullptr),
+    parent_(nullptr)
+  {}
 }
 #endif
