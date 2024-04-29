2406377bbbb636409ac90364b528f9f1fe9e52d6
Dmitriy <Dmitry5335@yandex.ru>
2024-04-29 15:17:41 +0300

    Tree declaration and default constructor

--- nikitov.dmitriy/S4/binary_search_tree.hpp
+++ nikitov.dmitriy/S4/binary_search_tree.hpp
@@ -1,12 +1,30 @@
 #ifndef BINARY_SEARCH_TREE_HPP
 #define BINARY_SEARCH_TREE_HPP
 
+#include "tree.hpp"
+
 namespace nikitov
 {
-  template< class Key, class Value, class Compare >
+  template< class Key, class T, class Compare >
   class BinarySearchTree
   {
+  public:
+    BinarySearchTree();
+    ~BinarySearchTree();
 
+    void insert(Key& key, T& value);
+
+  private:
+    Tree< Key, T, Compare >* root_;
+    size_t size_;
+    Compare cmp_;
   };
+
+  template< class Key, class T, class Compare >
+  BinarySearchTree< Key, T, Compare >::BinarySearchTree():
+    root_(Tree()),
+    size_(0),
+    cmp_(Compare())
+  {}
 }
 #endif
