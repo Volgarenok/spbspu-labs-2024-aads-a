0dbc16494e4927c0ec76ada0a5e3d45480405b26
Dmitriy <Dmitry5335@yandex.ru>
2024-04-29 14:28:45 +0300

    Rework tree node

--- nikitov.dmitriy/S4/tree.hpp
+++ nikitov.dmitriy/S4/tree.hpp
@@ -1,34 +1,40 @@
 #ifndef TREE_HPP
 #define TREE_HPP
 
+#include <utility>
+
 namespace nikitov
 {
-  template< class T, class Compare >
+  template< class Key, class T, class Compare >
   class Tree
   {
   public:
-    template< class... Args >
-    Tree(Args&&... args);
+    Tree(Key key, T& value, Compare cmp = std::less< Key >);
     ~Tree() = default;
 
+    void add();
+
   private:
-    T firstValue_;
-    T secondValue_;
+    std::pair< Key, T > firstValue_;
+    std::pair< Key, T > secondValue_;
     Tree< T >* left_;
     Tree< T >* middle_;
     Tree< T >* right_;
     Tree< T >* parent_;
+    size_t size_;
+    Compare cmp_;
   };
 
-  template< class T, class Compare >
-  template< class... Args >
-  Tree< T, Compare >::Tree(Args&&... args):
-    firstValue_(std::forward< Args >(args)...),
-    secondValue_(T()),
+  template< class Key, class T, class Compare >
+  Tree< Key, T, Compare >::Tree(Key key, T& value, Compare cmp = std::less< Key >):
+    firstValue_({ key, value }),
+    secondValue_({ Key(), T() }),
     left_(nullptr),
     middle_(nullptr),
     right_(nullptr),
     parent_(nullptr)
+    size_(1),
+    cmp_(cmp)
   {}
 }
 #endif
