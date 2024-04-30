aeec1ecc63ec93bdf3048cdba73530f587eee270
pashko2303 <baranov-p.b@yandex.ru>
2024-04-30 19:47:59 +0300

    Trees input

--- baranov.pavel/S4/main.cpp
+++ baranov.pavel/S4/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 #include "inputTrees.hpp"
 
 int main(int argc, char ** argv)
 {
+  using namespace baranov;
   if (argc != 2)
   {
     std::cerr << "Invalid CLA\n";
     return 1;
   }
   std::ifstream file(argv[1]);
-  std::cout << argv[1];
+  using basicTree = std::map< int, std::string >;
+  using treeOfTrees = std::map< std::string, basicTree >;
+  treeOfTrees trees;
+  baranov::inputTrees(trees, file);
 }
