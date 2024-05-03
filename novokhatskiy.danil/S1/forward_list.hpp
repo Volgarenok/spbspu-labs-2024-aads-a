9efe104e6af4382ee226ba7a0fcbcc3b06ac7e45
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-05-03 11:32:27 +0300

    i hate whitespaces plus delete in previous commit #include <iostream> in forward_list

--- novokhatskiy.danil/S1/forward_list.hpp
+++ novokhatskiy.danil/S1/forward_list.hpp
@@ -327,9 +327,9 @@ namespace novokhatskiy
     size_t remove(const T& value)
     {
       return remove_if(
-        [&value](const T& tmp) -> bool 
-        { 
-          return tmp = value; 
+        [&value](const T& tmp) -> bool
+        {
+          return tmp = value;
         }
       );
     }
