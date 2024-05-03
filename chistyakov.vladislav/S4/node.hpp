68c85199fd1cf9c75abf9e2b93c008467f98b39e
jiggerman <liggerman@mail.ru>
2024-05-03 22:46:27 +0300

    Add base constructors for node

--- chistyakov.vladislav/S4/node.hpp
+++ chistyakov.vladislav/S4/node.hpp
@@ -6,6 +6,19 @@ namespace chistyakov
   template< typename T >
   struct Node
   {
+    Node():
+      left(nullptr),
+      right(nullptr),
+      parent(nullptr)
+    {}
+
+    Node(const T & value)
+      value(value),
+      left(nullptr),
+      right(nullptr),
+      parent(nullptr)
+    {}
+
     T value;
     Node * left;
     Node * right:
