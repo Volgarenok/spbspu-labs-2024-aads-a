f1e8e211fe944f13c124c732f0baeb4948fe28c4
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-05-05 17:25:00 +0300

    Добавил специальные методы класса для node, начал работу с constIterator

--- novokhatskiy.danil/S4/AVLtree.hpp
+++ novokhatskiy.danil/S4/AVLtree.hpp
@@ -1,11 +1,55 @@
 #ifndef AVL_TREE_HPP
 #define AVL_TREE_HPP
 
+#include <functional>
+#include <cstddef>
+#include "AVLtreeNode.hpp"
+
 namespace novokhatskiy
 {
+  template< class Key, class Value, class Compare = std::less< Key > >
   class Tree
   {
+  public:
+    using node_t = detail::NodeTree< Key, Value >;
+    int getBalanceFactor(node_t* node)
+    {
+      if (node == nullptr)
+      {
+        return -1;
+      }
+      else
+      {
+        return 0;
+      }
+    }
 
+    size_t size() const
+    {
+      return size_;
+    }
+
+    bool empty() const noexcept
+    {
+      return !size_;
+    }
+
+    void clear()
+    {
+
+    }
+
+    void swap(Tree& other)
+    {
+      std::swap(root_, other.root_);
+      std::swap(size_, other.size_);
+      std::swap(cmp_, other.cmp_);
+    }
+
+  private:
+    node_t* root_;
+    size_t size_;
+    Compare cmp_;
   };
 }
 
---
+++ novokhatskiy.danil/S4/constIteratorTree.hpp
@@ -0,0 +1,87 @@
+#ifndef CONST_ITERATOR_AVL
+#define CONST_ITERATOR_AVL
+
+#include <cassert>
+#include <iterator>
+#include <functional>
+#include <utility>
+#include "AVLtreeNode.hpp"
+
+
+namespace novokhatskiy
+{
+  template< class Key, class Value, class Compare >
+  class Tree;
+
+  template< class Key, class Value, class Compare  = std::less< Key > >
+  struct ConstIteratorTree : public std::iterator< std::bidirectional_iterator_tag, Value >
+  {
+    friend class Tree< Key, Value,Compare >;
+    using node_t = detail::NodeTree< Key, Value >;
+    using constIter = ConstIteratorTree< Key, Value, Compare >;
+
+    ConstIteratorTree(node_t* node):
+      node_(node)
+    {}
+    constIter& operator=(const constIter& other) = default;
+
+    bool operator!=(const constIter& other) const
+    {
+      assert(node_ != nullptr);
+      return !(*this == other);
+    }
+
+    bool operator==(const constIter& other) const
+    {
+      assert(node_ != nullptr);
+      return *this == other;
+    }
+
+    constIter& operator++()
+    {
+
+    }
+
+    constIter& operator--()
+    {
+
+    }
+
+    const std::pair< Key, Value > operator*() const
+    {
+      assert(node_ != nullptr);
+      return node_->value;
+    }
+
+    const std::pair< Key, Value >* operator->() const
+    {
+      assert(node_ != nullptr);
+      return std::addressof(node_->value);
+    }
+
+    void goLastRight()
+    {
+      auto tmp = node_->right;
+      while (tmp->right)
+      {
+        tmp = tmp->right;
+      }
+      node_ = tmp;
+    }
+
+    void goLastLeft()
+    {
+      auto tmp = node_->left;
+      while (tmp->left)
+      {
+        tmp = tmp->left;
+      }
+      node_ = tmp;
+    }
+
+  private:
+    node_t* node_;
+  };
+}
+
+#endif
