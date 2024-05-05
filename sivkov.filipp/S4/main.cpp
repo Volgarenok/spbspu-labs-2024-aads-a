7633381cad3bec5796b269269b9bb9ce1f03b68a
exxiiint <filipp.sivkov@yandex.ru>
2024-05-05 19:49:40 +0300

    edit main

--- sivkov.filipp/S4/main.cpp
+++ sivkov.filipp/S4/main.cpp
@@ -5,129 +5,9 @@
 #include <map>
 #include <stdexcept>
 #include <functional>
-#include <limits>
 
-namespace sivkov
-{
-  void inputDictionary(std::map < std::string, std::map< size_t, std::string > >& treeOfdic, std::istream& in)
-  {
-    while (!in.eof())
-    {
-      in.clear();
-      std::string name = "";
-      std::map< size_t, std::string > dictionary;
-      size_t key = 0;
-      in >> name;
-      while (in >> key)
-      {
-        std::string word = "";
-        in >> word;
-        dictionary[key] = word;
-      }
-      treeOfdic[name] = dictionary;
-    }
-  }
-}
-
-
-namespace sivkov
-{
-
-  void print(std::map < std::string, std::map< size_t, std::string > >& treeOfdic, std::istream& in, std::ostream& out)
-  {
-    std::string dataset = "";
-    in >> dataset;
-    auto found = treeOfdic.find(dataset);
-    if (found != treeOfdic.end())
-    {
-      out << dataset;
-      for (auto it = found->second.begin(); it != found->second.end(); ++it)
-      {
-        out << " " << it->first << " " << it->second;
-      }
-
-    }
-    else
-    {
-      throw std::logic_error("<EMPTY>");
-    }
-  }
-
-  void complement(std::map<std::string, std::map<size_t, std::string>>& treeOfdic, std::istream& in)
-  {
-    std::string newDataset, dataset1, dataset2;
-    in >> newDataset >> dataset1 >> dataset2;
-
-    auto found1 = treeOfdic.find(dataset1);
-    auto found2 = treeOfdic.find(dataset2);
-
-    if (found1 != treeOfdic.end() && found2 != treeOfdic.end())
-    {
-      std::map<size_t, std::string> complementDict;
-      for (auto it = found1->second.begin(); it != found1->second.end(); ++it)
-      {
-        complementDict[it->first] = it->second;
-      }
-      for (auto it = found2->second.begin(); it != found2->second.end(); ++it)
-      {
-        auto iter = complementDict.find(it->first);
-        if (iter != complementDict.end())
-        {
-          complementDict.erase(iter);
-        }
-      }
-      treeOfdic[newDataset] = complementDict;
-    }
-  }
-
-
-
-  void intersect(std::map<std::string, std::map<size_t, std::string>>& treeOfdic, std::istream& in)
-  {
-    std::string newDataset, dataset1, dataset2;
-    in >> newDataset >> dataset1 >> dataset2;
-
-    auto found1 = treeOfdic.find(dataset1);
-    auto found2 = treeOfdic.find(dataset2);
-
-    if (found1 != treeOfdic.end() && found2 != treeOfdic.end())
-    {
-      std::map<size_t, std::string> intersectDict;
-      for (auto it = found1->second.begin(); it != found1->second.end(); ++it)
-      {
-        auto it2 = found2->second.find(it->first);
-        if (it2 != found2->second.end())
-        {
-          intersectDict[it->first] = it->second;
-        }
-      }
-      treeOfdic[newDataset] = intersectDict;
-    }
-  }
-
-  void uni(std::map<std::string, std::map<size_t, std::string>>& treeOfdic, std::istream& in)
-  {
-    std::string newDataset, dataset1, dataset2;
-    in >> newDataset >> dataset1 >> dataset2;
-
-    auto found1 = treeOfdic.find(dataset1);
-    auto found2 = treeOfdic.find(dataset2);
-
-    if (found1 != treeOfdic.end() && found2 != treeOfdic.end())
-    {
-      std::map<size_t, std::string> unionDict;
-      for (auto it = found1->second.begin(); it != found1->second.end(); ++it)
-      {
-        unionDict[it->first] = it->second;
-      }
-      for (auto it = found2->second.begin(); it != found2->second.end(); ++it)
-      {
-        unionDict[it->first] = it->second;
-      }
-      treeOfdic[newDataset] = unionDict;
-    }
-  }
-}
+#include "cmd.hpp"
+#include "input.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -163,7 +43,7 @@ int main(int argc, char* argv[])
       {
         std::cerr << e.what() << "\n";
         std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
       }
     }
     else
