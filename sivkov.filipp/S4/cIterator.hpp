e3238dcd56d9f241cada43b31b218ab69bd12124
exxiiint <filipp.sivkov@yandex.ru>
2024-05-06 18:51:54 +0300

    add new cIterators

--- sivkov.filipp/S4/cIterator.hpp
+++ sivkov.filipp/S4/cIterator.hpp
@@ -2,74 +2,79 @@
 #define CITERATOR_HPP
 
 #include "AVLTree.hpp"
-
-template<typename Key, typename Value, typename Comp>
-class ConstIterator
+namespace sivkov
 {
-  friend class AVLTree;
-public:
-  using Node = TreeNode<Key, Value>;
-
-  ConstIterator(Node* node):
-    current_(node)
-  {}
-
-  const std::pair<const Key, Value>& operator*() const
+  template<typename Key, typename Value, typename Comp>
+  class ConstIterator
   {
-    return current_->data;
-  }
+  public:
+    using Node = detail::TreeNode<Key, Value>;
 
-  const std::pair<const Key, Value>* operator->() const
-  {
-    return &(current_->data);
-  }
+    ConstIterator(Node* node):
+      current_(node)
+    {}
 
-  ConstIterator& operator++()
-  {
-    if (current_ == nullptr)
+    std::pair<Key, Value>& operator*() const
     {
+      return current_->data;
+    }
+
+    std::pair<Key, Value>* operator->() const
+    {
+      return &(current_->data);
+    }
+
+    ConstIterator& operator++()
+    {
+      if (current_ == nullptr)
+      {
+        return *this;
+      }
+      if (current_->right != nullptr)
+      {
+        current_ = current_->right;
+        while (current_->left != nullptr)
+        {
+          current_ = current_->left;
+        }
+      }
+      else
+      {
+        Node* prev;
+        do
+        {
+          prev = current_;
+          current_ = current_->parent;
+        } while (current_ != nullptr && prev == current_->right);
+      }
       return *this;
     }
-    if (current_->right != nullptr)
+
+    ConstIterator operator++(int)
     {
-      current_ = current_->right;
-      while (current_->left != nullptr)
-      {
-        current_ = current_->left;
-      }
+      ConstIterator tmp = *this;
+      ++(*this);
+      return tmp;
     }
-    else
+
+    bool operator==(const ConstIterator& other) const
     {
-      Node* prev;
-      do
-      {
-        prev = current_;
-        current_ = current_->parent;
-      } while (current_ != nullptr && prev == current_->right);
+      return current_ == other.current_;
     }
-    return *this;
-  }
 
-  ConstIterator operator++(int)
-  {
-    ConstIterator tmp = *this;
-    ++(*this);
-    return tmp;
-  }
+    bool operator!=(const ConstIterator& other) const
+    {
+      return !(*this == other);
+    }
 
-  bool operator==(const ConstIterator& other) const
-  {
-    return current_ == other.current_;
-  }
+    Node* getCurrent() const
+    {
+      return current_;
+    }
 
-  bool operator!=(const ConstIterator& other) const
-  {
-    return !(*this == other);
-  }
-
-private:
-  Node* current_;
-};
-
+  private:
+    Node* current_;
+  };
+}
 #endif
 
