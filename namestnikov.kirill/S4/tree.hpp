5b3c016f438fa2bba346c82d6e97a4fcc269c881
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-07 17:39:15 +0300

    new methods

--- namestnikov.kirill/S4/tree.hpp
+++ namestnikov.kirill/S4/tree.hpp
@@ -20,6 +20,124 @@ namespace namestnikov
       head_(nullptr),
       size_(0),
     {}
+    Tree(const Tree & other):
+      root_(other.root_),
+      size_(other.size_),
+      compare_(other.compare_)
+    {
+      other.root_ = nullptr;
+      other.size_ = 0;
+    }
+    Tree & operator=(const Tree & other)
+    {
+      if (this != std::addressof(other))
+      {
+        Tree< Key, Value, Compare > temp(other);
+        swap(temp);
+      }
+      return *this;
+    }
+    Tree & operator=(Tree && other)
+    {
+      if (this != std::addressof(other))
+      {
+        swap(other);
+      }
+      return *this;
+    }
+    void rotate_left(node_t * node)
+    {
+      node_t * newRoot = node->right;
+      node_t * temp = newRoot->left;
+      node->right = temp;
+      newRoot->left = node;
+      updateHeight(newRoot);
+      updateHeight(node);
+      return newRoot; 
+    }
+    void * rotate_right(node_t * node)
+    {
+      node_t * newRoot = node->left;
+      node_t * temp = newRoot->right;
+      node->left = temp;
+      newRoot->right = node;
+      updateHeight(node);
+      updateHeight(newRoot);
+    }
+    void print_impl(node_t * root)
+    {
+      if (root->left != nullptr)
+      {
+        print_impl(root->left);
+      }
+      std::cout << " " << root->data.second << " ";
+      if (root->right != nullptr)
+      {
+        print_impl(root->right);
+      }
+    }
+    void print()
+    {
+      print_impl(root_);
+    }
+    void balance(node_t * node)
+    {
+      fixHeight(node);
+      if (getBalance(node) == 2)
+      {
+        if (getBalance(node->right) < 0)
+        {
+          node->right = rotate_right(node->right);
+        }
+        rotate_left(node);
+      }
+      else if (getBalance(node) == -2)
+      {
+        if (getBalance(node->left) > 0)
+        {
+          node->left = rotate_left(node->left);
+        }
+        rotate_right(node);
+      }
+    }
+    node_t * insert(const pair_key_t & value, node_t * root)
+    {
+      if (root == nullptr)
+      {
+        return new node_t(value);
+      }
+      if (root->data.second < value.second)
+      {
+        root->right = insert(value, root->right);
+      }
+      else if (root->data.second == value.second)
+      {
+        return balance(root);
+      }
+      else
+      {
+        root->left = insert(value, root->left);
+      }
+      return balance(root);
+    }
+    void fixHeight(node_t * node)
+    {
+      int rightHeight = getHeight(node->right);
+      int leftHeight = getHeight(node->left);
+      node->height = std::max(rightHeight, leftHeight) + 1;
+    }
+    int getHeight(node_t * root)
+    {
+      return root ? (root->height) : 0;
+    }
+    void updateHeight(node_t * node)
+    {
+      node->height = 1 + std::max(node->getHeight(node->left), node->getHeight(node->right));
+    }
+    int getBalance(node_t * node)
+    {
+      return getHeight(node->right) - getHeight(node->left);
+    }
     bool empty() const noexcept
     {
       return (size_ == 0);
@@ -28,10 +146,30 @@ namespace namestnikov
     {
       return size_;
     }
+    void swap(Tree & other)
+    {
+      std::swap(root_, other.root_);
+      std::swap(size_, other.size_);
+      std::swap(compare_, other.compare_);
+    }
   private:
     node_t * root_;
     size_t size_;
     Compare compare_;
+    void clear()
+    {
+      clear(root_);
+      root_ = nullptr;
+    }
+    void clear(node_t * node)
+    {
+      if (node)
+      {
+        clear(node->left);
+        delete node;
+        clear(node->right);
+      }
+    }
   };
 }
 
