6e8c582e9ce5f4e397b35d77428f0ffcc747ec3f
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-05-08 13:28:59 +0300

    огузок

--- novokhatskiy.danil/S4/main.cpp
+++ novokhatskiy.danil/S4/main.cpp
@@ -211,7 +211,7 @@ int main(int argc, char** argv)
     }
     catch (const std::out_of_range&)
     {
-      std::cout << "<INVALID ARGUMENT>\n";
+      std::cout << "<INVALID COMMAND>\n";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
