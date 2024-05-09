1093077d52b84a1d47e6f4a2c8135cbed606f191
NovokhatskiyDanil <karuk.vova2005@mail.ru>
2024-05-09 20:12:52 +0300

    at, left,right

--- novokhatskiy.danil/S4/AVLtree.hpp
+++ novokhatskiy.danil/S4/AVLtree.hpp
@@ -4,17 +4,18 @@
 #include <functional>
 #include <cstddef>
 #include <iostream>
+#include <stdexcept>
 #include <utility>
 #include "AVLtreeNode.hpp"
 
 namespace novokhatskiy
 {
-  template< class Key, class Value, class Compare = std::less< Key > >
+  template <class Key, class Value, class Compare = std::less<Key>>
   class Tree
   {
   public:
-    using node_t = detail::NodeTree< Key, Value >;
-    using v_type = std::pair< Key, Value >;
+    using node_t = detail::NodeTree<Key, Value>;
+    using v_type = std::pair<Key, Value>;
     /*int getBalanceFactor(node_t* node)
     {
       if (node == nullptr)
@@ -27,26 +28,25 @@ namespace novokhatskiy
       }
     }*/
 
-    Tree(const Tree& other):
-      root_(other.root_),
-      size_(other.size_),
-      cmp_(other.cmp_)
+    Tree(const Tree &other) : root_(other.root_),
+                              size_(other.size_),
+                              cmp_(other.cmp_)
     {
       other.root_ = nullptr;
       other.size_ = 0;
     }
 
-    Tree& operator=(const Tree& other)
+    Tree &operator=(const Tree &other)
     {
       if (this != std::addressof(other))
       {
-        Tree< Key, Value > tmp(other);
+        Tree<Key, Value> tmp(other);
         swap(tmp);
       }
       return *this;
     }
 
-    Tree& operator=(Tree&& other)
+    Tree &operator=(Tree &&other)
     {
       if (this != std::addressof(other))
       {
@@ -55,10 +55,10 @@ namespace novokhatskiy
       return *this;
     }
 
-    node_t* rotate_left()
+    node_t *rotate_left()
     {
-      node_t* currRoot = this;
-      node_t* newRoot = currRoot->right;
+      node_t *currRoot = this;
+      node_t *newRoot = currRoot->right;
       currRoot->right = newRoot->left;
       /*node_t* newRoot = node->right;
       node_t* tmp = newRoot->left;
@@ -73,21 +73,75 @@ namespace novokhatskiy
       newRoot->right = node;
       node->parent = newRoot;
       node->right->parent = node;*/
-
     }
 
-    node_t* rotate_right(node_t* node)
+    node_t *find(node_t *node, const Key &key)
     {
-      node_t* newRoot = node->left;
-      node_t* tmp = newRoot->right;
+      node_t *root = (node->height < 0) ? node->left : node;
+      while (root)
+      {
+        if (key == root->value.first)
+        {
+          return root;
+        }
+        if (cmp_(key, root->value.first))
+        {
+          if (!root->left)
+          {
+            return root;
+          }
+          root = root->left;
+        }
+        else
+        {
+          if (!root->right)
+          {
+            return root;
+          }
+          root = root->right;
+        }
+      }
+      return nullptr;
+    }
+
+    Value &at(const Key &key)
+    {
+      node_t *curr = find(root_->left, key);
+      if (curr && curr->value.first == key)
+      {
+        return curr->value.second;
+      }
+      throw std::out_of_range("No such element");
+    }
+
+    const Value &at(const Key &key) const
+    {
+      node_t *curr = find(root_->left, key);
+      if (curr && curr->value.first == key)
+      {
+        return curr->value.second;
+      }
+      throw std::out_of_range("No such element");
+    }
+
+    Value &operator[](const Key &key)
+    {
+    }
+
+    node_t *rotate_right(node_t *node)
+    {
+      node_t *newRoot = node->left;
+      node_t *tmp = newRoot->right;
       node->left = tmp;
       newRoot->right = node;
       node = updateHeight(node);
+      // fixedHeight
       newRoot = updateHeight(newRoot);
+      // fixheight
       return newRoot;
     }
 
-    void prePrint(node_t* root)
+    void prePrint(node_t *root)
     {
       if (root->left != nullptr)
       {
@@ -105,7 +159,7 @@ namespace novokhatskiy
       prePrint(root_);
     }
 
-    node_t* balance(node_t* node)
+    node_t *balance(node_t *node)
     {
       fixedHeight(node);
       if (getBalanceFactor(node) == 2)
@@ -127,32 +181,32 @@ namespace novokhatskiy
       return node;
     }
 
-    void fixedHeight(node_t* node)
+    void fixedHeight(node_t *node)
     {
       int rightH = getHeight(node->right);
       int leftH = getHeight(node->left);
       node->height = (rightH > leftH ? rightH : leftH) + 1;
     }
 
-    int getHeight(node_t* root)
+    int getHeight(node_t *root)
     {
       return root ? root->height : 0;
     }
 
-    int getBalanceFactor(node_t* node)
+    int getBalanceFactor(node_t *node)
     {
       return getHeight(node->right) - getHeight(node->left);
     }
 
-    node_t* updateHeight(node_t* node)
+    node_t *updateHeight(node_t *node)
     {
       node->height = 1 + std::max(node->getHeight(node->left), node->getHeight(node->right));
       return node;
     }
 
-    void insert(const std::pair< Key, Value >& value)
+    void insert(const std::pair<Key, Value> &value)
     {
-      node_t* curr = this;
+      node_t *curr = this;
       if (curr == nullptr)
       {
         curr = new node_t(value);
@@ -185,10 +239,9 @@ namespace novokhatskiy
 
     void clear()
     {
-
     }
 
-    void swap(Tree& other)
+    void swap(Tree &other)
     {
       std::swap(root_, other.root_);
       std::swap(size_, other.size_);
@@ -196,7 +249,7 @@ namespace novokhatskiy
     }
 
   private:
-    node_t* root_;
+    node_t *root_;
     size_t size_;
     Compare cmp_;
   };
