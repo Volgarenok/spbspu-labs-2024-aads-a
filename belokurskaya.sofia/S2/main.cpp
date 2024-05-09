c53b9459d66fcb97e5601a2094628afffd3b08e0
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-05-09 22:07:13 +0300

    added reading from file

--- belokurskaya.sofia/S2/main.cpp
+++ belokurskaya.sofia/S2/main.cpp
@@ -1,12 +1,29 @@
 ﻿#include <iostream>
+#include <fstream>
 
 #include "stack.hpp"
 #include "queue.hpp"
 #include "evaluatePostfixExpression.hpp"
 #include "infixToPostfix.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
   using namespace belokurskaya;
+  if (argc < 2)
+  {
+    std::cerr << "Invalid command line args\n";
+    return 1;
+  }
+
+  const std::string inputFileName = argv[1];
+  std::ifstream inputFile(inputFileName);
+  if (!inputFile.is_open())
+  {
+    std::cerr << "Unable to open input file\n";
+    return 1;
+  }
+
+  std::string line;
+  
   return 0;
 }
