9f2f73344b6a9979d6143ccb99a22f9b1c2aac9d
exxiiint <filipp.sivkov@yandex.ru>
2024-05-11 14:24:05 +0300

    fix cg

--- sivkov.filipp/S4/main.cpp
+++ sivkov.filipp/S4/main.cpp
@@ -43,14 +43,14 @@ int main(int argc, char* argv[])
       }
       catch (const std::logic_error& e)
       {
-        std::cerr << e.what() << "\n";
+        std::cout << e.what() << "\n";
         std::cin.clear();
         std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
       }
     }
     else
     {
-      std::cerr << "<INVALID COMMAND>\n";
+      std::cout << "<INVALID COMMAND>\n";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
