4f71ddf86cd22a8f9e47554762db36909765d9e2
IastWish <antony_lebedevv@mail.ru>
2024-05-11 14:51:28 +0300

    Update push methods

--- lebedev.anton/S1/list.hpp
+++ lebedev.anton/S1/list.hpp
@@ -2,8 +2,9 @@
 #define LIST_HPP
 
 #include <iostream>
-#include "list_iterator.hpp"
+#include <utility>
 #include "const_list_iterator.hpp"
+#include "list_iterator.hpp"
 #include "node.hpp"
 
 namespace lebedev
@@ -17,15 +18,17 @@ namespace lebedev
 
     List();
     ~List();
-    
+
     void push_back(const T & val);
+    void push_back(T && val);
     void push_front(const T & val);
+    void push_front(T && val);
 
     void pop_back();
     void pop_front();
 
-    bool empty() const;
-    void clear();
+    bool empty() const noexcept;
+    void clear() noexcept;
     void swap(List< T > & list);
 
     T & front();
@@ -86,9 +89,41 @@ namespace lebedev
     }
   }
   template<class T >
+  void List< T >::push_back(T && val)
+  {
+    detail::Node< T > * node = new detail::Node< T >(std::move(val), nullptr, tail_);
+    if ((head_ == nullptr) && (tail_ == nullptr))
+    {
+      head_ = node;
+      tail_ = node;
+    }
+    else
+    {
+      node->prev_ = tail_;
+      tail_->next_ = node;
+      tail_ = node;
+    }
+  }
+  template<class T >
   void List< T >::push_front(const T & val)
   {
     detail::Node< T > * node = new detail::Node< T >(val, head_, nullptr);
+    if ((head_ == nullptr) && (tail_ == nullptr))
+    {
+      head_ = node;
+      tail_ = node;
+    }
+    else
+    {
+      node->next_ = head_;
+      head_->prev_ = node;
+      head_ = node;
+    }
+  }
+  template<class T >
+  void List< T >::push_front(T && val)
+  {
+    detail::Node< T > * node = new detail::Node< T >(std::move(val), head_, nullptr);
     if ((head_ == nullptr) && (tail_ == nullptr))
     {
       head_ = node;
@@ -142,12 +177,12 @@ namespace lebedev
   }
 
   template<class T >
-  bool List< T >::empty() const
+  bool List< T >::empty() const noexcept
   {
     return head_ == nullptr;
   }
   template<class T >
-  void List< T >::clear()
+  void List< T >::clear() noexcept
   {
     while (tail_)
     {
