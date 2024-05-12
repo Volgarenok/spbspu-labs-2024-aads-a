f736ee3a60aff761c4c660326e9adc38cdb36b13
Piyavva <piyavkin05@bk.ru>
2024-05-12 16:50:41 +0300

    Add simple functor

--- piyavkin.anton/S5/main.cpp
+++ piyavkin.anton/S5/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <tree.hpp>
 
+struct Sumkey
+{
+  void operator()(const std::pair< int, std::string >& val)
+  {
+    str += val.second;
+    key += val.first;
+  }
+  std::string str;
+  long long int key;
+};
+
+
 int main()
 {
   using namespace piyavkin;
-  Tree< int, int > tree({{1,1}, {2,2}, {3,3}});
+  Tree< int, std::string > tree({{1,"a"}, {2,"b"}, {3,"c"}, {4, "d"}, {-3, "e"}, {0, "f"}, {6, "g"}, {5, "h"}, {-10, "i"}});
   for (auto i = tree.clrbegin(); i != tree.clrend(); ++i)
   {
     std::cout << (*i).second << ' ';
   }
+  std::cout << tree.traverse_lnr(Sumkey()).key << ' ' << tree.traverse_lnr(Sumkey()).str;
+  std::cout << tree.traverse_rnl(Sumkey()).key << ' ' << tree.traverse_rnl(Sumkey()).str;
 }
