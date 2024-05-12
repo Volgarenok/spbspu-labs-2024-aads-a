465f16d43e7828059671a969f639ca797287c306
deadshumz <shumzz@bk.ru>
2024-05-13 00:00:28 +0300

    Improve List methods, add tail

--- petuhov.kirill/S1/list.hpp
+++ petuhov.kirill/S1/list.hpp
@@ -2,6 +2,7 @@
 #define LIST_HPP
 
 #include "node.hpp"
+#include "iterator.hpp"
 
 namespace petuhov
 {
@@ -16,10 +17,15 @@ namespace petuhov
     void pop_front();
     void clear();
     void swap(List &other);
+
+    Iterator< T > begin() noexcept;
+    Iterator< T > end() noexcept;
+
     ~List();
 
     private:
       Node* head_;
+      Node* tail_;
   };
 
   template < typename T >
@@ -48,7 +54,12 @@ namespace petuhov
   template < typename T >
   void List< T >::push_front(const T &value)
   {
+    Node< T > *new_node = new Node< T >(value);
     head_ = new Node< T >(value, head_);
+    if (tail_ == nullptr)
+    {
+      tail_ = new_node;
+    }
   }
 
   template < typename T >
@@ -56,6 +67,10 @@ namespace petuhov
   {
     Node< T > *temp = head_;
     head_ = head_->next_;
+    if (head_ == nullptr)
+    {
+      tail_ = nullptr;
+    }
     delete temp;
   }
 
@@ -66,18 +81,32 @@ namespace petuhov
     {
       pop_front();
     }
+    tail_ = nullptr;
   }
 
   template < typename T >
   void List< T >::swap(List &other)
   {
     std::swap(head_, other.head_);
+    std::swap(tail_, other.tail_);
   }
 
   template < typename T >
   List< T >::~List()
   {
     clear();
+  }
+
+  template < typename T >
+  Iterator< T > List< T >::begin() noexcept
+  {
+    return Iterator< T >(head_);
+  }
+
+  template < typename T >
+  Iterator< T > List< T >::end() noexcept
+  {
+    return Iterator< T >(tail_->next);
   }
 
 }
