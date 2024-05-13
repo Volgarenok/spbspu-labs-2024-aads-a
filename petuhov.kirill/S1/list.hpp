f32d5d506b430316eaca6c08b42019ab367f4bf5
deadshumz <shumzz@bk.ru>
2024-05-13 18:57:55 +0300

    Add fill constructor, remove, remove_if, assign

--- petuhov.kirill/S1/list.hpp
+++ petuhov.kirill/S1/list.hpp
@@ -11,6 +11,8 @@ namespace petuhov
   {
     public:
       List();
+      List(size_t count, const T& value);
+
       T &front();
       const T &front() const;
       bool empty() const;
@@ -18,6 +20,10 @@ namespace petuhov
       void pop_front();
       void clear();
       void swap(List &other);
+      void remove(const T& value);
+      template < typename Predicate >
+      void remove_if(Predicate pred);
+      void assign(size_t count, const T& value);
 
     Iterator< T > begin() noexcept;
     Iterator< T > end() noexcept;
@@ -34,6 +40,16 @@ namespace petuhov
     head_(nullptr),
     tail_(nullptr)
   {}
+
+  template < typename T >
+  List< T >::List(size_t count, const T& value): 
+    head_(nullptr), 
+    tail_(nullptr) 
+  {
+    for (size_t i = 0; i < count; ++i) {
+      push_front(value);
+    }
+  }
 
   template < typename T >
   T &List< T >::front()
@@ -91,6 +107,77 @@ namespace petuhov
   {
     std::swap(head_, other.head_);
     std::swap(tail_, other.tail_);
+  }
+
+  template < typename T >
+  void List< T >::remove(const T& value)
+  {
+    Node< T >* current = head_;
+    Node< T >* prev = nullptr;
+    while (current != nullptr)
+    {
+      if (current->value_ == value)
+      {
+        Node< T >* to_delete = current;
+        if (prev)
+        {
+          prev->next_ = current->next_;
+        } else {
+           head_ = current->next_;
+        }
+        if (current == tail_)
+        {
+          tail_ = prev;
+        }
+        current = current->next_;
+        delete to_delete;
+      }
+      else
+      {
+        prev = current;
+        current = current->next_;
+      }
+    }
+  }
+
+  template < typename T >
+  template < typename Predicate >
+  void List< T >::remove_if(Predicate pred)
+  {
+    Node< T >* current = head_;
+    Node< T >* prev = nullptr;
+    while (current != nullptr)
+    {
+      if (pred(current->value_))
+      {
+        Node< T >* to_delete = current;
+        if (prev) {
+          prev->next_ = current->next_;
+        } else {
+          head_ = current->next_;
+        }
+        if (current == tail_)
+        {
+          tail_ = prev;
+        }
+        current = current->next_;
+        delete to_delete;
+      }
+      else
+      {
+        prev = current;
+        current = current->next_;
+      }
+    }
+  }
+
+  template < typename T >
+  void List< T >::assign(size_t count, const T& value)
+  {
+    clear();
+    for (size_t i = 0; i < count; ++i) {
+      push_back(value);
+    }
   }
 
   template < typename T >
