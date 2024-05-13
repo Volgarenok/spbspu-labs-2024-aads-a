51178c367dad87dc4fb4f0080841d67a73326c01
deadshumz <shumzz@bk.ru>
2024-05-14 01:00:34 +0300

    Fix readinput

--- petuhov.kirill/S1/readinput.cpp
+++ petuhov.kirill/S1/readinput.cpp
@@ -1,31 +1,31 @@
 #include "readinput.hpp"
 #include <iostream>
+#include <string>
+#include <limits>
+#include <stdexcept>
 #include <sstream>
 #include "list.hpp"
 
-namespace petuhov
-{
-  void readInput(List< std::pair< std::string, List< int > > > &sequences, std::istream &input)
-  {
+namespace petuhov {
+  void readInput(List< std::pair< std::string, List< int > > > &sequences, std::istream &input) {
     std::string line;
-
-    while (std::getline(input, line))
-    {
-      size_t pos = line.find(' ');
-      std::string name = line.substr(0, pos);
-      List< int > nums;
-
-      if (pos != std::string::npos)
-      {
-        std::string num_string = line.substr(pos + 1);
-        std::istringstream num_stream(num_string);
-        int num;
-        while (num_stream >> num)
-        {
-          nums.push_front(num);
-        }
+    while (std::getline(input, line)) {
+      std::istringstream iss(line);
+      std::string name;
+      if (!(iss >> name)) {
+        continue;
       }
-      sequences.push_front(std::make_pair(name, nums));
+      List< int > numbers;
+      int num;
+      while (iss >> num) {
+        if (num < 0 || num > std::numeric_limits<int>::max()) {
+          throw std::overflow_error("Number out of range");
+        }
+        numbers.push_front(num);
+      }
+      numbers.reverse();
+      sequences.push_front(std::make_pair(name, numbers));
     }
+    sequences.reverse();
   }
 }
