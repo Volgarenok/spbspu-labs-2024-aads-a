c8f63e189da5365a914edf6eba896d62d090491c
marlions <marishin.lv@edu.spbstu.ru>
2024-05-13 23:39:47 +0300

    hpp

--- marishin.leonid/S2/getPostfixForm.hpp
+++ marishin.leonid/S2/getPostfixForm.hpp
@@ -8,6 +8,11 @@
 namespace marishin
 {
   void getPostfixForm(std::istream& in, Queue< std::string >& queue);
+  void getFromStack(Stack<std::string> &stack, std::string &value);
+  std::string& getPostfixFormIn(std::istream& in, std::string& result);
+  bool binary(const std::string& p);
+  bool priority(const std::string& stack, const std::string& current);
+  void operations(Stack<std::string> &stack, const std::string &str, std::string &value);
 }
 
 #endif
