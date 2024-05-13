48997fb80ef43489c586a57ea123dfd863f6c17a
deadshumz <shumzz@bk.ru>
2024-05-14 01:12:54 +0300

    Fix output

--- petuhov.kirill/S1/main.cpp
+++ petuhov.kirill/S1/main.cpp
@@ -4,55 +4,77 @@
 #include "list.hpp"
 #include "readinput.hpp"
 
-int main() {
+int main()
+{
   petuhov::List< std::pair< std::string, petuhov::List< int > > > sequences;
-  try {
+  try
+  {
     petuhov::readInput(sequences, std::cin);
-  } catch (const std::overflow_error &e) {
+  }
+  catch (const std::overflow_error& e)
+  {
     std::cerr << e.what() << "\n";
     return 1;
   }
 
-  if (sequences.empty()) {
+  if (sequences.empty())
+  {
     std::cout << "0\n";
     return 0;
   }
 
   petuhov::List< std::string > names;
-  for (petuhov::ConstIterator< std::pair< std::string, petuhov::List< int > > > it = sequences.cbegin(); it != sequences.cend(); ++it) {
+  for (petuhov::ConstIterator< std::pair< std::string, petuhov::List< int > > > it = sequences.cbegin(); it != sequences.cend(); ++it)
+  {
     names.push_front(it->first);
   }
   names.reverse();
 
-  for (petuhov::ConstIterator< std::string > it = names.cbegin(); it != names.cend(); ++it) {
-    std::cout << *it << " ";
+  bool first = true;
+  for (petuhov::ConstIterator< std::string > it = names.cbegin(); it != names.cend(); ++it)
+  {
+    if (!first) 
+    {
+      std::cout << " ";
+    }
+    first = false;
+    std::cout << *it;
   }
   std::cout << "\n";
 
   int max_length = 0;
-  for (petuhov::ConstIterator< std::pair< std::string, petuhov::List< int > > > it = sequences.cbegin(); it != sequences.cend(); ++it) {
+  for (petuhov::ConstIterator< std::pair< std::string, petuhov::List< int > > > it = sequences.cbegin(); it != sequences.cend(); ++it)
+  {
     int length = 0;
-    for (petuhov::ConstIterator< int > num_it = it->second.cbegin(); num_it != it->second.cend(); ++num_it) {
+    for (petuhov::ConstIterator< int > num_it = it->second.cbegin(); num_it != it->second.cend(); ++num_it)
+    {
       ++length;
     }
-    if (length > max_length) {
+    if (length > max_length)
+    {
       max_length = length;
     }
   }
 
   petuhov::List< petuhov::List< int > > columns;
-  for (int i = 0; i < max_length; ++i) {
+  for (int i = 0; i < max_length; ++i)
+  {
     petuhov::List< int > column;
-    for (petuhov::ConstIterator< std::pair< std::string, petuhov::List< int > > > it = sequences.cbegin(); it != sequences.cend(); ++it) {
+    for (petuhov::ConstIterator< std::pair< std::string, petuhov::List< int > > > it = sequences.cbegin(); it != sequences.cend(); ++it)
+    {
       petuhov::ConstIterator< int > num_it = it->second.cbegin();
       int index = 0;
-      while (num_it != it->second.cend() && index < i) {
+      while (num_it != it->second.cend() && index < i)
+      {
         ++num_it;
         ++index;
       }
-      if (num_it != it->second.cend() && index == i) {
+      if (num_it != it->second.cend() && index == i)
+      {
         column.push_front(*num_it);
-      } else {
+      }
+      else
+      {
         column.push_front(-1);
       }
     }
@@ -62,25 +84,42 @@ int main() {
   columns.reverse();
 
   petuhov::List< int > sums;
-  for (petuhov::ConstIterator< petuhov::List< int > > col_it = columns.cbegin(); col_it != columns.cend(); ++col_it) {
+  for (petuhov::ConstIterator< petuhov::List< int > > col_it = columns.cbegin(); col_it != columns.cend(); ++col_it)
+  {
     int sum = 0;
     bool has_numbers = false;
-    for (petuhov::ConstIterator< int > num_it = col_it->cbegin(); num_it != col_it->cend(); ++num_it) {
-      if (*num_it != -1) {
-        std::cout << *num_it << " ";
+    first = true;
+    for (petuhov::ConstIterator< int > num_it = col_it->cbegin(); num_it != col_it->cend(); ++num_it)
+    {
+      if (*num_it != -1)
+      {
+        if (!first)
+        {
+          std::cout << " ";
+        }
+        first = false;
+        std::cout << *num_it;
         sum += *num_it;
         has_numbers = true;
       }
     }
-    if (has_numbers) {
+    if (has_numbers)
+    {
       sums.push_front(sum);
     }
     std::cout << "\n";
   }
   sums.reverse();
 
-  for (petuhov::ConstIterator< int > sum_it = sums.cbegin(); sum_it != sums.cend(); ++sum_it) {
-    std::cout << *sum_it << " ";
+  first = true;
+  for (petuhov::ConstIterator< int > sum_it = sums.cbegin(); sum_it != sums.cend(); ++sum_it)
+  {
+    if (!first)
+    {
+      std::cout << " ";
+    }
+    first = false;
+    std::cout << *sum_it;
   }
   std::cout << "\n";
 
--- petuhov.kirill/S1/readinput.cpp
+++ petuhov.kirill/S1/readinput.cpp
@@ -1,32 +1,46 @@
 #include "readinput.hpp"
+
 #include <iostream>
-#include <string>
 #include <limits>
-#include <stdexcept>
 #include <sstream>
+#include <stdexcept>
+#include <string>
+
 #include "list.hpp"
 
-namespace petuhov {
-  void readInput(List< std::pair< std::string, List< int > > > &sequences, std::istream &input) {
+namespace petuhov
+{
+
+  void readInput(List< std::pair< std::string, List< int > > >& sequences, std::istream& input)
+  {
     std::string line;
-    while (std::getline(input, line)) {
+    while (std::getline(input, line))
+    {
       std::istringstream iss(line);
       std::string name;
-      if (!(iss >> name)) {
+      if (!(iss >> name))
+      {
         continue;
       }
       List< int > numbers;
       std::string num_str;
-      while (iss >> num_str) {
-        try {
+      while (iss >> num_str)
+      {
+        try
+        {
           unsigned long long num = std::stoull(num_str);
-          if (num > static_cast<unsigned long long>(std::numeric_limits<int>::max())) {
+          if (num > static_cast<unsigned long long>(std::numeric_limits< int >::max()))
+          {
             throw std::overflow_error("Number out of range");
           }
-          numbers.push_front(static_cast<int>(num));
-        } catch (const std::invalid_argument&) {
+          numbers.push_front(static_cast< int >(num));
+        }
+        catch (const std::invalid_argument&)
+        {
           throw std::overflow_error("Invalid number format");
-        } catch (const std::out_of_range&) {
+        }
+        catch (const std::out_of_range&)
+        {
           throw std::overflow_error("Number out of range");
         }
       }
@@ -35,4 +49,5 @@ namespace petuhov {
     }
     sequences.reverse();
   }
+
 }
