5272414675812b3bcc2bda8ed0d3302278a6ec48
marlions <marishin.lv@edu.spbstu.ru>
2024-05-14 00:07:02 +0300

    fix error

--- marishin.leonid/S2/getPostfixForm.cpp
+++ marishin.leonid/S2/getPostfixForm.cpp
@@ -5,68 +5,17 @@
 #include "queue.hpp"
 #include "input.hpp"
 
-void marishin::getPostfixForm(std::istream& in, Queue< std::string >& queue)
-{
-  std::string name = "";
-  while (!in.eof())
-  {
-    std::string postfix = getPostfixFormIn(in, name);
-    if (!postfix.empty())
-    {
-      queue.push(postfix);
-    }
-    name.clear();
-  }
-}
-
-void marishin::getFromStack(Stack<std::string> &stack, std::string &value)
-{
-  while (!stack.empty())
-  {
-    if (stack.top() == "(")
-    {
-      throw std::runtime_error("incorrect characters");
-    }
-    value += stack.top() + " ";
-    stack.drop();
-  }
-}
-
-std::string& marishin::getPostfixFormIn(std::istream& in, std::string& result)
-{
-  Stack<std::string> stack;
-  std::string name;
-  std::getline(in, name, '\n');
-  input_it_t it = name.cbegin();
-  while (it != name.cend())
-  {
-    std::string str;
-    it = input(it, name.cend(), str);
-    try
-    {
-      std::stoll(str);
-      result += str + " ";
-    }
-    catch (const std::invalid_argument &)
-    {
-      operations(stack, str, result);
-    }
-  }
-  getFromStack(stack, result);
-  return result;
-}
-
-bool marishin::binary(const std::string& p)
+bool binary(const std::string& p)
 {
   return (p == "+") || (p == "-") || (p == "/") || (p == "*") || (p == "%");
 }
 
-bool marishin::priority(const std::string& stack, const std::string& current)
+bool priority(const std::string& stack, const std::string& current)
 {
   return (stack != "(") && (((current == "*" || current == "/") && (stack == "*" || stack == "/")) || (current == "+" || current == "-"));
 }
 
-void marishin::operations(Stack<std::string> &stack, const std::string &str, std::string &value)
+void operations(marishin::Stack<std::string> &stack, const std::string &str, std::string &value)
 {
   if (binary(str))
   {
@@ -89,5 +38,56 @@ void marishin::operations(Stack<std::string> &stack, const std::string &str, std
       stack.drop();
     }
     stack.drop();
+  }
+}
+
+void getFromStack(marishin::Stack<std::string> &stack, std::string &value)
+{
+  while (!stack.empty())
+  {
+    if (stack.top() == "(")
+    {
+      throw std::runtime_error("incorrect characters");
+    }
+    value += stack.top() + " ";
+    stack.drop();
+  }
+}
+
+std::string& getPostfixFormIn(std::istream& in, std::string& result)
+{
+  marishin::Stack<std::string> stack;
+  std::string name;
+  std::getline(in, name, '\n');
+  input_it_t it = name.cbegin();
+  while (it != name.cend())
+  {
+    std::string str;
+    it = marishin::input(it, name.cend(), str);
+    try
+    {
+      std::stoll(str);
+      result += str + " ";
+    }
+    catch (const std::invalid_argument &)
+    {
+      operations(stack, str, result);
+    }
+  }
+  getFromStack(stack, result);
+  return result;
+}
+
+void marishin::getPostfixForm(std::istream& in, Queue< std::string >& queue)
+{
+  std::string name = "";
+  while (!in.eof())
+  {
+    std::string postfix = getPostfixFormIn(in, name);
+    if (!postfix.empty())
+    {
+      queue.push(postfix);
+    }
+    name.clear();
   }
 }
