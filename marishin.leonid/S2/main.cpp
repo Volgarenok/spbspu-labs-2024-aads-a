7aacfc5cfa6e705db4acabf13b52f281dccb8980
marlions <marishin.lv@edu.spbstu.ru>
2024-05-13 18:27:34 +0300

    main

--- marishin.leonid/S2/main.cpp
+++ marishin.leonid/S2/main.cpp
@@ -1,1 +1,30 @@
-int main(){}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+  using namespace marishin;
+  try
+  {
+    Stack< long long > result;
+    Queue< std::string > queue;
+    if (argc == 2)
+    {
+      std::ifstream in(argv[1]);
+      getPostfixFromInfix(input, queue);
+      evaluatePostfix(result, queue);
+    }
+    else
+    {
+      getPostfixFromInfix(std::cin, queue);
+      evaluatePostfix(result, queue);
+    }
+    outputResult(std::cout, result);
+    std::cout << '\n';
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+}
