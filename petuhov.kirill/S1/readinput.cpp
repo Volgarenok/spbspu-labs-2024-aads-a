f61c31a59e045205c77803e4a5e5c307d13eb5e8
deadshumz <shumzz@bk.ru>
2024-05-14 21:08:31 +0300

    Fix valgrind errors

--- petuhov.kirill/S1/readinput.cpp
+++ petuhov.kirill/S1/readinput.cpp
@@ -21,9 +21,9 @@ namespace petuhov
       }
       List< int > numbers;
       std::string num_str;
-      while (iss >> num_str)
+      try
       {
-        try
+        while (iss >> num_str)
         {
           unsigned long long num = std::stoull(num_str);
           if (num > static_cast<unsigned long long>(std::numeric_limits<int>::max()))
@@ -33,16 +33,16 @@ namespace petuhov
           }
           numbers.push_front(static_cast<int>(num));
         }
-        catch (const std::invalid_argument&)
-        {
-          overflow_occurred = true;
-          throw std::invalid_argument("Invalid number");
-        }
-        catch (const std::out_of_range&)
-        {
-          overflow_occurred = true;
-          throw std::out_of_range("Number out of range");
-        }
+      }
+      catch (const std::invalid_argument&)
+      {
+        overflow_occurred = true;
+        throw std::invalid_argument("Invalid number");
+      }
+      catch (const std::out_of_range&)
+      {
+        overflow_occurred = true;
+        throw std::out_of_range("Number out of range");
       }
       numbers.reverse();
       sequences.push_front(std::make_pair(name, numbers));
