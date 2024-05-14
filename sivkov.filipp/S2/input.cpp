7c4c8e0382c30d507ecf0037d9fb7762e1149faf
exxiiint <filipp.sivkov@yandex.ru>
2024-05-14 19:49:53 +0300

    add input

---
+++ sivkov.filipp/S2/input.cpp
@@ -0,0 +1,33 @@
+#include "input.hpp"
+#include <iostream>
+#include <string>
+#include <queue.hpp>
+
+
+namespace sivkov
+{
+  void inputString(std::istream& input, Queue< Queue< std::string > >& queueOfString)
+  {
+    std::string in = "";
+    std::string token = "";
+    while (std::getline(input, in))
+    {
+      if (in == "")
+      {
+        continue;
+      }
+      Queue< std::string > infix1;
+      for (size_t i = 0; i < in.length(); ++i)
+      {
+        token = "";
+        while (i < in.length() && !std::isspace(in[i]))
+        {
+          token += in[i];
+          ++i;
+        }
+        infix1.push(token);
+      }
+      queueOfString.push(infix1);
+    }
+  }
+}
