bdc356f83872e6a32a4b6ad358fbe54e198b2950
exxiiint <filipp.sivkov@yandex.ru>
2024-05-15 00:29:20 +0300

    add rotate to tree node

--- sivkov.filipp/common/treeNode.hpp
+++ sivkov.filipp/common/treeNode.hpp
@@ -29,11 +29,35 @@ namespace sivkov
         data(std::make_pair(key, value))
       {}
 
-      ~TreeNode() = default;
+      TreeNode< Key, Value >* rotate_right(TreeNode< Key, Value >* root)
+      {
+        TreeNode< Key, Value >* new_root = root->left;
+        root->left = new_root->right;
+        if (new_root->right != nullptr)
+        {
+          new_root->right->parent = root;
+        }
+        new_root->right = root;
+        new_root->parent = root->parent;
+        root->parent = new_root;
+        return new_root;
+      }
+
+      TreeNode< Key, Value >* rotate_left(TreeNode< Key, Value >* root)
+      {
+        TreeNode< Key, Value >* new_root = root->right;
+        root->right = new_root->left;
+        if (new_root->left != nullptr)
+        {
+          new_root->left->parent = root;
+        }
+        new_root->left = root;
+        new_root->parent = root->parent;
+        root->parent = new_root;
+        return new_root;
+      }
     };
   }
 }
 
 #endif
-
-
