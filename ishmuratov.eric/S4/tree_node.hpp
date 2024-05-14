834e46df00f93b3a6d5c6b359bc7e57581ea64c8
Eric Ishmuratov <eric@debian.local>
2024-05-14 01:14:49 -0800

    nested namespace

--- ishmuratov.eric/S4/tree_node.hpp
+++ ishmuratov.eric/S4/tree_node.hpp
@@ -1,25 +1,30 @@
 #ifndef TREENODE_HPP
 #define TREENODE_HPP
 
-namespace ishmuratov::detail
-{
-  template< class Key, class Value >
-  struct TNode
-  {
-    std::pair< Key, Value > data;
-    int height;
-    TNode * parent;
-    TNode * left;
-    TNode * right;
+#include <tuple>
 
-    TNode(const Key key, const Value value):
-      data(std::make_pair(key, value)),
-      parent(nullptr),
-      left(nullptr),
-      right(nullptr),
-      height(0)
-    {}
-  };
+namespace ishmuratov
+{
+  namespace detail
+  {
+    template<class Key, class Value>
+    struct TNode
+    {
+      std::pair<Key, Value> data;
+      int height;
+      TNode *parent;
+      TNode *left;
+      TNode *right;
+
+      TNode(const Key key, const Value value):
+        data(std::make_pair(key, value)),
+        parent(nullptr),
+        left(nullptr),
+        right(nullptr),
+        height(0)
+      {}
+    };
+  }
 }
 
 #endif
