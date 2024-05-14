33ccf57e01bed3bf88f8f282eee0035e58f5a2c5
deadshumz <shumzz@bk.ru>
2024-05-14 12:28:19 +0300

    Fix trailing spaces

--- petuhov.kirill/S1/outputsequences.cpp
+++ petuhov.kirill/S1/outputsequences.cpp
@@ -17,7 +17,7 @@ namespace petuhov
         max_length = length;
       }
     }
-  
+
     List< List< int > > columns;
     for (int i = 0; i < max_length; ++i)
     {
@@ -44,7 +44,7 @@ namespace petuhov
       columns.push_front(column);
     }
     columns.reverse();
-  
+
     for (auto col_it = columns.cbegin(); col_it != columns.cend(); ++col_it)
     {
       int sum = 0;
