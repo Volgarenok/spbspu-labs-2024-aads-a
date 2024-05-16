f8ba014c17911a01ca6e0333f0648f64859a8607
kastiela <kristinazak150@gmail.com>
2024-05-16 18:03:00 +0300

    main

--- zakozhurnikova.kristina/S6/main.cpp
+++ zakozhurnikova.kristina/S6/main.cpp
@@ -1,2 +1,29 @@
-int main()
-{}
+#include <iostream>
+#include <forward_list>
+#include <list.hpp>
+#include <deque>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+if (argc == 3)
+  {
+    try
+    {
+      std::ifstream file(argv[2]);
+      inputMap(file, map);
+    }
+    catch (...)
+    {
+      std::cerr << "Bad input\n";
+      return 1;
+    }
+  }
+  else
+  {
+    std::cerr << "No file argument";
+    return 1;
+  }
+
+
+}
