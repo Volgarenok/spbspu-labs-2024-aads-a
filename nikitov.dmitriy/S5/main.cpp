2bd67bfc1db4af70aa31fd6ad37e65a790026b37
Dmitriy <Dmitry5335@yandex.ru>
2024-05-16 20:32:26 +0300

    Add command parameters check and file reading

--- nikitov.dmitriy/S5/main.cpp
+++ nikitov.dmitriy/S5/main.cpp
@@ -1,2 +1,28 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <tree.hpp>
+
+int main(int argc, char* argv[])
+{
+  using namespace nikitov;
+
+  Tree< long long, std::string > dictionary;
+  if (argc == 2)
+  {
+    std::ifstream input(argv[2]);
+    size_t key = {};
+    while (input >> key)
+    {
+      std::string value = {};
+      input >> value;
+      dictionary.insert({ key, value });
+    }
+  }
+  else
+  {
+    std::cerr << "Error: Wrong input parameters" << '\n';
+    return 1;
+  }
+  return 0;
+}
