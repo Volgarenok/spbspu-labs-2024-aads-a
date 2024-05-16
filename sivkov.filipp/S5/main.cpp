6c593082fff071eba99129dc10ddf10087b1a333
exxiiint <filipp.sivkov@yandex.ru>
2024-05-17 00:46:45 +0300

    add main

---
+++ sivkov.filipp/S5/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <AVLTree.hpp>
+#include "input.hpp"
+#include "key_summ.hpp"
+
+int main(int argc, char* argv[])
+{
+  using namespace sivkov;
+  using avlTree = AVLTree< int, std::string >;
+  avlTree tree;
+  std::string command = "";
+  if (argc != 3)
+  {
+    std::cerr << "Error CMD line\n";
+    return 1;
+  }
+  else
+  {
+    std::ifstream file(argv[2]);
+    inputDictionary(tree, file);
+    command = argv[1];
+  }
+  using func = std::function< void(avlTree&, KeySum&)>;
+  AVLTree< std::string, func > cmd;
+  cmd.push("ascending", ascending);
+  cmd.push("descending", descending);
+  cmd.push("breadth", breadth);
+  KeySum f;
+  try
+  {
+    auto it = cmd.find(command);
+    if (it != cmd.cend())
+    {
+      it->second(tree, f);
+    }
+    else
+    {
+      throw std::out_of_range("error comand");
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what() << "\n";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+
+  int a = f.key_;
+  std::string str = f.string_;
+
+  std::cout << a << str << "\n";
+
+  return 0;
+}
