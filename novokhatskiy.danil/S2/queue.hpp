27e1075bc289ca1ed84a53922ec16c071ddb0294
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-05-17 23:08:17 +0300

    Queue and Stack

--- novokhatskiy.danil/S2/queue.hpp
+++ novokhatskiy.danil/S2/queue.hpp
@@ -6,69 +6,37 @@
 
 namespace novokhatskiy
 {
-  template < typename T >
+  template < class T >
   class Queue
   {
   public:
-    void push(const T& value);
-    bool empty() const noexcept;
-    size_t size() const noexcept;
-    void pop();
-    T& front();
-
+    void push(const T& value)
+    {
+      data_.push_back(value);
+    }
+    bool empty() const noexcept
+    {
+      return data_.empty();
+    }
+    size_t size() const noexcept
+    {
+      return data_.size();
+    }
+    void pop()
+    {
+      data_.pop_front();
+    }
+    T& front()
+    {
+      if (empty())
+      {
+        throw std::invalid_argument("Queue is empty");
+      }
+      return data_.front();
+    }
   private:
     ForwardList< T > data_;
   };
-
-  template < class T >
-  Queue< T >::Queue(Queue< T >&& other) noexcept:
-    data_(other.data_)
-  {}
-
-  template < class T >
-  T& Queue< T >::front()
-  {
-    if (empty())
-    {
-      throw std::invalid_argument("Queue is empty");
-    }
-    return data_.front();
-  }
-
-  template < class T >
-  Queue< T >& Queue< T >::operator=(Queue< T >&& other) noexcept
-  {
-    if (std::addressof(other) != this)
-    {
-      std::swap(data_, other.data_);
-      other.data_.clear();
-    }
-    return *this;
-  }
-
-  template < class T >
-  void Queue< T >::pop()
-  {
-    data_.pop_front();
-  }
-
-  template < class T >
-  void Queue< T >::push(const T& value)
-  {
-    data_.push_back(value);
-  }
-
-  template < class T >
-  bool Queue< T >::empty() const noexcept
-  {
-    return data_.empty();
-  }
-
-  template < class T >
-  size_t Queue< T >::size() const noexcept
-  {
-    return data_.size();
-  }
 }
 
 #endif
--- novokhatskiy.danil/S2/stack.hpp
+++ novokhatskiy.danil/S2/stack.hpp
@@ -10,65 +10,34 @@ namespace novokhatskiy
   class Stack
   {
   public:
-    T& top();
-    bool empty() const noexcept;
-    size_t size() const noexcept;
-    void push(const T& value);
-    void pop();
 
+    T& top()
+    {
+      if (empty())
+      {
+        throw std::invalid_argument("Stack is empty");
+      }
+      return data_.front();
+    }
+    bool empty() const noexcept
+    {
+      return data_.empty();
+    }
+    size_t size() const noexcept
+    {
+      return data_.size();
+    }
+    void push(const T& value)
+    {
+      data_.push_front(value);
+    }
+    void pop()
+    {
+      data_.pop_front();
+    }
   private:
     ForwardList< T > data_;
   };
-
-  template < class T >
-  T& Stack< T >::top()
-  {
-    if (empty())
-    {
-      throw std::invalid_argument("Stack is empty");
-    }
-    return data_.front();
-  }
-
-  template < class T >
-  bool Stack< T >::empty() const noexcept
-  {
-    return data_.empty();
-  }
-
-  template < class T >
-  size_t Stack< T >::size() const noexcept
-  {
-    return data_.size();
-  }
-
-  template < class T >
-  void Stack< T >::push(const T& value)
-  {
-    data_.push_front(value);
-  }
-
-  template < class T >
-  void Stack< T >::pop()
-  {
-    data_.pop_front();
-  }
-
-  template < class T >
-  Stack< T >::Stack(Stack< T >&& other) noexcept:
-    data_(std::move(other.data))
-  {}
-
-  template < class T >
-  Stack< T >& Stack< T >::operator=(Stack< T >&& other) noexcept
-  {
-    if (std::addressof(other) != this)
-    {
-      std::swap(data_, other.data_);
-      other.data_.clear();
-    }
-    return *this;
-  }
 }
 
 #endif
