16e04e479133d2511071f75a8846ce2e9dda75c1
Skuratov09 <skyratovdaniil28@gmail.com>
2024-05-19 01:21:11 +0300

    Implemented operator precedence and applied operation

--- skuratov.danil/S2/main.cpp
+++ skuratov.danil/S2/main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
 
-int main()
+namespace skuratov
 {
-  std::cout << "Initial commit for S2\n";
-  return 0;
+  int operatorPrecedence(char op)
+  {
+    if (op == '-' || op == '+')
+    {
+      return 1;
+    }
+    else if (op == '%' || op == '*' || op == '/')
+    {
+      return 2;
+    }
+    return 0;
+  }
+
+  int applyOperation(int a, int b, char op)
+  {
+    if (op == '+')
+    {
+      return a + b;
+    }
+    else if (op == '-')
+    {
+      return a - b;
+    }
+    else if (op == '*')
+    {
+      return a * b;
+    }
+    else if (op == '/')
+    {
+      if (b == 0)
+      {
+        throw std::runtime_error("Division by zero");
+      }
+      return a / b;
+    }
+    else if (op == '%')
+    {
+      if (b == 0)
+      {
+        throw std::runtime_error("Division by zero");
+      }
+      return a % b;
+    }
+    throw std::invalid_argument("Invalid operator");
+  }
+
+  int main()
+  {
+    std::cout << "Initial commit for S2\n";
+    return 0;
+  }
 }
