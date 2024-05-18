d5363c7f09f61f77f8cdb7f6e621791cb22a15b4
Dmitriy <Dmitry5335@yandex.ru>
2024-05-18 15:54:38 +0300

    Add commands system in main

--- nikitov.dmitriy/S5/main.cpp
+++ nikitov.dmitriy/S5/main.cpp
@@ -2,16 +2,17 @@
 #include <fstream>
 #include <string>
 #include <tree.hpp>
+#include "commands.hpp"
+#include "sum_keys.hpp"
 
 int main(int argc, char* argv[])
 {
   using namespace nikitov;
-
-  Tree< long long, std::string > dictionary;
-  if (argc == 2)
+  Tree< int, std::string > dictionary;
+  if (argc == 3)
   {
     std::ifstream input(argv[2]);
-    size_t key = {};
+    int key = {};
     while (input >> key)
     {
       std::string value = {};
@@ -23,6 +24,28 @@ int main(int argc, char* argv[])
   {
     std::cerr << "Error: Wrong input parameters" << '\n';
     return 1;
+  }
+
+  Tree< std::string, std::function< void(const Tree< int, std::string >&, SumKeys& f) > > commands;
+  commands["ascending"] = traverseAscending;
+  commands["descending"] = traverseDescending;
+  commands["breadth"] = traverseBreadth;
+
+  try
+  {
+    SumKeys f;
+    commands.at(argv[1])(dictionary, f);
+    std::cout << f.getKey() << f.getLine() << '\n';
+  }
+  catch (const std::out_of_range&)
+  {
+    std::cerr << "Error: Wrong input parameters" << '\n';
+    return 1;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 2;
   }
   return 0;
 }
