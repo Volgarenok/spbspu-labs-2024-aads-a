11de578e2824e9da3aa7c60d9c44d77e7803d80e
marlions <marishin.lv@edu.spbstu.ru>
2024-05-19 23:59:21 +0300

    struct

--- marishin.leonid/common/constTreeIterator.hpp
+++ marishin.leonid/common/constTreeIterator.hpp
@@ -7,27 +7,80 @@
 
 namespace marishin
 {
-  template < class Key, class Value, class Compare >
-  class BinarySearchTree;
+  template< typename Key, typename Value, typename Compare >
+  class BinarySearchTree:
 
-  template < class Key, class Value, class Compare = std::less< Key > >
-  struct ConstIteratorTree:
+  template< typename Key, typename Value, typename Compare = std::less< Key > >
+  struct ConstIteratorTree
   {
-    friend class BinarySearchTree< Key, Value, Compare >;
-    using this_t = ConstIteratorTree< Key, Value, Compare >;
+    friend class BinarySearchTree< Key, Value, std::less< Key > >;
     using TreeNode = detail::TreeNode< Key, Value >;
 
     ConstIteratorTree() = default;
-    this_t& operator=(const this_t&) = default;
-    this_t& operator++();
-    this_t operator++(int);
-    bool operator!=(const this_t&) const;
-    bool operator==(const this_t&) const;
-    const std::pair< Key, Value >& operator*() const;
-    const std::pair< Key, Value >* operator->() const;
+    ConstIteratorTree< Key, Value, Compare >& operator=(const ConstIteratorTree< Key, Value, Compare >&) = default;
 
-  private:
-    detail::TreeNode< Key, Value >* node_;
-    ConstIteratorTree(detail::TreeNode< Key, Value >* node);
+    template< typename Key, typename Value, typename Compare >
+    ConstIteratorTree(detail::TreeNode< Key, Value >* node):
+      node_(node)
+    {}
+
+    template< typename Key, typename Value, typename Compare >
+    const std::pair< Key, Value >& operator*() const
+    {
+      return node_->data;
+    }
+
+    template< typename Key, typename Value, typename Compare >
+    const std::pair< Key, Value >* operator->() const
+    {
+      return std::addressof(node_->data);
+    }
+
+    template< typename Key, typename Value, typename Compare >
+    ConstIteratorTree< Key, Value, Compare >& operator++()
+    {
+      if (node_->right)
+      {
+        node_ = node_->right;
+        while (node_->left)
+        {
+          node_ = node_->left;
+        }
+        return *this;
+      }
+      while (node_->parent && node_->parent->right == node_)
+      {
+        node_ = node_->parent;
+      }
+      node_ = node_->parent;
+      return *this;
+    }
+
+    template< typename Key, typename Value, typename Compare >
+    ConstIteratorTree< Key, Value, Compare >& operator++(int)
+    {
+      ConstIteratorTree< Key, Value > result(*this);
+      ++(*this);
+      return result;
+    }
+
+    template< typename Key, typename Value, typename Compare >
+    bool operator==(const ConstIteratorTree< Key, Value, Compare >& rhs) const
+    {
+      return node_ == rhs.node_;
+    }
+
+    template < typename Key, typename Value, typename Compare >
+    bool operator!=(const ConstIteratorTree< Key, Value, Compare >& rhs) const
+    {
+      return !(*this == rhs);
+    }
+
+    private:
+      detail::TreeNode< Key, Value >* node_;
+
+      ConstIterator(detail::TreeNode< Key, Value >* node):
+        node_(node)
+      {}
   };
 }
