ade1b0c414e06df846fb30b12474efe07e39bd3a
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-19 15:50:46 +0300

    Added some handling of CLA's

--- erohin.vladimir/S5/main.cpp
+++ erohin.vladimir/S5/main.cpp
@@ -1,2 +1,17 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char ** argv)
+{
+  if (argc != 3)
+  {
+    std::cerr << "Wrong CLA number";
+    return 1;
+  }
+  std::ifstream file(argv[2]);
+  if (!file)
+  {
+    std::cerr << "Error in file opening";
+  }
+  return 0;
+}
