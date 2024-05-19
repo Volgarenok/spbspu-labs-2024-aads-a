1f1aed9915e9910a1dc0666f3de64532ba2e61f6
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-19 21:32:45 +0300

    overflow checking

--- namestnikov.kirill/S5/sum_func.cpp
+++ namestnikov.kirill/S5/sum_func.cpp
@@ -1,4 +1,6 @@
 #include "sum_func.hpp"
+#include <stdexcept>
+#include <limits>
 
 namestnikov::SumFunc::SumFunc():
   result_(0),
@@ -17,6 +19,14 @@ std::string namestnikov::SumFunc::getLine() const
 
 void namestnikov::SumFunc::operator()(const std::pair< const int, std::string > & data)
 {
+  int maxNum = std::numeric_limits< int >::max();
+  int minNum = std::numeric_limits< int >::min();
+  bool isOverflow = ((result_ > 0) && (maxNum - result_ < data.first));
+  isOverflow = isOverflow || ((result_ < 0) && (data.first < 0) && (minNum - result_ > data.first));
+  if (isOverflow)
+  {
+    throw std::overflow_error("Numeric overflow");
+  }
   result_ += data.first;
   line_ = line_ + data.second + " ";
 }
