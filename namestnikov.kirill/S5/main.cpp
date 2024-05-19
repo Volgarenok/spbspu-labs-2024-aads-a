11769386c998ecd48e2b272f116f8d43e56ed559
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-19 16:50:09 +0300

    trying basic input

--- namestnikov.kirill/S5/main.cpp
+++ namestnikov.kirill/S5/main.cpp
@@ -1,2 +1,21 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <utility>
+#include <map>
+
 int main()
-{}
+{
+  std::map< int, std::string > myMap;
+  while (!std::cin.eof())
+  {
+    std::cin.clear();
+    int keyNumber = 0;
+    std::string value = "";
+    while (std::cin >> keyNumber >> value)
+    {
+      myMap.insert(std::make_pair(keyNumber, value));
+    }
+  }
+  std::cout << "|||" << myMap[10];
+}
