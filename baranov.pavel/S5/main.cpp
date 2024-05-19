5ea2014fd3521fb7ff6720a35d1bc7472f7185ad
pashko2303 <baranov-p.b@yandex.ru>
2024-05-19 19:32:19 +0300

    Input tree from file

--- baranov.pavel/S5/main.cpp
+++ baranov.pavel/S5/main.cpp
@@ -1,2 +1,25 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <map>
+#include "inputTree.hpp"
+
+int main(int argc, char * argv[])
+{
+  if (argc != 3)
+  {
+    std::cerr << "Invalid CLA\n";
+    return 1;
+  }
+  using namespace baranov;
+  std::map< int, std::string > tree;
+  try
+  {
+    std::ifstream file(argv[2]);
+    inputTree(file, tree);
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what();
+    return 1;
+  }
+}
