0945ce574ec0885cd6bb203b4ab082dec89618ea
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-19 12:18:34 +0300

    operator-- for const iterator

--- namestnikov.kirill/S5/const_tree_iterator.hpp
+++ namestnikov.kirill/S5/const_tree_iterator.hpp
@@ -57,6 +57,33 @@ namespace namestnikov
       return temp;
 
     }
+    const_iterator & operator--()
+    {
+      if (node_->left)
+      {
+        node_ = node_->left;
+        while (node_->right)
+        {
+          node_ = node_->right;
+        }
+        return *this;
+      }
+      else
+      {
+        while (node_->isLeftChild())
+        {
+          node_ = node_->parent;
+        }
+        node_ = node_->parent;
+        return *this;
+      }
+    }
+    const_iterator operator--(int)
+    {
+      const_iterator temp(*this);
+      --(*this);
+      return temp;
+    }
     bool operator==(const const_iterator & other) const
     {
       return (node_ == other.node_);
