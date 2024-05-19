0203d3674da2c6aa1495d94b0cafe3f52a19f9ee
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-19 17:01:20 +0300

    SumFunc class

--- namestnikov.kirill/S5/main.cpp
+++ namestnikov.kirill/S5/main.cpp
@@ -4,6 +4,42 @@
 #include <utility>
 #include <map>
 
+namespace namestnikov
+{
+  class SumFunc
+  {
+  public:
+    SumFunc();
+    void operator()(const std::pair< const int, std::string > & data);
+    int getResult() const;
+    std::string getLine() const;
+  private:
+    int result_;
+    std::string line_;
+  };
+}
+
+namestnikov::SumFunc::SumFunc():
+  result_(0),
+  line_("")
+{}
+
+int namestnikov::SumFunc::getResult() const
+{
+  return result_;
+}
+
+std::string namestnikov::SumFunc::getLine() const
+{
+  return line_;
+}
+
+void namestnikov::SumFunc::operator()(const std::pair< const int, std::string > & data)
+{
+  result_ += data.first;
+  line_ = line_ + data.second + " ";
+}
+
 int main()
 {
   std::map< int, std::string > myMap;
