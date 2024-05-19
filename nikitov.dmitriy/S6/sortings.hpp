37c615b22a1bb04102e3421ce6e88dff090d9775
Dmitriy <Dmitry5335@yandex.ru>
2024-05-19 19:10:08 +0300

    QSort

--- nikitov.dmitriy/S6/sortings.hpp
+++ nikitov.dmitriy/S6/sortings.hpp
@@ -8,33 +8,86 @@ namespace nikitov
   template< class BiIterator, class Compare >
   void oddEvenSort(BiIterator begin, BiIterator end, Compare cmp)
   {
+    bool isOdd = true;
+    bool isSorted = false;
+    while (!isSorted || isOdd)
     {
-      bool isOdd = true;
-      bool isSorted = false;
-      while (!isSorted || isOdd)
+      BiIterator first = begin;
+      BiIterator second = begin;
+      ++second;
+      if (!isOdd)
       {
-        BiIterator first = begin;
-        BiIterator second = begin;
+        ++first;
         ++second;
-        if (!isOdd)
-        {
-          ++first;
-          ++second;
-        }
-        isOdd = !isOdd;
-
-        isSorted = true;
-        while (second != end)
-        {
-          if (!cmp(*first, *second))
-          {
-            isSorted = false;
-            std::swap(*first, *second);
-          }
-          ++first;
-          ++second;
-        }
       }
+      isOdd = !isOdd;
+
+      isSorted = true;
+      while (second != end)
+      {
+        if (!cmp(*first, *second))
+        {
+          isSorted = false;
+          std::swap(*first, *second);
+        }
+        ++first;
+        ++second;
+      }
+    }
+  }
+
+  template< class RandIterator >
+  RandIterator findPivot(RandIterator begin, RandIterator end)
+  {
+    --end;
+    while (begin < end)
+    {
+      ++begin;
+      --end;
+    }
+    return begin;
+  }
+
+  template< class RandIterator, class Compare >
+  void QSort(RandIterator begin, RandIterator end, Compare cmp)
+  {
+    RandIterator toSwap = findPivot(begin, end);
+    RandIterator pivot = end;
+    --pivot;
+    std::swap(*toSwap, *pivot);
+
+    RandIterator first = begin;
+    RandIterator second = begin;
+    bool isFirst = true;
+    while (second != pivot)
+    {
+      if (cmp(*second, *pivot))
+      {
+        if (isFirst)
+        {
+          isFirst = false;
+        }
+        else
+        {
+          ++first;
+        }
+        std::swap(*first, *second);
+      }
+      ++second;
+    }
+
+    if (!isFirst)
+    {
+      ++first;
+    }
+    std::swap(*first, *pivot);
+    if (first != begin)
+    {
+      QSort(begin, first, cmp);
+    }
+    if (first != pivot)
+    {
+      QSort(++first, end, cmp);
     }
   }
 }
