177ad7813797f10c77cafd5a93c6cc1428540f8d
Eric Ishmuratov <eric@debian.local>
2024-05-20 21:32:49 -0800

    olo

--- ishmuratov.eric/S5/main.cpp
+++ ishmuratov.eric/S5/main.cpp
@@ -1,2 +1,24 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <tree.hpp>
+
+int main(int argc, char * argv[])
+{
+  if (argc != 3)
+  {
+    std::cerr << "Incorrect number of arguments!\n";
+    return 1;
+  }
+  std::string command = argv[1];
+  try
+  {
+    std::ifstream file(argv[2]);
+  }
+  catch (...)
+  {
+    std::cerr << "Error while reading from file!";
+    return 1;
+  }
+  std::cout << command;
+  return 0;
+}
