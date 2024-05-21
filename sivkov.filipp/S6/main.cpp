836c91b8d457307c22cdf997f9ea18982c1030bd
exxiiint <filipp.sivkov@yandex.ru>
2024-05-21 20:29:11 +0300

    add odd even sort

--- sivkov.filipp/S6/main.cpp
+++ sivkov.filipp/S6/main.cpp
@@ -1,3 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <list>
+#include <list.hpp>
+
+void oddEvenSort(ForwardIt first, ForwardIt last, Compare comp)
+{
+  if (first == last) return;
+
+  bool sorted = false;
+
+  while (!sorted)
+  {
+    sorted = true;
+    ForwardIt it = first;
+    ForwardIt next = it;
+    ++next;
+
+    while (next != last)
+    {
+      if (comp(*next, *it))
+      {
+        std::swap(*it, *next);
+        sorted = false;
+      }
+      if (next != last)
+      {
+        ++next;
+        if (next != last)
+        {
+          ++it;
+        }
+      }
+    }
+    it = first;
+    next = it;
+    while (next != last)
+    {
+      if (comp(*next, *it))
+      {
+        std::swap(*it, *next);
+        sorted = false;
+      }
+      if (next != last)
+      {
+        ++next;
+        if (next != last)
+        {
+          ++it;
+        }
+      }
+    }
+  }
+}
+
 int main()
 {
 }
