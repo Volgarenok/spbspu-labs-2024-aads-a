e08d054d54650c2ebfb62f5b82e7b5f7ecdc0e0e
IastWish <antony_lebedevv@mail.ru>
2024-05-22 01:33:56 +0300

    Expand stack interface

--- lebedev.anton/S2/main.cpp
+++ lebedev.anton/S2/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "stack.hpp"
 
 int main()
 {
-
+  using namespace lebedev;
+  Stack< int > stack;
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  stack.push(4);
+  std::cout << stack.top() << '\n';
+  stack.pop();
+  std::cout << stack.top() << '\n';
+  std::cout << stack.empty() << '\n';
+  std::cout << stack.size() << '\n';
 }
