b4166285d7d825a3019f5ca82593f2b39a7b4828
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-05-21 19:28:45 +0300

    Sortings

--- novokhatskiy.danil/S6/main.cpp
+++ novokhatskiy.danil/S6/main.cpp
@@ -1,6 +1,89 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <deque>
+#include <list>
+#include <forward_list.hpp>
+#include "sortings.hpp"
 
-int main()
+template< class T >
+void sortSeq(std::string& sort, std::string& type, size_t size, std::ostream& out)
 {
-  
+	novokhatskiy::ForwardList< T > forwardList;
+	std::list< T > biList;
+	std::deque< T > deque;
+
+	
+}
+
+int getInt()
+{
+	return std::rand() % 100;
+}
+
+float getFloat()
+{
+	return static_cast<float>(std::rand() / 1.2);
+}
+
+template< class It >
+void printSeq(It begin, It end, std::ostream& out)
+{
+	while (begin != end)
+	{
+		out << *begin;
+		begin++;
+		if (begin != end)
+		{
+			out << ' ';
+		}
+	}
+	out << '\n';
+}
+
+int main(int argc, char** argv)
+{
+  using namespace novokhatskiy;
+	try
+	{
+		std::list< int > f1;
+		for (size_t i = 0; i < 5; i++)
+		{
+			f1.push_front(getInt());
+		}
+		std::cout << "BEFORE SORT: ";
+		for (auto i = f1.cbegin(); i != f1.cend(); i++)
+		{
+			std::cout << *i << ' ';
+		}
+		std::cout << '\n' << "SHAKER SORT: ";
+		doShakerSort(f1.begin(), f1.end(), f1.size(), std::greater< int >());
+		for (auto i = f1.cbegin(); i != f1.cend(); i++)
+		{
+			std::cout << *i << ' ';
+		}
+		std::cout << '\n';
+		std::list< int > f2;
+		for (size_t i = 0; i < 5; i++)
+		{
+			f2.push_front(getInt());
+		}
+		std::cout << "BEFORE SORT: ";
+		for (auto i = f2.cbegin(); i != f2.cend(); i++)
+		{
+			std::cout << *i << ' ';
+		}
+		std::cout << '\n' << "INSERTION SORT: ";
+		doInsertionSort(f2.begin(), f2.end(), std::greater< int >());
+		for (auto i = f2.cbegin(); i != f2.cend(); i++)
+		{
+			std::cout << *i << ' ';
+		}
+		//size_t size = std::stoll(argv[3]);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return 1;
+	}
 }
---
+++ novokhatskiy.danil/S6/sortings.hpp
@@ -0,0 +1,79 @@
+#ifndef SORTINGS_HPP
+#define SORTINGS_HPP
+
+#include <list>
+#include <cstddef>
+
+namespace novokhatskiy
+{
+  template< class Iter, class P >
+  void doShakerSort(Iter begin, Iter end, size_t size, P cmp)
+  {
+    auto last = std::prev(end);
+    bool swapped = true;
+    while (swapped)
+    {
+      swapped = false;
+      for (auto i = begin; i != last; i++)
+      {
+        auto next = std::next(i);
+        if (cmp(*i, *next))
+        {
+          std::swap(*i, *next);
+          swapped = true;
+        }
+      }
+      last = std::prev(last);
+      if (!swapped)
+      {
+        break;
+      }
+      swapped = false;
+      for (auto i = last; i != begin; --i)
+      {
+        auto prev = std::prev(i);
+        if (!cmp(*i, *prev))
+        {
+          std::swap(*i, *prev);
+          swapped = true;
+        }
+      }
+      begin = std::next(begin);
+    }
+  }
+
+  template< class It, class P >
+  void doInsertionSort(It begin, It end, P cmp)
+  {
+    for (auto i = begin; i != end; i++)
+    {
+      for (auto j = begin; j != i; j++)
+      {
+        if (cmp(*i,*j))
+        {
+          std::swap(*i, *j);
+        }
+      }
+    }
+    /*auto it = std::next(begin);
+    while (it != end)
+    {
+      auto key = *it;
+      auto prev = std::prev(it);
+      while (prev != begin && cmp(*prev, key))
+      {
+        *it = *prev;
+        it = prev;
+        prev = std::prev(it);
+      }
+      if (!cmp(key, *prev))
+      {
+        *it = key;
+      }
+      
+      it = std::next(it);
+    }*/
+  }
+}
+
+#endif
