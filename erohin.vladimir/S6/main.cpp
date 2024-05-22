df6047f4632695cef4c268b17fad8799ae038fc2
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-22 03:54:29 +0300

    Added some handling of CLA

--- erohin.vladimir/S6/main.cpp
+++ erohin.vladimir/S6/main.cpp
@@ -1,2 +1,25 @@
-int main()
-{}
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char ** argv)
+{
+  if (argc != 4)
+  {
+    std::cerr << "Wrong CLA's number\n";
+    return 1;
+  }
+  try
+  {
+    size_t size = std::stoull(argv[3]);
+    if (size == 0)
+    {
+      throw std::invalid_argument("Invalid sequence size");
+    }
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
+  return 0;
+}
