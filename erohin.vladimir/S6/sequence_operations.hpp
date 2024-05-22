13e08ec0840145dcb33e1870d9940d1f31d62e71
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-22 22:48:36 +0300

    Changed data structure to farward_list in random generation

--- erohin.vladimir/S6/sequence_operations.hpp
+++ erohin.vladimir/S6/sequence_operations.hpp
@@ -3,30 +3,30 @@
 
 #include <algorithm>
 #include <iterator>
-#include <vector>
+#include <forward_list>
 #include <iostream>
 
 namespace erohin
 {
   template< class T >
-  T random(std::pair< T, T > range)
+  T random(T min, T max)
   {
-    return (rand() % (range.second - range.first) + range.first);
+    return (std::rand() % (max - min) + min);
   }
 
   template<>
-  float random< float >(std::pair< float, float > range)
+  float random< float >(float min, float max)
   {
-    return ((static_cast< float >(rand()) / RAND_MAX) * (range.second - range.first) + range.first);
+    return ((static_cast< float >(std::rand()) / RAND_MAX) * (max - min) + min);
   }
 
   template< class T, class Container >
   void generate_random(Container & cnt, size_t n)
   {
-    std::vector< T > seq;
+    std::forward_list< T > seq;
     for (size_t i = 0; i < n; ++i)
     {
-      seq.push_back(random< T >(std::make_pair(0, 100)));
+      seq.push_front(random< T >(-1000, 1000));
     }
     Container temp(seq.begin(), seq.end());
     cnt.clear();
