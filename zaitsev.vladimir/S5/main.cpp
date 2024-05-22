c7c7b74fe07c560aed3f96152337e27eb67d3c91
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-05-22 06:39:38 +0300

    Summator added

--- zaitsev.vladimir/S5/main.cpp
+++ zaitsev.vladimir/S5/main.cpp
@@ -1,6 +1,15 @@
 #include "map.hpp"
+#include <operators.hpp>
 
+struct summator
+{
+  void operator()(const std::pair< const int, std::string >& key_value) {
+    result_ += key_value.first;
+  }
+  int result_ = 0;
+};
 
+int main(int argc, char** argv)
+{
 
-int main()
-{}
+}
