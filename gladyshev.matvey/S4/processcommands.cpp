32f8b25fbd3eaa17445b20306e271f8e9336eee2
magggottt <gladyshevmatvey@gmail.com>
2024-05-24 01:48:42 +0700

    fix processcommands

--- gladyshev.matvey/S4/processcommands.cpp
+++ gladyshev.matvey/S4/processcommands.cpp
@@ -6,46 +6,45 @@
 std::ostream& gladyshev::process_commands(std::istream& in, std::ostream& out, mainDic& findic)
 {
   Tree < std::string, std::function < mainDic(const std::string&, const dic&, const dic&) > > cmds;
+  Tree < std::string, std::function < void(const std::string&, const mainDic&) > > printCmds;
   cmds["complement"] = complement;
   cmds["intersect"] = intersect;
   cmds["union"] = union_dictionaries;
+  printCmds["print"] = print_dictionaries;
   std::string command = "";
   std::string dataset = "";
   std::string name1 = "";
   std::string name2 = "";
-  std::string name3 = "";
   while (in >> command)
   {
-    if (command == "print")
+    in >> dataset;
+    try
     {
-      in >> dataset;
-      try
-      {
-        print_dictionaries(dataset, findic);
-      }
-      catch (const std::overflow_error& e)
-      {
-        std::cout << e.what() << "\n";
-      }
+      printCmds.at(command)(dataset, findic);
     }
-    else
+    catch (const std::out_of_range& e)
     {
       try
       {
-        in >> name1 >> name2 >> name3;
-        dic supdic1 = findName(findic, name2);
-        dic supdic2 = findName(findic, name3);
-        mainDic temp = cmds.at(command)(name1, supdic1, supdic2);
+        in >> name1 >> name2;
+        dic supdic1 = findName(findic, name1);
+        dic supdic2 = findName(findic, name2);
+        mainDic temp = cmds.at(command)(dataset, supdic1, supdic2);
         for (const auto& pair: temp)
         {
           findic.insert(pair.first, pair.second);
         }
       }
-      catch (const std::out_of_range&)
+      catch (const std::out_of_range& e)
       {
-        std::cout << "<INVALID COMMAND>\n";
+        out << "<INVALID COMMAND>\n";
         std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
       }
+    }
+    catch (const std::overflow_error& e)
+    {
+      out << e.what() << "\n";
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
   return out;
