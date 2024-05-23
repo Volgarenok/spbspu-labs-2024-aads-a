9944c2448410c29e6935d27b339eac11cda0eb85
Rail <ksovfox@mail.ru>
2024-05-23 16:29:43 +0300

    Добавлен словарь вариаций функции тестирования

--- zhalilov.rail/S6/main.cpp
+++ zhalilov.rail/S6/main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
-#include <deque>
 #include <iomanip>
 
-#include "randomGenerators.hpp"
+#include <tree/twoThreeTree.hpp>
+
 #include "testSorts.hpp"
 
 int main(int argc, char *argv[])
 {
   if (argc != 4)
   {
-    std::cerr << "Incorrect args";
+    std::cerr << "Incorrect args\n";
     return 1;
   }
 
+  using namespace zhalilov;
+  using testSortFuncType = void (*)(size_t, std::ostream &);
+  TwoThree< std::pair< std::string, std::string >, testSortFuncType > testSortVariantsMap;
+  testSortVariantsMap[{ "ints", "ascending" }] = testSorts< int, std::less< int > >;
+  testSortVariantsMap[{ "ints", "descending" }] = testSorts< int, std::greater< int > >;
+  testSortVariantsMap[{ "floats", "ascending" }] = testSorts< float, std::less< float > >;
+  testSortVariantsMap[{ "floats", "descending" }] = testSorts< float, std::greater< float > >;
   try
   {
-    using namespace zhalilov;
     std::string direction = argv[1];
     std::string type = argv[2];
     size_t size = std::stoull(argv[3]);
@@ -24,22 +30,11 @@ int main(int argc, char *argv[])
       throw std::invalid_argument("main.cpp: size == 0");
     }
     std::cout << std::fixed << std::setprecision(1);
-    if (type == "ints")
-    {
-      std::deque< int > nums(size);
-      fillRandomInts(nums, size);
-      testSorts(nums, direction, std::cout);
-    }
-    else if (type == "floats")
-    {
-      std::deque< float > nums(size);
-      fillRandomFloats(nums, size);
-      testSorts(nums, direction, std::cout);
-    }
+    testSortVariantsMap[{ type, direction }](size, std::cout);
   }
   catch (const std::exception &e)
   {
-    std::cerr << e.what();
+    std::cerr << e.what() << '\n';
     return 1;
   }
 }
