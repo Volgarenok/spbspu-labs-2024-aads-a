66d66252b4b1fc6056cf0f4019d9d20a1c0d7428
pashko2303 <baranov-p.b@yandex.ru>
2024-05-24 23:46:16 +0300

    Command Line Arguments

--- baranov.pavel/S6/main.cpp
+++ baranov.pavel/S6/main.cpp
@@ -1,3 +1,23 @@
-int main()
-{}
+#include <iostream>
+#include <cstddef>
+
+int main(int argc, char * argv[])
+{
+  if (argc < 4)
+  {
+    std::cerr << "Incorrect CLA\n";
+    return 1;
+  }
+  size_t size = 0;
+  try
+  {
+    size = std::stoull(argv[3]);
+  }
+  catch (const std::exception &)
+  {
+    std::cerr << "Invalid size\n";
+    return 1;
+  }
+  std::cout << argv[1] << ' ' << argv[2] << ' ' << size << '\n';
+}
 
