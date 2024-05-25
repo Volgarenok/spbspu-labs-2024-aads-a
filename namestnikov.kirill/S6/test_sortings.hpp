c768bfd913a6dd75925e3bced7cd039fe5681e53
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-25 17:04:23 +0300

    testing other containers

--- namestnikov.kirill/S6/test_sortings.hpp
+++ namestnikov.kirill/S6/test_sortings.hpp
@@ -29,10 +29,21 @@ namespace namestnikov
   void testSortings(std::ostream & out, std::vector< T > data, size_t size, Compare compare)
   {
     printData(data.begin(), data.end(), out);
-    std::forward_list myList(data.begin(), data.end());
-    printData(myList.begin(), myList.end(), out);
-    doSelectionSort(myList.begin(), size, compare);
-    printData(myList.begin(), myList.end(), out);
+    std::forward_list< T > forwardlistSelection(data.begin(), data.end());
+    std::list< T > listSelection(data.begin(), data.end());
+    std::list< T > listShell(data.begin(), data.end());
+    std::deque< T > dequeSelection(data.begin(), data.end());
+    std::deque< T > dequeShell(data.begin(), data.end());
+    doSelectionSort(forwardlistSelection.begin(), size, compare);
+    doSelectionSort(listSelection.begin(), size, compare);
+    doShellSort(listShell.begin(), size, compare);
+    doSelectionSort(dequeSelection.begin(), size, compare);
+    doShellSort(dequeShell.begin(), size, compare);
+    printData(forwardlistSelection.begin(), forwardlistSelection.end(), out);
+    printData(listSelection.begin(), listSelection.end(), out);
+    printData(listShell.begin(), listShell.end(), out);
+    printData(dequeSelection.begin(), dequeSelection.end(), out);
+    printData(dequeShell.begin(), dequeShell.end(), out);
   }
 }
 
