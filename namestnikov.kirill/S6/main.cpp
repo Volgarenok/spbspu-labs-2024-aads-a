691b3f83aed3a30a975d1e54493d124dee3a2023
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-25 16:02:11 +0300

    trying my random numbers generator

--- namestnikov.kirill/S6/main.cpp
+++ namestnikov.kirill/S6/main.cpp
@@ -1,8 +1,29 @@
 #include <algorithm>
 #include <iostream>
 #include <list>
+#include <random>
 #include "shell_sort.hpp"
 #include "selection_sort.hpp"
+
+template< class T >
+T generateNumber(T minNumber, T maxNumber)
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution< float > dist(minNumber, maxNumber);
+  return static_cast< T >(dist(gen));
+}
+
+template< class T >
+std::vector< T > generateNumbers(size_t size)
+{
+  std::vector< T > data(size);
+  for (size_t i = 0; i < size; ++i)
+  {
+    data[i] = generateNumber< T >(-1000, 1000);
+  }
+  return data;
+}
 
 int main()
 {
@@ -17,5 +38,15 @@ int main()
   for (auto it = temp.begin(); it != temp.end(); ++it)
   {
     std::cout << *it;
+  }
+  std::vector< int > vec = generateNumbers< int >(20);
+  for (auto it = vec.begin(); it != vec.end(); ++it)
+  {
+    std::cout << *it << "|||";
+  }
+  doSelectionSort(vec.begin(), vec.size(), std::less< int >());
+  for (auto it = vec.begin(); it != vec.end(); ++it)
+  {
+    std::cout << *it << "|||";
   }
 }
