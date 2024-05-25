5dee1de0c9daf0344f6123a98f04d79d14282940
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-25 12:21:05 +0300

    started selection sort realization

--- namestnikov.kirill/S6/main.cpp
+++ namestnikov.kirill/S6/main.cpp
@@ -20,6 +20,25 @@ void doShellSort(Iter begin, size_t size, Compare compare)
   }
 }
 
+template< class Iter, class Compare >
+void doSelectionSort(Iter begin, size_t size, Compare compare)
+{
+  for (size_t i = 0; i < n - 1; ++i)
+  {
+    size_t minIndex = i;
+    for (size_t j = i + 1; j < n; ++j)
+    {
+      if (compare(*std::next(begin, j), *std::next(begin, minIndex)))
+      {
+        minIndex = j;
+      }
+    }
+    if (minIndex != i)
+    {
+      std::swap(*std::next(begin, minIndex), *std::next(begin, i));
+    }
+  }
+}
 
 int main()
 {
