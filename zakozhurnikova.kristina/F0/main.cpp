9376866372fb2192f9123c36e478bea52f4aa808
kastiela <kristinazak150@gmail.com>
2024-05-26 15:15:08 +0300

    add save uncorrect

--- zakozhurnikova.kristina/F0/main.cpp
+++ zakozhurnikova.kristina/F0/main.cpp
@@ -35,6 +35,7 @@ int main(int argc, char* argv[])
   commands["palindrome"] = std::bind(palindrome, _1, std::ref(dictionary));
   commands["rider"] = std::bind(rider, _1, std::ref(dictionary));
   commands["interpreter"] = std::bind(interpreter, _1, std::ref(dictionary));
+  commands["save"] = std::bind(save, _1, std::ref(dictionary));
 
   std::string command;
   while (std::cin >> command)
