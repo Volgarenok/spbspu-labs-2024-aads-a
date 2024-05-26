8911d4c3e9031ea21176ca9debbc24f2c64bcd77
exxiiint <filipp.sivkov@yandex.ru>
2024-05-26 19:20:46 +0300

    fix

--- sivkov.filipp/F0/cmd.hpp
+++ sivkov.filipp/F0/cmd.hpp
@@ -12,12 +12,15 @@ namespace sivkov
   void create_dictionary(AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, std::istream& in);
   void remove_word(AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, std::istream& in);
   void list_words(const AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, std::istream& in, std::ostream& out);
+  void count_words(const AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, std::istream& in, std::ostream& out);
   void search_words(const AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, std::istream& in, std::ostream& out);
   void delete_dictionary(AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, std::istream& in);
   void rename_dictionary(AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, std::istream& in);
   void merge_dictionaries(AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, std::istream& in);
   void repeating_words(const AVLTree<std::string, AVLTree<std::string, std::string>>& treeOfdic, std::istream& in, std::ostream& out);
   void save(const AVLTree< std::string, AVLTree< std::string, std::string > >& treeOfdic, const std::string& filename);
+  void help(std::ostream& out);
 }
 
 #endif
+
