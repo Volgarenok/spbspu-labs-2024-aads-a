0c16ad52c37d7f1c0f7e6410e546eec554b9179f
Rail <ksovfox@mail.ru>
2024-05-27 23:38:34 +0300

    Реализация файла main

--- zhalilov.rail/F0/main.cpp
+++ zhalilov.rail/F0/main.cpp
@@ -13,12 +13,26 @@ int main()
   using modulesMap = TwoThree< std::string, varModule >;
   modulesMap modules;
 
-  TwoThree< std::string, std::function< void(std::istream &, std::ostream &) > > сommands;
+  TwoThree< std::string, std::function< void(std::istream &, std::ostream &) > > commands;
+  std::string history = "history";
 
   using namespace std::placeholders;
-  сommands["calc"] = std::bind(calc, std::cref(modules), std::ref(std::cout), _1, _2);
-  сommands["modulesadd"] = std::bind(modulesadd, std::ref(modules), _1, _2);
-  сommands["modulesvaradd"] = std::bind(modulesvarradd, std::ref(modules), _1, _2);
+  commands["calc"] = std::bind(calc, std::cref(modules), std::cref(history), _1, _2);
+  commands["modulesadd"] = std::bind(modulesadd, std::ref(modules), _1, _2);
+  commands["modulesdelete"] = std::bind(modulesdelete, std::ref(modules), _1, _2);
+
+  commands["modulesvaradd"] = std::bind(modulesvaradd, std::ref(modules), _1, _2);
+  commands["modulesvardelete"] = std::bind(modulesvardelete, std::ref(modules), _1, _2);
+
+  commands["modulesshow"] = std::bind(modulesshow, std::cref(modules), _1, _2);
+
+  commands["modulesimport"] = std::bind(modulesimport, std::ref(modules), _1, _2);
+  commands["modulesexport"] = std::bind(modulesexport, std::ref(modules), _1, _2);
+
+  commands["historyshow"] = std::bind(historyshow, std::cref(history), _1, _2);
+  commands["historyclear"] = std::bind(historyclear, std::cref(history), _1, _2);
+
+  modules.insert(std::make_pair("main", varModule{}));
 
   while (!std::cin.eof())
   {
@@ -26,11 +40,15 @@ int main()
     std::cin >> command;
     try
     {
-      сommands.at(command)(std::cin, std::cout);
+      commands.at(command)(std::cin, std::cout);
+    }
+    catch (const std::out_of_range &e)
+    {
+      std::cerr << "command doesn't exist\n";
     }
     catch (const std::exception &e)
     {
-      std::cerr << e.what();
+      std::cerr << e.what() << '\n';
     }
   }
 }
