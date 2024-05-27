933f11441c6dd5d2209b818499004f97d04a8a45
Rail <ksovfox@mail.ru>
2024-05-27 23:35:52 +0300

    Исправлен метод erase в дереве

--- zhalilov.rail/common/tree/twoThreeTree.hpp
+++ zhalilov.rail/common/tree/twoThreeTree.hpp
@@ -45,7 +45,7 @@ namespace zhalilov
     std::pair< iterator, bool > insert(const MapPair &);
     iterator erase(iterator);
     iterator erase(const_iterator);
-    char erase(const Key &);
+    size_t erase(const Key &);
     void clear() noexcept;
     void swap(TwoThree &) noexcept;
 
@@ -358,10 +358,15 @@ namespace zhalilov
   }
 
   template < class Key, class T, class Compare >
-  char TwoThree< Key, T, Compare >::erase(const Key &key)
+  size_t TwoThree< Key, T, Compare >::erase(const Key &key)
   {
-    erase(find(key));
-    return doFind(key).second;
+    auto result = find(key);
+    if (result != end())
+    {
+      erase(result);
+      return 1;
+    }
+    return 0;
   }
 
   template < class Key, class T, class Compare >
