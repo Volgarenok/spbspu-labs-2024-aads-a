595e5f3a792ace55c5959c78a03a687acf256666
magggottt <gladyshevmatvey@gmail.com>
2024-05-28 05:03:36 +0700

    some overflow fix

--- gladyshev.matvey/S5/sumfunc.cpp
+++ gladyshev.matvey/S5/sumfunc.cpp
@@ -6,7 +6,7 @@
 void gladyshev::Key_summ::operator()(const std::pair< const int, std::string >& value)
 {
   bool firstCheck = (result > 0 && value.first > std::numeric_limits< int >::max() - result);
-  bool secCheck = (result < 0 && value.first < std::numeric_limits< int >::min() - result);
+  bool secCheck = ((result < 0 && value.first < 0) && (value.first < std::numeric_limits< int >::min() - result));
   if (firstCheck || secCheck)
   {
     throw std::overflow_error("overflow");
