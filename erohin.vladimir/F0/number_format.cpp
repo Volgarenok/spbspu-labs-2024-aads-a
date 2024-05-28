80dbe50f3c71aac0861af3856a86f2e9bff6f9d8
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-28 07:25:22 +0300

    Added NumberFormat struct for formatted frequency output

---
+++ erohin.vladimir/F0/number_format.cpp
@@ -0,0 +1,32 @@
+#include "number_format.hpp"
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include "scope_guard.hpp"
+
+std::ostream & erohin::operator<<(std::ostream & output, NumberFormat && numformat)
+{
+  std::ostream::sentry sentry(output);
+  if (!sentry)
+  {
+    return output;
+  }
+  ScopeGuard sg(output);
+  if (numformat.format == NUMBER)
+  {
+    output << numformat.number;
+  }
+  else if (numformat.format == PROPORTION)
+  {
+    output << numformat.number << " / " << numformat.size;
+  }
+  else if (numformat.format == FRACTIONAL)
+  {
+    output << std::setprecision(6) << std::fixed << static_cast< double >(numformat.number) / numformat.size;
+  }
+  else if (numformat.format == PERCENTAGE)
+  {
+    output << std::setprecision(4) << std::fixed << static_cast< double >(numformat.number) / numformat.size * 100 << " %";
+  }
+  return output;
+}
