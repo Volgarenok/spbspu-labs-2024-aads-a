c13cec45b20737d2bca666f615fa75bcd2436f26
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-28 07:21:33 +0300

    Added CLA's handling of help and check commands

--- erohin.vladimir/F0/main.cpp
+++ erohin.vladimir/F0/main.cpp
@@ -1,2 +1,19 @@
-int main()
-{}
+#include <iostream>
+#include <cstring>
+#include "secondary_commands.hpp"
+
+int main(int argc, char ** argv)
+{
+  using namespace erohin;
+  if (argc == 2 && !strcmp(argv[1], "--help"))
+  {
+    printHelp(std::cout);
+    return 0;
+  }
+  else if (argc == 3 && !strcmp(argv[1], "--check"))
+  {
+    checkDictionary(argv[2], std::cout);
+    return 0;
+  }
+  return 0;
+}
