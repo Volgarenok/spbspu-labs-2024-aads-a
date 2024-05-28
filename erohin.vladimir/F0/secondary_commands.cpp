0e723258ccba4f06d5e59469e44d91982b50f9b6
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-28 13:24:02 +0300

    Added check CLA implementation

--- erohin.vladimir/F0/secondary_commands.cpp
+++ erohin.vladimir/F0/secondary_commands.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include "format.hpp"
+#include "input_output_collection.hpp"
 
 void erohin::printHelp(std::ostream & output)
 {
@@ -42,7 +43,25 @@ void erohin::printHelp(std::ostream & output)
   output << "\tCreate a frequency dictionary <new_dict> based on the intersection between <dict1> and <dirt2>.\n\n";
 }
 
-void erohin::checkDictionary(const char *, std::ostream &)
+void erohin::checkDictionary(const char * file_name, std::ostream & output)
 {
-
+  std::fstream file(file_name, std::ios::in);
+  collection dict_context;
+  std::string dict_name;
+  file >> StringFormat{ dict_name, ':' };
+  while (!file.eof())
+  {
+    if (file.fail())
+    {
+      break;
+    }
+    inputDictionary(dict_context[dict_name], file);
+    if (!file)
+    {
+      file.clear();
+    }
+    file >> StringFormat{ dict_name, ':' };
+  }
+  file.close();
+  output << "File contains " << (!file.eof() && file.fail() ? "incorrect" : "correct") << " frequency dictionaries.\n";
 }
