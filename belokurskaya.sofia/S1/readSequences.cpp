a70bea0f1484b941148938b275fceaee0ec00b53
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-05-29 11:12:51 +0300

    fixed readSequences.cpp

--- belokurskaya.sofia/S1/readSequences.cpp
+++ belokurskaya.sofia/S1/readSequences.cpp
@@ -1,21 +1,47 @@
 #include "readSequences.hpp"
 
+#include <limits>
+#include <string>
+
 void belokurskaya::readSequences(std::istream& in, List< std::pair< std::string, List< size_t > > >& sequences)
 {
-  std::string name = "";
-
-  while (in >> name)
+  std::string line;
+  while (std::getline(in, line))
   {
-    List< size_t > sequence;
-    size_t number = 0;
-    while (in >> number)
+    if (line.empty()) continue;
+
+    std::string name;
+    size_t index = 0;
+
+    while (index < line.size() && line[index] != ' ')
     {
+      name.push_back(line[index]);
+      ++index;
+    }
+
+    List<size_t> sequence;
+
+    while (index < line.size() && line[index] == ' ')
+    {
+      ++index;
+    }
+
+    while (index < line.size())
+    {
+      size_t number = 0;
+      while (index < line.size() && line[index] >= '0' && line[index] <= '9')
+      {
+        number = number * 10 + (line[index] - '0');
+        ++index;
+      }
       sequence.push_back(number);
+
+      while (index < line.size() && line[index] == ' ')
+      {
+        ++index;
+      }
     }
-    if (!in.eof())
-    {
-      in.clear();
-    }
-    sequences.push_back({name, sequence});
+
+    sequences.push_back({ name, sequence });
   }
 }
