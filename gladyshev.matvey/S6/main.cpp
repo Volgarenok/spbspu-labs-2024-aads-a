b7baeda82ed3320849b9c112d867dd26d0049557
magggottt <gladyshevmatvey@gmail.com>
2024-05-30 01:12:22 +0700

    git push

--- gladyshev.matvey/S6/main.cpp
+++ gladyshev.matvey/S6/main.cpp
@@ -1,16 +1,33 @@
-#include <iostream>
+#include <map>
+#include <iomanip>
+#include <utility>
+#include <functional>
 
+#include "sortprocessing.hpp"
 #include "sorts.hpp"
-#include <list>
 
 int main(int argc, char *argv[])
 {
   using namespace gladyshev;
-  std::list< int > data = {9, 7, 5, 11, 12, 2, 14, 3, 10, 6};
-  merge(data.begin(), data.end(), std::greater< int >());
-  for (int value: data)
+  if (argc != 4)
   {
-    std::cout << value << " ";
+    std::cerr << "bad data\n";
+    return 1;
   }
-  std::cout << "\n";
+  size_t size = std::stoull(argv[3]);
+  std::map< std::pair< std::string, std::string >, std::function< void(size_t, std::ostream&) > > cmds;
+  cmds[std::make_pair("ascending", "floats")] = processSorts< float, std::less< float > >;
+  cmds[std::make_pair("descending", "floats")] = processSorts< float, std::greater< float > >;
+  cmds[std::make_pair("ascending", "ints")] = processSorts< int, std::less< int > >;
+  cmds[std::make_pair("descending", "ints")] = processSorts< int, std::greater< int > >;
+  std::cout << std::fixed << std::setprecision(1);
+  try
+  {
+    cmds.at(std::make_pair(std::string(argv[1]), std::string(argv[2])))(size, std::cout);
+  }
+  catch (...)
+  {
+    std::cerr << "Error\n";
+    return 1;
+  }
 }
