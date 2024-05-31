6ba6fdaf7821a547e1fd0165042970202e17becf
Skuratov09 <skyratovdaniil28@gmail.com>
2024-05-31 21:02:14 +0300

    Implementing AVLTree and interface

--- skuratov.danil/S4/main.cpp
+++ skuratov.danil/S4/main.cpp
@@ -2,38 +2,85 @@
 
 namespace skuratov
 {
-	namespace detail
-	{
-		template < typename Key, typename Value >
-		struct TreeNode
-		{
-			TreeNode* parent;
-			TreeNode* left;
-			TreeNode* right;
-			std::pair< Key, Value > data;
+  namespace detail
+  {
+    template < typename Key, typename Value >
+    struct TreeNode
+    {
+      TreeNode(const Key key, const Value value):
+        parent_(nullptr),
+        left_(nullptr),
+        right_(nullptr),
+        data_(std::pair< Key, Value >(key, value))
+      {}
+      ~TreeNode() = default;
+      TreeNode* parent_;
+      TreeNode* left_;
+      TreeNode* right_;
+      std::pair< Key, Value > data;
+    };
+  }
 
-			TreeNode(const Key key, const Value value):
-			  parent(nullptr),
-				left(nullptr),
-				right(nullptr),
-				data(std::pair< Key, Value >(key, value))
-			{}
-		};
-	}
+  template < typename Key, typename Value, typename Compare >
+  class AVLTree
+  {
+  public:
+    AVLTree():
+      root_(nullptr),
+      cmp_(Compare()),
+      size_(0)
+    {}
 
-	template < typename Key, typename Value, typename Compare >
-	class BinarySearchTree
-	{
-	public:
-		void push(Key k, Value v);
-		Value get(Key k);
-		Value drop(Key k);
-		//...
-	};
+    ~AVLTree()
+    {
+      clear();
+    }
 
-	int main()
-	{
-		std::cout << "Hello!";
-		return 0;
-	}
+    AVLTree(const AVLTree& diff):
+      root_(nullptr),
+      cmp_(diff.cmp_),
+      size_(0)
+    {
+      try
+      {
+        for (auto it = diff.cbegin(); it != diff.cend(); it++)
+        {
+          push(it->first, it->second)
+        }
+      }
+      catch (...)
+      {
+        clear();
+        throw;
+      }
+    }
+
+    AVLTree(const AVLTree&& diff):
+      root_(diff.root_),
+      cmp_(diff.cmp_),
+      size_(diff.size_)
+    {
+      diff.root_ = nullptr;
+      diff.size_ = 0;
+    }
+
+    size_t size() const;
+    bool empty() const noexcept;
+    void clear() noexcept;
+    void swap();
+
+    void push(Key k, Value v);
+    Value get(Key k);
+    Value drop(Key k);
+  private:
+    detail::TreeNode< Key, Value >* root_;
+    Compare* cmp_;
+    size_t size_;
+  };
+
+  int main()
+  {
+    std::cout << "Hello!";
+    return 0;
+  }
 }
