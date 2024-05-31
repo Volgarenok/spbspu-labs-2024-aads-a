7241e2212a4e2358464f972ee0c64fb39ec45a5f
Skuratov09 <skyratovdaniil28@gmail.com>
2024-05-31 19:49:30 +0300

    Implemented tree node

--- skuratov.danil/S4/main.cpp
+++ skuratov.danil/S4/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 
-int main()
+namespace skuratov
 {
-	std::cout << "Hello, I'm Cristiano Ronaldo";
-	return 0;
+	namespace detail
+	{
+		template < typename Key, typename Value >
+		struct TreeNode
+		{
+			TreeNode* parent;
+			TreeNode* left;
+			TreeNode* right;
+			std::pair< Key, Value > data;
+
+			TreeNode(const Key key, const Value value):
+			  parent(nullptr),
+				left(nullptr),
+				right(nullptr),
+				data(std::pair< Key, Value >(key, value))
+			{}
+		};
+	}
+
+	template < typename Key, typename Value, typename Compare >
+	class BinarySearchTree
+	{
+	public:
+		void push(Key k, Value v);
+		Value get(Key k);
+		Value drop(Key k);
+		//...
+	};
+
+	int main()
+	{
+		std::cout << "Hello!";
+		return 0;
+	}
 }
