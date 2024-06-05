11f293a88caa045075d1d37cceadfcaab48d3d7e
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-06-05 22:07:28 +0300

    added bidirectional node and iterator

---
+++ namestnikov.kirill/F0/bidirectional_node.hpp
@@ -0,0 +1,32 @@
+#ifndef BIDIRECTIONAL_NODE_HPP
+#define BIDIRECTIONAL_NODE_HPP
+
+namespace namestnikov
+{
+  namespace detail
+  {
+    struct BaseListNode
+    {
+      BaseListNode * next;
+      BaseListNode * prev;
+      BaseListNode():
+        next(nullptr),
+        prev(nullptr)
+      {}
+      virtual ~BaseListNode() = default;
+    };
+
+    template< class T >
+    struct BiListNode: BaseListNode
+    {
+      T data;
+      BiListNode(const T& newData):
+        BaseListNode(),
+        data(newData)
+      {}
+    };
+  }
+}
+
+
+#endif
