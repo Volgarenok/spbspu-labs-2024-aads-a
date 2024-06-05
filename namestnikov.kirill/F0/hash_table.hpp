07cd675167f7ca0318594163087953bf494ecba9
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-06-05 13:29:22 +0300

    constructors for hash table

--- namestnikov.kirill/F0/hash_table.hpp
+++ namestnikov.kirill/F0/hash_table.hpp
@@ -18,6 +18,38 @@ namespace namestnikov
     using list_iterator_t = typename std::list< node_t * >::iterator;
     using hash_table_iterator = HashTableIterator< Key, Value >;
     using const_hash_table_iterator = ConstHashTableIterator< Key, Value >;
+    HashTable():
+      capacity_(5),
+      count_(0),
+      buckets_(new list_iterator_t[5]),
+      elements_()
+    {
+      for (size_t i = 0; i < 5; ++i)
+      {
+        buckets_[i] = elements_.end();
+      }
+    }
+    HashTable(std::initializer_list< val_type_t > initList):
+      capacity_(initList.size() / 0.75 + 1),
+      count_(0),
+      buckets_(new list_iterator_t[initList.size() / 0.75 + 1]),
+      elements_()
+    {
+      for (size_t i = 0; i < capacity_; ++i)
+      {
+        buckets_[i] = elements_.end();
+      }
+      auto it = initList.begin();
+      for (; it != initList.end(); ++it)
+      {
+        insert((*it).first, (*it).second);
+      }
+    }
+  private:
+    size_t count_;
+    size_t capacity_;
+    std::list< node_t * > elements_;
+    list_iterator_t * buckets_;
   };
 }
 
