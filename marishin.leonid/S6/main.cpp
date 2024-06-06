44751c6111037aabe2779045bfe16c17176bee19
marlions <marishin.lv@edu.spbstu.ru>
2024-06-06 16:47:19 +0300

    sort

--- marishin.leonid/S6/main.cpp
+++ marishin.leonid/S6/main.cpp
@@ -1,2 +1,50 @@
-int main()
-{}
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <forward_list>
+#include <functional>
+#include <iostream>
+#include <linkedList.hpp>
+#include <list>
+#include <map>
+#include <string>
+#include "generate.hpp"
+#include "print.hpp"
+#include "sorting.hpp"
+#include "sortingList.hpp"
+
+int main(int argc, char* argv[])
+{
+  using namespace marishin;
+  if (argc != 4)
+  {
+    std::cerr << "No right argument";
+    return 1;
+  }
+  std::string sort = argv[1];
+  std::string type = argv[2];
+  auto condition = sort + type;
+  size_t size = std::atoi(argv[3]);
+  if (size == 0)
+  {
+    std::cerr << "must be greater than 0";
+    return 1;
+  }
+  try
+  {
+    std::default_random_engine generator;
+    using namespace std::placeholders;
+    std::map< std::string, std::function< void(std::ostream&, size_t, std::default_random_engine&) > > cmd;
+    cmd["ascendingints"] = fillContainer< int, std::less< int > >;
+    cmd["ascendingfloats"] = fillContainer< float, std::less< float > >;
+    cmd["descendingints"] = fillContainer< int, std::greater< int > >;
+    cmd["descendingfloats"] = fillContainer< float, std::greater< float > >;
+
+    cmd.at(condition)(std::cout, size, generator);
+  }
+  catch (const std::exception&)
+  {
+    std::cout << "No right argument\n";
+    return 1;
+  }
+}
---
+++ marishin.leonid/S6/sorting.hpp
@@ -0,0 +1,99 @@
+#ifndef SORTING_HPP
+#define SORTING_HPP
+#include <utility>
+#include <algorithm>
+#include <iterator>
+#include "iterator.hpp"
+#include <linkedList.hpp>
+
+namespace marishin
+{
+  template < class Iterator, class Compare >
+  void shaker(Iterator begin, size_t size, Compare cmp)
+  {
+    ++begin;
+    size_t leftMark = 1;
+    size_t rightMark = size - 1;
+    auto end = begin;
+    size_t count = 0;
+    for (size_t i = 1; i < size - 1; ++i)
+    {
+      ++end;
+    }
+    while (leftMark <= rightMark)
+    {
+      auto tempEnd = end;
+      auto tempBegin = begin;
+      for (size_t i = 0; i < count; i++)
+      {
+        --tempEnd;
+        ++tempBegin;
+      }
+      auto tmpBegin = tempBegin;
+      for (size_t i = rightMark; i >= leftMark; --i)
+      {
+        auto curr = tempEnd;
+        --curr;
+        if (cmp(*(tempEnd), *(curr)))
+        {
+          std::iter_swap(curr, tempEnd);
+        }
+        --tempEnd;
+      }
+      leftMark++;
+      tmpBegin++;
+
+      for (size_t i = leftMark; i <= rightMark; ++i)
+      {
+        auto curr = tmpBegin;
+        --curr;
+        if (cmp(*(tmpBegin), *(curr)))
+        {
+          std::iter_swap(curr, tmpBegin);
+        }
+        ++tmpBegin;
+      }
+      rightMark--;
+      count++;
+    }
+  }
+
+  template< class Iterator, class Compare >
+  void qSort(Iterator begin, size_t size, Compare cmp)
+  {
+    if (size < 2)
+    {
+      return;
+    }
+    Iterator pivot = begin + size / 2;
+
+    Iterator left = begin;
+    Iterator right = begin + size - 1;
+    while (left <= right)
+    {
+      while (cmp(*(left), *(pivot)))
+      {
+        ++left;
+      }
+      while (cmp(*pivot, *right))
+      {
+        --right;
+      }
+      if (left <= right)
+      {
+        std::iter_swap(left, right);
+        ++left;
+        --right;
+      }
+    }
+    if (right > begin)
+    {
+      qSort(begin, right - begin + 1, cmp);
+    }
+    if (left < begin + size)
+    {
+      qSort(left, begin + size - left, cmp);
+    }
+  }
+}
+#endif
