75d52b300c884f6885b19f7bab53ba1fa2878788
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-06-06 16:05:58 +0300

    big changes

--- novokhatskiy.danil/S6/sortAndPrintSequence.hpp
+++ novokhatskiy.danil/S6/sortAndPrintSequence.hpp
@@ -1,89 +1,63 @@
 #ifndef SORT_AND_PRINT_SEQUENCE
 #define SORT_AND_PRINT_SEQUENCE
 
-#include <algorithm>
-#include <cstdlib>
-#include <deque>
-#include <forward_list>
-#include <iomanip>
-#include <list>
 #include <ostream>
+#include <list>
+#include <deque>
+#include <algorithm>
 #include <stdexcept>
+#include <iomanip>
+#include <forward_list>
 #include "sortings.hpp"
 
 namespace novokhatskiy
 {
-  template < class It >
-  void printSeq(It begin, It end, std::ostream& out)
-  {
-    while (begin != end)
-    {
-      out << *begin;
-      begin++;
-      if (begin != end)
-      {
-        out << ' ';
-      }
-    }
-    out << '\n';
-  }
+	template< class It >
+	void printSeq(It begin, It end, std::ostream& out)
+	{
+		if (begin != end)
+		{
+			out << *begin;
+			while (++begin != end)
+			{
+				out << ' ' << *begin;
+			}
+			out << '\n';
+		}
+	}
 
-  template < class T >
-  void sortSeq(std::string& sort, std::string& type, size_t size, std::ostream& out)
-  {
-    using namespace novokhatskiy;
-    std::forward_list< T > forwardList;
-    std::list< T > biList;
-    std::deque< T > deque;
-    for (size_t i = 0; i != size; i++)
-    {
-      T value;
-      if (type == "ints")
-      {
-        value = std::rand();
-      }
-      else
-      {
-        value = static_cast< float >(std::rand() / (static_cast< float >(RAND_MAX / RAND_MAX)));
-      }
-      forwardList.push_front(value);
-      biList.push_back(value);
-      deque.push_back(value);
-    }
-    std::list< T > secondBiList = biList;
-    std::deque< T > secondDeque = deque;
-    std::deque< T > thirdDeque = deque;
-    out << std::setprecision(1) << std::fixed;
-    if (sort == "descending")
-    {
-      doInsertionSort(forwardList.begin(), forwardList.end(), std::greater< T >());
-      doInsertionSort(biList.begin(), biList.end(), std::greater< T >());
-      doShakerSort(secondBiList.begin(), secondBiList.end(), std::greater< T >());
-      doInsertionSort(deque.begin(), deque.end(), std::greater< T >());
-      doShakerSort(secondDeque.begin(), secondDeque.end(), std::greater< T >());
-      std::sort(thirdDeque.begin(), thirdDeque.end(), std::greater< T >());
-    }
-    else if (sort == "ascending")
-    {
-      doInsertionSort(forwardList.begin(), forwardList.end(), std::less< T >());
-      doInsertionSort(biList.begin(), biList.end(), std::less< T >());
-      doShakerSort(secondBiList.begin(), secondBiList.end(), std::less< T >());
-      doInsertionSort(deque.begin(), deque.end(), std::less< T >());
-      doShakerSort(secondDeque.begin(), secondDeque.end(), std::less< T >());
-      std::sort(thirdDeque.begin(), thirdDeque.end(), std::less< T >());
-    }
-    else
-    {
-      throw std::invalid_argument("Incorrect sorting");
-    }
-    printSeq(forwardList.cbegin(), forwardList.cend(), out);
-    printSeq(biList.cbegin(), biList.cend(), out);
-    printSeq(secondBiList.cbegin(), secondBiList.cend(), out);
-    printSeq(deque.cbegin(), deque.cend(), out);
-    printSeq(secondDeque.cbegin(), secondDeque.cend(), out);
-    printSeq(thirdDeque.cbegin(), thirdDeque.cend(), out);
-    out << '\n';
-  }
+	template< class It, class Cmp >
+	void optSort(It begin, It end, std::ostream& out, Cmp cmp, void(*sort)(It, It, Cmp))
+	{
+		sort(begin, end, cmp);
+		printSeq(begin, end, out);
+	}
+
+	template< class T, class Cmp >
+	void sortSeq(size_t size, std::ostream& out)
+	{
+		using namespace novokhatskiy;
+		std::deque< T > deque;
+		T value;
+		for (size_t i = 0; i != size; i++)
+		{
+			value = static_cast < T > (std::rand() / (static_cast < T > (RAND_MAX / RAND_MAX)));
+			deque.push_back(value);
+		}
+		std::forward_list< T > forwardList(deque.cbegin(), deque.cend());
+		std::list< T > biList(deque.cbegin(), deque.cend());
+		std::list< T > secondBiList(deque.cbegin(), deque.cend());
+		std::deque< T > secondDeque(deque.cbegin(), deque.cend());
+		std::deque< T > thirdDeque(deque.cbegin(), deque.cend());
+		out << std::setprecision(1) << std::fixed;
+		Cmp cmp;
+		optSort(forwardList.begin(), forwardList.end(), out, cmp, doInsertionSort);
+		optSort(biList.begin(), biList.end(), out, cmp, doInsertionSort);
+		optSort(secondBiList.begin(), secondBiList.end(), out, cmp, doShakerSort);
+		optSort(deque.begin(), deque.end(), out, cmp, doInsertionSort);
+		optSort(secondDeque.begin(), secondDeque.end(), out, cmp, doShakerSort);
+		optSort(thirdDeque.begin(), thirdDeque.end(), out, cmp, doInsertionSort);
+	}
 }
 
 #endif
