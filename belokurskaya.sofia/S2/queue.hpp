e5113fbef0435f40bdd0f848ec5d260de922d9b6
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-06-07 14:03:08 +0300

    small changes in queue and stack

--- belokurskaya.sofia/S2/queue.hpp
+++ belokurskaya.sofia/S2/queue.hpp
@@ -8,125 +8,128 @@ namespace belokurskaya
   template< class T >
   class Queue
   {
-  public:
-    Queue()
-    {
-      capacity_ = initial_capacity_;
-      size_ = 0;
-      front_ = 0;
-      rear_ = -1;
-      data_ = new T[capacity_];
-    }
-
-    Queue(const Queue& other)
-    {
-      capacity_ = other.capacity_;
-      size_ = other.size_;
-      front_ = other.front_;
-      rear_ = other.rear_;
-      data_ = new T[capacity_];
-      std::copy(other.data_, other.data_ + capacity_, data_);
-    }
-
-    Queue(Queue&& other)
-    {
-      capacity_ = other.capacity_;
-      size_ = other.size_;
-      front_ = other.front_;
-      rear_ = other.rear_;
-      data_ = other.data_;
-      other.capacity_ = initial_capacity_;
-      other.size_ = 0;
-      other.front_ = 0;
-      other.rear_ = -1;
-      other.data_ = new T[capacity_];
-    }
-
-    ~Queue()
-    {
-      delete[] data_;
-    }
-
-    void push(T rhs)
-    {
-      if (isFull())
+    public:
+      Queue()
       {
-        addMemory();
+        capacity_ = initial_capacity_;
+        size_ = 0;
+        front_ = 0;
+        rear_ = -1;
+        data_ = new T[capacity_];
       }
-      size_++;
-      data_[++rear_] = rhs;
-    }
 
-    T drop()
-    {
-      if (capacity_ / capacity_change_factor_ >= initial_capacity_ && size_ < capacity_ / capacity_change_factor_)
+      Queue(const Queue& other)
       {
-        freeMemory();
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        rear_ = other.rear_;
+        data_ = new T[capacity_];
+        std::copy(other.data_, other.data_ + capacity_, data_);
       }
-      if (size_ > 0)
+
+      Queue(Queue&& other)
       {
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        rear_ = other.rear_;
+        data_ = other.data_;
+        other.capacity_ = initial_capacity_;
+        other.size_ = 0;
+        other.front_ = 0;
+        other.rear_ = -1;
+        other.data_ = new T[capacity_];
+      }
+
+      ~Queue()
+      {
+        delete[] data_;
+      }
+
+      void push(T rhs)
+      {
+        if (isFull())
+        {
+          addMemory();
+        }
+        size_++;
+        data_[++rear_] = rhs;
+      }
+
+      T drop()
+      {
+        if (isEmpty())
+        {
+          throw std::runtime_error("Attempt to drop from an empty queue");
+        }
+        T result = data_[front_];
+        front_++;
         size_--;
-        return data_[front_++];
+        if (capacity_ / capacity_change_factor_ >= initial_capacity_ && size_ < capacity_ / capacity_change_factor_)
+        {
+          freeMemory();
+        }
+        return result;
       }
-    }
 
-    T front()
-    {
-      if (isEmpty())
+      T front()
       {
-        throw std::runtime_error("Queue is empty");
+        if (isEmpty())
+        {
+          throw std::runtime_error("Queue is empty");
+        }
+        return data_[front_];
       }
-      return data_[front_];
-    }
 
-    bool isEmpty()
-    {
-      return rear_ < front_;
-    }
-
-    bool isFull()
-    {
-      return rear_ == capacity_ - 1;
-    }
-
-    friend std::ostream& operator<<(std::ostream& out, const Queue< T >& queue)
-    {
-      for (size_t i = 0; i <= queue.rear_; ++i)
+      bool isEmpty()
       {
-        out << queue.data_[i] << " ";
+        return rear_ < front_;
       }
-      return out;
-    }
 
-  private:
-    T* data_;
-    size_t size_;
-    size_t front_;
-    size_t rear_;
-    size_t capacity_;
-    const size_t initial_capacity_ = 3;
-    const size_t capacity_change_factor_ = 2;
+      bool isFull()
+      {
+        return rear_ == capacity_ - 1;
+      }
 
-    void addMemory()
-    {
-      T* newData = new T[capacity_ * capacity_change_factor_];
-      std::copy(data_, data_ + capacity_, newData);
-      capacity_ *= capacity_change_factor_;
-      delete[] data_;
-      data_ = newData;
-    }
+      friend std::ostream& operator<<(std::ostream& out, const Queue< T >& queue)
+      {
+        for (size_t i = 0; i <= queue.rear_; ++i)
+        {
+          out << queue.data_[i] << " ";
+        }
+        return out;
+      }
 
-    void freeMemory()
-    {
-      size_t newFront = initial_capacity_;
-      T* newData = new T[capacity_ / capacity_change_factor_];
-      std::copy(data_ + front_, data_ + rear_ + 1, newData);
-      rear_ = rear_ - front_;
-      front_ = 0;
-      capacity_ /= capacity_change_factor_;
-      delete[] data_;
-      data_ = newData;
-    }
+    private:
+      T* data_;
+      size_t size_;
+      size_t front_;
+      size_t rear_;
+      size_t capacity_;
+      const size_t initial_capacity_ = 3;
+      const size_t capacity_change_factor_ = 2;
+
+      void addMemory()
+      {
+        T* newData = new T[capacity_ * capacity_change_factor_];
+        std::copy(data_, data_ + capacity_, newData);
+        capacity_ *= capacity_change_factor_;
+        delete[] data_;
+        data_ = newData;
+      }
+
+      void freeMemory()
+      {
+        size_t newFront = initial_capacity_;
+        T* newData = new T[capacity_ / capacity_change_factor_];
+        std::copy(data_ + front_, data_ + rear_ + 1, newData);
+        rear_ = rear_ - front_;
+        front_ = 0;
+        capacity_ /= capacity_change_factor_;
+        delete[] data_;
+        data_ = newData;
+      }
   };
 }
 
--- belokurskaya.sofia/S2/stack.hpp
+++ belokurskaya.sofia/S2/stack.hpp
@@ -8,120 +8,119 @@ namespace belokurskaya
   template< class T >
   class Stack
   {
-  public:
-    Stack()
-    {
-      capacity_ = initial_capacity_;
-      size_ = 0;
-      top_ = -1;
-      data_ = new T[capacity_];
-    }
-
-    Stack(const Stack< T >& other):
-      size_(other.size_),
-      top_(other.top_),
-      capacity_(other.capacity_),
-      data_(new T[other.capacity_])
-    {
-      std::copy(other.data_, other.data_ + other.capacity_, data_);
-    }
-
-    Stack(Stack< T >&& other):
-      size_(other.size_),
-      top_(other.top_),
-      capacity_(other.capacity_),
-      data_(other.data_)
-    {
-      other.capacity_ = initial_capacity_;
-      other.size_ = 0;
-      other.top_ = -1;
-      other.data_ = new T[capacity_];
-    }
-
-    ~Stack()
-    {
-      delete[] data_;
-    }
-
-    void push(const T& value)
-    {
-      if (top_ == capacity_ - 1)
+    public:
+      Stack()
       {
-        addMemory();
+        capacity_ = initial_capacity_;
+        size_ = 0;
+        top_ = -1;
+        data_ = new T[capacity_];
       }
-      size_++;
-      data_[++top_] = value;
-    }
 
-    T pop()
-    {
-      if (top_ == -1)
+      Stack(const Stack< T >& other):
+        size_(other.size_),
+        top_(other.top_),
+        capacity_(other.capacity_),
+        data_(new T[other.capacity_])
       {
-        throw std::runtime_error("Stack is empty");
+        std::copy(other.data_, other.data_ + other.capacity_, data_);
       }
-      T value = data_[top_];
-      if (top_ == capacity_ / capacity_change_factor_ && capacity_ != initial_capacity_)
+
+      Stack(Stack< T >&& other):
+        size_(other.size_),
+        top_(other.top_),
+        capacity_(other.capacity_),
+        data_(other.data_)
       {
-        freeMemory();
+        other.capacity_ = initial_capacity_;
+        other.size_ = 0;
+        other.top_ = -1;
+        other.data_ = new T[capacity_];
       }
-      top_--;
-      size_--;
-      return value;
-    }
 
-    bool empty()
-    {
-      return (top_ == -1);
-    }
-
-    T top() const
-    {
-      if (top_ == -1)
+      ~Stack()
       {
-        throw std::runtime_error("Stack is empty");
+        delete[] data_;
       }
-      return data_[top_];
-    }
 
-    size_t size()
-    {
-      return size_;
-    }
-
-    friend std::ostream& operator<<(std::ostream& out, const Stack< T >& stack)
-    {
-      for (size_t i = 0; i <= stack.top_; ++i)
+      void push(const T& value)
       {
-        out << stack.data_[i] << " ";
+        if (top_ >= capacity_ - 1)
+        {
+          addMemory();
+        }
+        size_++;
+        data_[++top_] = value;
       }
-      return out;
-    }
 
-  private:
-    T* data_;
-    size_t size_;
-    size_t top_;
-    size_t capacity_;
-    const size_t initial_capacity_ = 3;
-    const size_t capacity_change_factor_ = 2;
+      T pop()
+      {
+        if (empty())
+        {
+          throw std::runtime_error("Stack is empty");
+        }
+        T value = data_[top_];
+        top_--;
+        if (top_ < 0)
+        {
+          top_ = -1;
+        }
+        return value;
+      }
 
-    void addMemory()
-    {
-      T* newData = new T[capacity_ * capacity_change_factor_];
-      std::copy(data_, data_ + capacity_, newData);
-      capacity_ *= capacity_change_factor_;
-      delete[] data_;
-      data_ = newData;
-    }
+      bool empty() const
+      {
+        return (top_ == -1);
+      }
 
-    void freeMemory()
-    {
-      T* newData = new T[capacity_ / capacity_change_factor_];
-      std::copy(data_, data_ + capacity_ / capacity_change_factor_, newData);
-      capacity_ /= capacity_change_factor_;
-      delete[] data_;
-      data_ = newData;
-    }
+      T top() const
+      {
+        if (empty())
+        {
+          throw std::runtime_error("Stack is empty");
+        }
+        return data_[top_];
+      }
+
+      size_t size()
+      {
+        return size_;
+      }
+
+      friend std::ostream& operator<<(std::ostream& out, const Stack< T >& stack)
+      {
+        for (size_t i = 0; i <= stack.top_; ++i)
+        {
+          out << stack.data_[i] << " ";
+        }
+        return out;
+      }
+
+    private:
+      T* data_;
+      size_t size_;
+      int top_;
+      size_t capacity_;
+      const size_t initial_capacity_ = 3;
+      const size_t capacity_change_factor_ = 2;
+
+      void addMemory()
+      {
+        T* newData = new T[capacity_ * capacity_change_factor_];
+        std::copy(data_, data_ + capacity_, newData);
+        capacity_ *= capacity_change_factor_;
+        delete[] data_;
+        data_ = newData;
+      }
+
+      void freeMemory()
+      {
+        T* newData = new T[capacity_ / capacity_change_factor_];
+        std::copy(data_, data_ + capacity_ / capacity_change_factor_, newData);
+        capacity_ /= capacity_change_factor_;
+        delete[] data_;
+        data_ = newData;
+      }
   };
 }
 
