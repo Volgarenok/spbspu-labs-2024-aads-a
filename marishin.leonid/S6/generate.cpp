e88adbfb932ffcc0d0a177afc32d8544b857644d
marlions <marishin.lv@edu.spbstu.ru>
2024-06-07 09:24:41 +0300

    fix

--- marishin.leonid/S6/generate.cpp
+++
@@ -1,8 +0,0 @@
-#include "generate.hpp"
-#include <cstdlib>
-
-float marishin::detail::randomNumber(std::default_random_engine& generate, float)
-{
-  std::uniform_real_distribution< float > dis(-1000, 1000);
-  return dis(generate);
-}
