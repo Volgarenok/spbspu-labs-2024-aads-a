f5f48826c1a7542ac38dcb6e3cba4c783cd820f9
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-07-07 18:54:08 +1000

    Iterator Story 1

--- vyzhanov.daniil/S1/iterator.hpp
+++ vyzhanov.daniil/S1/iterator.hpp
@@ -6,21 +6,112 @@
 
 namespace vyzhanov
 {
-	template< class T >
-	class List;
+  template< typename T >
+  class List;
 
-	template < typename T >
-	class Iterator : public std::iterator< std::bidirectional_iterator_tag, T >
-	{
-	  friend List< T >;
-	  using node = Node< T >;
-	  using iterator = Iterator< T >;
-	public:
-	  Iterator() = default;
-	  Iterator(const iterator& it) = default;
-	  Iterator(iterator&& it) = default;
-	  ~Iterator() = default;
-	};
+  template< typename T >
+  class BiIterator: public std::iterator< std::bidirectional_iterator_tag, T >
+  {
+    friend List< T >;
+    using iterator = BiIterator< T >;
+    BiIterator() = default;
+    BiIterator(const iterator&) = default;
+    ~BiIterator() = default;
+
+    iterator& operator=(const iterator&) = default;
+    bool operator==(const iterator&) const;
+    bool operator!=(const iterator&) const;
+
+    iterator& operator++();
+    iterator& operator--();
+    iterator operator++(int);
+    iterator operator--(int);
+
+    T& operator*();
+    T* operator->();
+    const T& operator*() const;
+    const T* operator->() const;
+  private:
+    Node< T >* node_;
+    explicit BiIterator(Node< T >* node);
+  };
+
+  template< typename T >
+  BiIterator< T >::BiIterator() :
+    node_(nullptr)
+  {}
+
+  template< typename T >
+  BiIterator< T >::BiIterator(Node< T >* node) :
+    node_(node)
+  {}
+
+  template < typename T >
+  bool BiIterator< T >::operator==(const iterator& obj) const
+  {
+    return node_ == obj.node_;
+  }
+
+  template< typename T >
+  bool BiIterator< T >::operator!=(const iterator& obj) const
+  {
+    return !(node_ == obj.node_);
+  }
+
+  template< typename T >
+  BiIterator< T >& BiIterator< T >::operator++()
+  {
+    node_ = node_->next_;
+    return *this;
+  }
+
+  template< typename T >
+  BiIterator< T >& BiIterator< T >::operator--()
+  {
+    node_ = node_->prev_;
+    return *this;
+  }
+
+  template< typename T >
+  BiIterator< T > BiIterator< T >::operator++(int)
+  {
+    BiIterator< T > temp = *this;
+    node_ = node_->next_;
+    return temp;
+  }
+
+  template< typename T >
+  BiIterator< T > BiIterator< T >::operator--(int)
+  {
+    BiIterator< T > temp = *this
+    node_ = node_->prev_;
+    return *this;
+  }
+
+  template< class T >
+  T& BiIterator< T >::operator*()
+  {
+    return node_->data_;
+  }
+
+  template< class T >
+  T* BiIterator< T >::operator->()
+  {
+    return std::addressof(node_->data_);
+  }
+
+  template< class T >
+  const T& BiIterator< T >::operator*() const
+  {
+    return node_->data_;
+  }
+
+  template< class T >
+  const T* BiIterator< T >::operator->() const
+  {
+     return std::addressof(node_->data_);
+  }
 
 }
+
 #endif
