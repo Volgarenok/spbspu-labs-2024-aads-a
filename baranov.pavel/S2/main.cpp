3808238f57fb272e674b1c38a76c07c7be19b0f7
pashko2303 <baranov-p.b@yandex.ru>
2024-07-08 14:14:59 +0300

    Reading expressions from file

--- baranov.pavel/S2/main.cpp
+++ baranov.pavel/S2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "stack.hpp"
 #include "queue.hpp"
@@ -7,15 +8,26 @@
 #include "calculatePostfix.hpp"
 #include "outputFunction.hpp"
 
-int main()
+int main(int argc, char * argv [])
 {
   using namespace baranov;
   Queue< Queue< Token > > postfixes;
   Stack< long long int > results;
-
   try
   {
-    inputPostfixes(std::cin, postfixes);
+    if (argc == 2)
+    {
+      std::ifstream file(argv[1]);
+      if (!file.is_open())
+      {
+        throw std::logic_error("Cannot open input file\n");
+      }
+      inputPostfixes(file, postfixes);
+    }
+    else
+    {
+      inputPostfixes(std::cin, postfixes);
+    }
     calculatePostfixes(postfixes, results);
   }
   catch (const std::exception & e)
