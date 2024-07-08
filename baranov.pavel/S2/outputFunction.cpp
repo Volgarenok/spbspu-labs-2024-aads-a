0eeb38af73310acac0d5aae60f09b4d18739dc70
pashko2303 <baranov-p.b@yandex.ru>
2024-07-08 14:20:33 +0300

    Fix output function

--- baranov.pavel/S2/outputFunction.cpp
+++ baranov.pavel/S2/outputFunction.cpp
@@ -2,6 +2,10 @@
 
 void baranov::printResults(std::ostream & out, Stack< long long int > & results)
 {
+  if (results.empty())
+  {
+    return;
+  }
   out << results.top();
   results.pop();
   while (!results.empty())
