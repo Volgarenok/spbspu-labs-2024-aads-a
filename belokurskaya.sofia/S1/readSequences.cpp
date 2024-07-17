6d6ca0960dc86cdefc203b8c67fe8b754b094ee7
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-07-17 21:14:39 +0300

    fixing

--- belokurskaya.sofia/S1/readSequences.cpp
+++ belokurskaya.sofia/S1/readSequences.cpp
@@ -6,48 +6,42 @@
 void belokurskaya::readSequences(std::istream& in, List< std::pair< std::string, List< size_t > > >& sequences)
 {
   std::string line;
-  List<std::pair< std::string, List< size_t > > > sequenceList;
 
   while (std::getline(in, line))
   {
     if (line.empty()) continue;
 
     std::string name;
-    size_t index = 0;
+    List< size_t > numbers;
 
+    size_t index = 0;
     while (index < line.size() && line[index] != ' ')
     {
       name.push_back(line[index]);
       ++index;
     }
 
-    List< size_t > sequence;
-
-    while (index < line.size() && line[index] == ' ')
-    {
-      ++index;
-    }
-
     while (index < line.size())
     {
-      size_t number = 0;
-      while (index < line.size() && line[index] >= '0' && line[index] <= '9')
-      {
-        number = number * 10 + (line[index] - '0');
-        ++index;
-      }
-      sequence.push_front(number);
-
+      std::string sequences;
       while (index < line.size() && line[index] == ' ')
       {
         ++index;
       }
+
+      while (index < line.size() && line[index] >= '0' && line[index] <= '9')
+      {
+        sequences.push_back(line[index]);
+        ++index;
+      }
+
+      if (!sequences.empty())
+      {
+        numbers.push_back(std::stoi(sequences));
+      }
     }
-    sequenceList.push_front( {name, sequence} );
-  }
-  for (const auto& entry : sequenceList)
-  {
-    sequences.push_front(entry);
+
+    sequences.push_front({name, numbers});
   }
 }
 
