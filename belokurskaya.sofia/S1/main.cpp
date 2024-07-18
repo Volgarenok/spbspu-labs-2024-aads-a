e04c687c78367be59b2d330fdfd8a905f650b21b
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-07-18 13:59:39 +0300

    conflicts resolving

--- belokurskaya.sofia/S1/main.cpp
+++ belokurskaya.sofia/S1/main.cpp
@@ -1,64 +1,43 @@
-﻿#include <iostream>
+﻿﻿#include <iostream>
 #include <sstream>
 #include <string>
 #include <stdexcept>
 
-#include "SequenceVector.hpp"
 #include "readSequences.hpp"
+#include "resultsPrinter.hpp"
 
 int main()
 {
-	using namespace belokurskaya;
-	try
-	{
-		SequenceVector sequences = readSequences();
+  using namespace belokurskaya;
+  try
+  {
+    List< std::pair< std::string, List< size_t > > > sequences;
 
-		if (sequences.getSize() == 0)
-		{
-			std::cout << "0\n";
-			throw std::runtime_error("No sequences provided");
-		}
+    readSequences(std::cin, sequences);
+    if (sequences.empty())
+    {
+      std::cout << 0 << '\n';
+      return 0;
+    }
 
-		for (size_t i = 0; i < sequences.getSize(); ++i)
-		{
-			std::cout << sequences[i].getName() << " ";
-		}
-		std::cout << "\n";
+    printNames(std::cout, sequences);
+    std::cout << '\n';
 
-		size_t maxLength = 0;
-		for (size_t i = 0; i < sequences.getSize(); ++i)
-		{
-			maxLength = std::max(maxLength, sequences[i].getSequence().size());
-		}
+    List< List < size_t > > nums;
+    readElem(nums, sequences);
+    if (!nums.empty())
+    {
+      printSequences(std::cout, nums);
+      std::cout << '\n';
+    }
 
-		for (size_t j = 0; j < maxLength; ++j)
-		{
-			for (size_t i = 0; i < sequences.getSize(); ++i)
-			{
-				const List< int > & seq = sequences[i].getSequence();
-				if (j < seq.size())
-				{
-					std::cout << seq.at(j) << " ";
-				}
-			}
-		}
-		std::cout << "\n";
+    printSums(std::cout, nums);
+    std::cout << '\n';
+  }
 
-		for (size_t i = 0; i < sequences.getSize(); ++i)
-		{
-			const List< int > & seq = sequences[i].getSequence();
-			int sum = 0;
-			for (size_t j = 0; j < seq.size(); ++j)
-			{
-				sum += seq.at(j);
-			}
-			std::cout << sum << " ";
-		}
-		std::cout << "\n";
-	}
-	catch (const std::exception & e)
-	{
-		std::cerr << e.what() << "\n";
-		return 1;
-	}
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
 }
