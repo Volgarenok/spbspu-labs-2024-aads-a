e21d33a50b6729672c30b584ff288843863f4542
jiggerman <liggerman@mail.ru>
2024-07-27 18:37:51 +0300

    Add stack

---
+++ chistyakov.vladislav/S2/stack.hpp
@@ -0,0 +1,15 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "list.hpp"
+
+namespace chistyakov
+{
+  template< typename T >
+  class Stack
+  {
+  public:
+  private:
+    List< T > stack_;
+  }
+}
