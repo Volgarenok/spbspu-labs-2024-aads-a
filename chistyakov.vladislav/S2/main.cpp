1325ee74e8b57a4a7e991ac2c907217e6eec9b74
jiggerman <liggerman@mail.ru>
2024-07-30 15:25:14 +0300

    Check in main

--- chistyakov.vladislav/S2/main.cpp
+++ chistyakov.vladislav/S2/main.cpp
@@ -1,2 +1,23 @@
+#include <iostream>
+#include <string>
+#include "queue.hpp"
+#include "stack.hpp"
+#include "infixToPostfix.hpp"
+#include "input.hpp"
+#include "calculate.hpp"
+
 int main()
-{}
+{
+  using namespace chistyakov;
+
+  Queue< std::string > exp;
+  std::string a = "5*6+(2-9)";
+  std::cout << a << "\n";
+
+  inputExp(exp, a);
+  Queue< std::string > result;
+  infixToPostfix(result, exp);
+  int resultNum = calculate(result);
+
+  std::cout << "Result: " << resultNum << "\n";
+}
