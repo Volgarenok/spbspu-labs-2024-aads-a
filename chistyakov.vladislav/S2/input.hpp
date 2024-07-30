4af8e154cd574349b7cea99e357c84f87256b038
jiggerman <liggerman@mail.ru>
2024-07-30 15:26:23 +0300

    Add input

---
+++ chistyakov.vladislav/S2/input.hpp
@@ -0,0 +1,12 @@
+#ifndef INPUT_HPP
+#define INPUT_HPP
+
+#include <string>
+#include "queue.hpp"
+
+namespace chistyakov
+{
+  void inputExp(Queue< std::string > & queue, const std::string & exp);
+}
+
+#endif
