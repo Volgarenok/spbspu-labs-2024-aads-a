c1435e5bd1c2d5ec08343835ac018c920fb2cf82
jiggerman <liggerman@mail.ru>
2024-07-31 22:20:04 +0300

    fix input.hpp

--- chistyakov.vladislav/S2/input.cpp
+++ chistyakov.vladislav/S2/input.cpp
@@ -1,41 +1,56 @@
 #include "input.hpp"
 
-void chistyakov::inputExp(Queue< std::string > & queue, const std::string & exp)
+void chistyakov::inputExp(Stack< Queue< std::string > > & result, std::istream & in)
 {
-  std::string number;
-  for (size_t i = 0; i < exp.size(); i++)
+  while (in)
   {
-    if (isdigit(exp[i]))
-    {
-      number += exp[i];
-    }
-    else
-    {
-      if (!number.empty())
-      {
-        queue.push(number);
-        number = "";
-      }
+    std::string exp;
+    std::getline(in, exp);
 
-      if (exp[i] == ' ')
+    if (!in || exp == "")
+    {
+      continue;
+    }
+
+    Queue< std::string > queue;
+    std::string number;
+
+    for (size_t i = 0; i < exp.size(); i++)
+    {
+      if (isdigit(exp[i]))
       {
-        continue;
-      }
-      else if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/' || exp[i] == '%' || exp[i] == '(' || exp[i] == ')')
-      {
-        std::string sign;
-        sign += exp[i];
-        queue.push(sign);
+        number += exp[i];
       }
       else
       {
-        std::cerr << "Bad input\n";
+        if (!number.empty())
+        {
+          queue.push(number);
+          number = "";
+        }
+
+        if (exp[i] == ' ')
+        {
+          continue;
+        }
+        else if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/' || exp[i] == '%' || exp[i] == '(' || exp[i] == ')')
+        {
+          std::string sign;
+          sign += exp[i];
+          queue.push(sign);
+        }
+        else
+        {
+          std::cerr << "Bad input\n";
+        }
       }
     }
-  }
 
-  if (!number.empty())
-  {
-    queue.push(number);
+    if (!number.empty())
+    {
+      queue.push(number);
+    }
+
+    result.push(queue);
   }
 }
