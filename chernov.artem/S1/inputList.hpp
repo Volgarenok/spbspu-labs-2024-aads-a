00830c0b7bf5fab010165382a2a57f062952f35a
CA21lmFlame <chernov.ar@spbstu.ru>
2024-08-07 11:52:53 +0300

    added input

---
+++ chernov.artem/S1/inputList.hpp
@@ -0,0 +1,13 @@
+#ifndef INPUT_LIST_HPP
+#define	INPUT_LIST_HPP
+#include "forwardList.hpp"
+#include <istream>
+#include <cstddef>
+
+namespace chernov
+{
+  using namedNumberedList = std::pair< std::string, List< size_t > >;
+  void inputLists(std::istream& input, List< namedNumberedList >& list);
+}
+
+#endif
