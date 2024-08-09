a6a32644a9e03eb87776c7a2e0d2bea3086d7a33
pashko2303 <baranov-p.b@yandex.ru>
2024-08-09 14:11:50 +0300

    Node predicates and increment for iterator

--- baranov.pavel/S4/iterator.hpp
+++ baranov.pavel/S4/iterator.hpp
@@ -14,13 +14,47 @@ namespace baranov
   public:
     friend class Tree< Key, T, Compare >;
     using this_t = Iterator< Key, T, Compare >;
+
     Iterator();
     ~Iterator() = default;
     Iterator(const this_t &) = default;
     this_t & operator=(const this_t &) = default;
+
+    this_t & operator++();
+    this_t operator++(int);
   private:
     Node< Key, T > * node_;
   };
+
+  template< typename Key, typename T, typename Compare >
+  Iterator< Key, T, Compare > & Iterator< Key, T, Compare >::operator++()
+  {
+    if (node_->hasRight())
+    {
+      node_ = node_->right_;
+      while (node_->hasLeft())
+      {
+        node_ = node_->left_;
+      }
+    }
+    else
+    {
+      while (node_->isRight())
+      {
+        node_ = node_->parent_;
+      }
+      node_ = node_->parent_;
+    }
+    return * this;
+  }
+
+  template< typename Key, typename T, typename Compare >
+  Iterator< Key, T, Compare > Iterator< Key, T, Compare >::operator++(int)
+  {
+    Iterator< Key, T, Compare > result(*this);
+    ++(*this);
+    return result;
+  }
 }
 
 #endif
--- baranov.pavel/S4/node.hpp
+++ baranov.pavel/S4/node.hpp
@@ -11,6 +11,31 @@ namespace baranov
     Node< Key, T > * left_;
     Node< Key, T > * right_;
     Node< Key, T  > * parent_;
+
+    bool hasLeft() const noexcept
+    {
+      return parent_->left_;
+    }
+
+    bool hasRight() const noexcept
+    {
+      return parent_->right_;
+    }
+
+    bool isRoot() const noexcept
+    {
+      return parent_ == nullptr;
+    }
+
+    bool isLeft() const noexcept
+    {
+      return !isRoot && parent_->left_ == this;
+    }
+
+    bool isRight() const noexcept
+    {
+      return !isRoot && parent_->right_ == this;
+    }
   };
 }
 
