8e468e3185d27e354824c407a1fba837f8e89b54
MayaSakovskaya <zloyejik05@gmail.com>
2024-08-15 16:15:22 +0400

    main.cpp....

--- sakovskaia.maiia/S1/main.cpp
+++ sakovskaia.maiia/S1/main.cpp
@@ -1,2 +1,29 @@
+#include "data_processing.hpp"
+#include "input.hpp"
+#include "list.hpp"
+#include "output.hpp"
+#include <iostream>
 int main()
-{}
+{
+  using namespace sakovskaia;
+  List< std::pair< std::string, ullList > > sequences;
+  input(std::cin, sequences);
+  if (sequences.empty())
+  {
+    std::cout << "0" << "\n";
+    return 0;
+  }
+  outputSequences(sequences);
+  List< ullList > processedData = processData(sequences);
+  outputProcessedData(processedData);
+  try
+  {
+    List< unsigned long long int > sums = calculateSums(processedData);
+    outputSums(sums);
+  }
+  catch (const std::overflow_error &)
+  {
+    std::cerr << "Calculating sum is impossible" << "\n";
+    return 1;
+  }
+}
