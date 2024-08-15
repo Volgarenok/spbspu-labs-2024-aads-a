f5038a3ad7784d6a1229546ef16b3849e25157b8
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-08-16 00:15:03 +0300

    added commands in main

--- belokurskaya.sofia/S4/main.cpp
+++ belokurskaya.sofia/S4/main.cpp
@@ -1,11 +1,15 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
 
 #include "binarySearchTree.hpp"
 
 int main(int argc, char* argv[])
 {
   using namespace belokurskaya;
+
+  using Dictionary = BinarySearchTree< int, std::string >;
+  using DictionaryCollection = BinarySearchTree< std::string, Dictionary >;
   if (argc != 2)
   {
     std::cerr << "No args\n";
@@ -16,5 +20,24 @@ int main(int argc, char* argv[])
   {
     std::cerr << "Cannot open file\n";
     return 1;
+  }
+  DictionaryCollection dictionaries;
+  std::string line;
+
+  while (std::getline(file, line))
+  {
+    if (line.empty())
+    {
+      continue;
+    }
+  }
+  using fun = std::function< void(DictionaryCollection&, const std::string&, const std::string&, const std::string&) >;
+
+  BinarySearchTree< std::string, fun > cmds;
+  {
+    using namespace std::placeholders;
+    cmds["complement"] = std::bind(&complement, _1, _2, _3, std::placeholders::_4);
+    cmds["intersect"] = std::bind(&intersect, _1, _2, _3, _4);
+    cmds["union"] = std::bind(&unionDictionaries, _1, _2, _3, _4);
   }
 }
