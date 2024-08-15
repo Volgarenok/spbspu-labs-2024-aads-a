907ac28f534947eae9fdc131c046d7afed36cb1f
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-08-15 23:33:15 +0300

    added reading from file

--- belokurskaya.sofia/S4/main.cpp
+++ belokurskaya.sofia/S4/main.cpp
@@ -1,6 +1,20 @@
 ﻿#include <iostream>
-#include "binarySearchTree.hpp"
-int main()
-{
+#include <fstream>
 
+#include "binarySearchTree.hpp"
+
+int main(int argc, char* argv[])
+{
+  using namespace belokurskaya;
+  if (argc != 2)
+  {
+    std::cerr << "No args\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "Cannot open file\n";
+    return 1;
+  }
 }
