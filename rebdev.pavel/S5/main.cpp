a099f835e0a36d84da780db7dba7b1115c7f81e3
User1235321 <pavreb1235321@gmail.com>
2024-08-15 11:50:36 +0300

    commit to switch branch on S2

--- rebdev.pavel/S5/main.cpp
+++ rebdev.pavel/S5/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char ** argv)
 {
-
+  if (argc != 3)
+  {
+    str::cerr << "Bad number of input arguments!\n";
+    return 1;
+  }
+  std::ifstream inFile(argv[1]);
+  if ((!inFile.open()) || (inFile.eof()))
+  {
+    str::cerr << "Empty tree file!\n";
+    return 1;
+  }
+  switch(argv[2])
+  {
+    case "ascending":
+      //some work
+      break;
+    case "descending":
+      //some work
+      break;
+    case "breadth":
+      //some work
+      break;
+    default:
+      std::cerr << "Bad argument input!\n";
+      return 1;
+  }
   return 0;
 }
