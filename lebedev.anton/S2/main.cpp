de2cbf5dab4b06e91f4c4c925860f8eab96394b0
IastWish <antony_lebedevv@mail.ru>
2024-08-19 17:54:29 +0300

    Working with symbols to input

--- lebedev.anton/S2/main.cpp
+++ lebedev.anton/S2/main.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
+#include "expression_types.hpp"
+#include "symbols.hpp"
 #include "stack.hpp"
 #include "queue.hpp"
 
 int main()
 {
   using namespace lebedev;
-  Queue< int > stack;
-  stack.push(1);
-  stack.push(2);
-  stack.push(3);
-  stack.push(4);
-  std::cout << stack.front() << '\n';
-  std::cout << stack.back() << '\n';
-  stack.pop();
-  std::cout << stack.front() << '\n';
-  std::cout << stack.back() << '\n';
-  std::cout << stack.empty() << '\n';
-  std::cout << stack.size() << '\n';
 }
