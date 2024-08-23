0c3f735e9672bdf7479efb1adaecbf2cec6d3277
pashko2303 <baranov-p.b@yandex.ru>
2024-08-23 20:01:37 +0300

    Fix ascending

--- baranov.pavel/S5/commands.cpp
+++ baranov.pavel/S5/commands.cpp
@@ -5,7 +5,7 @@
 void baranov::ascending(std::ostream & out, const Tree< int, std::string > & tree)
 {
   Summator sum;
-  tree.traverse_lnr(sum);
+  sum = tree.traverse_lnr(sum);
   int resultKey = sum.getResultKey();
   std::string resultValue = sum.getResultValue();
   if (resultValue.empty())
