a5080c71e1ff2bef34f60032ef3e97bc96335dc4
CA21lmFlame <chernov.ar@spbstu.ru>
2024-08-25 14:28:18 +0300

    implemented the infix input

--- chernov.artem/S2/infixToPostfix.cpp
+++ chernov.artem/S2/infixToPostfix.cpp
@@ -1,4 +1,34 @@
 #include "infixToPostfix.hpp"
+#include <string>
+#include <cstddef>
 
 void chernov::inputInfix(std::istream& in, Queue< queue >& infix_queue)
-{}
+{
+  std::string str = "";
+  std::string detail = "";
+  while (std::getline(in, str, '\n'))
+  {
+    queue temp;
+    detail = "";
+    if (!str.empty())
+    {
+      for (size_t j = 0; j < str.length(); j++)
+      {
+        if (!std::isspace(str[j]))
+        {
+          detail += str[j];
+          if (str.length() - 1 == j)
+          {
+	    temp.push(detail);
+          }
+        }
+        else if (!detail.empty())
+        {
+          temp.push(detail);
+          detail = "";
+        }
+      }
+      infix_queue.push(temp);
+    }
+  }
+}
