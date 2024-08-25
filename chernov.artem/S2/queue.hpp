d5e77d20425c654641e0aca8183cac7a77c54af8
CA21lmFlame <chernov.ar@spbstu.ru>
2024-08-25 14:16:09 +0300

    added a queue

---
+++ chernov.artem/S2/queue.hpp
@@ -0,0 +1,54 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+#include "forwardList.hpp"
+#include <cstddef>
+
+namespace chernov
+{
+  template < class T >
+  class Queue
+  {
+  public:
+    bool empty()
+    {
+      return list_.isEmpty();
+    }
+    size_t size()
+    {
+      return list_.getSize();
+    }
+    T& front()
+    {
+      return list_.front();
+    }
+    const T& front() const
+    {
+      return list_.front();
+    }
+    T& back()
+    {
+      return list_.back();
+    }
+    const T& back() const
+    {
+      return list_.back();
+    }
+    void print() const
+    {
+      return list_.print();
+    }
+    void pop()
+    {
+      return list_.pop_back();
+    }
+    void push(const T& value)
+    {
+      return list_.push_front(value);
+    }
+    ~Queue() = default;
+  private:
+    List< T > list_;
+  };
+}
+
+#endif
