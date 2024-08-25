c6c50fc0202a565e01c7f6f90f270099c6b609f3
CA21lmFlame <chernov.ar@spbstu.ru>
2024-08-25 14:19:18 +0300

    added a stack

---
+++ chernov.artem/S2/stack.hpp
@@ -0,0 +1,46 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+#include "forwardList.hpp"
+#include <cstddef>
+
+namespace chernov
+{
+  template < class T >
+  class Stack
+  {
+  public:
+    bool empty()
+    {
+      return list_.isEmpty();
+    }
+    size_t size()
+    {
+      return list_.getSize();
+    }
+    T& top()
+    {
+      return list_.front();
+    }
+    const T& top() const
+    {
+      return list_.front();
+    }
+    void pop()
+    {
+      return list_.pop_front();
+    }
+    void push(const T& value)
+    {
+      return list_.push_front(value);
+    }
+    void print() const
+    {
+      return list_.print();
+    }
+    ~Stack() = default;
+  private:
+    List< T > list_;
+  };
+}
+
+#endif
