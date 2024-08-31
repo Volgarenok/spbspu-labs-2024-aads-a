df284f1e765a5592f541398eec1ce59c5b909bf9
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-08-31 19:36:13 +0300

    added args checking

--- belokurskaya.sofia/S6/main.cpp
+++ belokurskaya.sofia/S6/main.cpp
@@ -1,5 +1,16 @@
 ﻿#include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc != 4)
+  {
+    std::cerr << "Invalid args\n";
+    return 1;
+  }
+  size_t size = std::atoi(argv[3]);
+  if (size == 0)
+  {
+    std::cerr << "Invalid size\n";
+    return 1;
+  }
 }
