41223804a6ff5f0da51a5d879c1fc0f223cf7ee5
Skuratov09 <skyratovdaniil28@gmail.com>
2024-09-01 21:55:17 +0300

    Created ints sort type and etc

--- skuratov.danil/S6/main.cpp
+++ skuratov.danil/S6/main.cpp
@@ -112,13 +112,20 @@ void printContainer(const std::forward_list< T >& container)
   std::cout << "\n";
 }
 
-int main(/*int argc, char* argv[]*/)
+int main(int argc, char* argv[])
 {
-  /*if (argc != 4)
+  using namespace skuratov;
+  if (argc != 4)
   {
     std::cerr << "Not enough arguments" << '\n';
     return 1;
-  }*/
+  }
+
+  std::string sortType = argv[1];
+  std::string order = argv[2];
+  std::string type = argv[3];
+  size_t size = std::stoi(argv[4]);
+  bool ascending = order == "ascending";
 
   srand(static_cast< unsigned int >(time(0)));
 
@@ -126,11 +133,62 @@ int main(/*int argc, char* argv[]*/)
   List< int > dblList;
   std::forward_list< int > fwdList;
 
-  for (int i = 0; i < 10; ++i)
+  if (type == "ints")
   {
-    int randomNumber = rand() % 100;
-    std::cout << randomNumber << " ";
-  }
+    generateRandomNumbers(deq, size);
+    for (const auto& number : deq)
+    {
+      dblList.pushBack(number);
+      fwdList.push_front(number);
+    }
+  
+    if (sortType == "shell")
+    {
+      if (ascending)
+      {
+        shellSort(deq.begin(), deq.end(), std::less< int >());
+        shellSort(dblList.ñbegin(), dblList.ñend(), std::less< int >());
+        shellSort(fwdList.begin(), fwdList.end(), std::less< int >());
+      }
+      else
+      {
+        shellSort(deq.begin(), deq.end(), std::greater< int >());
+        shellSort(dblList.ñbegin(), dblList.ñend(), std::greater< int >());
+        shellSort(fwdList.begin(), fwdList.end(), std::greater< int >());
+      }
+    }
+    else if (sortType == "shaker")
+    {
+      if (ascending)
+      {
+        shakerSort(deq.begin(), deq.end(), std::less< int >());
+        shakerSort(dblList.ñbegin(), dblList.ñend(), std::less< int >());
+        shakerSort(fwdList.begin(), fwdList.end(), std::less< int >());
+      }
+      else
+      {
+        shakerSort(deq.begin(), deq.end(), std::greater< int >());
+        shakerSort(dblList.ñbegin(), dblList.ñend(), std::greater< int >());
+        shakerSort(fwdList.begin(), fwdList.end(), std::greater< int >());
+      }
+    }
+    else
+    {
+      std::cerr << "Invalid sort type" << '\n';
+      return 1;
+    }
 
+    std::cout << "Sorted using std::deque: ";
+    printContainer(deq);
+    std::cout << "Sorted using custom List: ";
+    printContainer(dblList);
+    std::cout << "Sorted using std::forward_list: ";
+    printContainer(fwdList);
+  }
+  else
+  {
+    std::cerr << "Invalid type" << '\n';
+    return 1;
+  }
   return 0;
 }
