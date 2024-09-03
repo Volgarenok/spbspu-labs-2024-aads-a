aa9971ee09fdd9fc7c10da399012cd2d302abbe9
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-09-03 14:40:50 +0300

    use List

--- vyzhanov.daniil/S2/queue.hpp
+++ vyzhanov.daniil/S2/queue.hpp
@@ -1,7 +1,7 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
-#include </Users/Daniil/source/repos/fwd_list/fwd_list/List.hpp>
+#include <common/List.hpp>
 
 namespace vyzhanov
 {
--- vyzhanov.daniil/S2/stack.hpp
+++ vyzhanov.daniil/S2/stack.hpp
@@ -1,7 +1,7 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
-#include </Users/Daniil/source/repos/fwd_list/fwd_list/List.hpp>
+#include <common/List.hpp>
 
 namespace vyzhanov
 {
