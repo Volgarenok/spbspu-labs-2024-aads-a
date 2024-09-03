16eb2c57739852dc4dcffa7408b2b04abdbd5041
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-09-03 19:46:46 +0300

    fix

--- vyzhanov.daniil/S2/calculatePostfix.cpp
+++ vyzhanov.daniil/S2/calculatePostfix.cpp
@@ -40,20 +40,23 @@ void vyzhanov::calculatePostfix(Queue< Queue< Token > >& expressions, Stack< lon
       Token it = curr.top();
       long long operand1 = 0;
       long long operand2 = 0;
-      long long res = 0;
       if (it.getType() == Type::OPERAND)
       {
         newStack.push(it.getOperand());
       }
       else if (it.getType() == Type::OPERATION)
       {
+        if (newStack.capacity() < 2)
+        {
+          throw std::invalid_argument("incorrect expression");
+        }
         operand2 = newStack.top();
         newStack.pop();
         operand1 = newStack.top();
         newStack.pop();
         long long maxRes = std::numeric_limits< long long >::max();
         long long minRes = std::numeric_limits< long long >::min();
-        if (operand1 > maxRes - operand2 ||(operand2 != 0 && operand1 > maxRes / operand2))
+        if (operand1 > maxRes - operand2 || (operand2 != 0 && operand1 > maxRes / operand2))
         {
           throw std::out_of_range("Overflow!");
         }
@@ -61,7 +64,7 @@ void vyzhanov::calculatePostfix(Queue< Queue< Token > >& expressions, Stack< lon
         {
           throw std::out_of_range("Underflow!");
         }
-        res = performOperation(operand1, operand2, it.getOperation());
+        long long res = performOperation(operand1, operand2, it.getOperation());
         newStack.push(res);
       }
       curr.pop();
