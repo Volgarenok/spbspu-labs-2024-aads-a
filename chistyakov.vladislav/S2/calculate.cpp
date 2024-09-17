f38a383451c218af91ba3c1953a028601b1a3947
jiggerman <liggerman@mail.ru>
2024-09-17 13:47:36 +0300

    add calculate.cpp

---
+++ chistyakov.vladislav/S2/calculate.cpp
@@ -0,0 +1,46 @@
+#include "calculate.hpp"
+#include "stack.hpp"
+
+int chistyakov::calculate(Queue< std::string > & exp)
+{
+  Stack< int > stack;
+
+  while (!exp.empty())
+  {
+    if (isdigit(exp.front()[0]))
+    {
+      stack.push(std::stol(exp.front()));
+    }
+    else
+    {
+      int secondNum = stack.top();
+      stack.pop();
+      int firstNum = stack.top();
+      stack.pop();
+
+      if (exp.front() == "+")
+      {
+        stack.push(firstNum + secondNum);
+      }
+      else if (exp.front() == "-")
+      {
+        stack.push(firstNum - secondNum);
+      }
+      else if (exp.front() == "*")
+      {
+        stack.push(firstNum * secondNum);
+      }
+      else if (exp.front() == "/")
+      {
+        stack.push(firstNum / secondNum);
+      }
+      else if (exp.front() == "%")
+      {
+        stack.push(firstNum % secondNum);
+      }
+    }
+    exp.pop();
+  }
+
+  return stack.top();
+}
