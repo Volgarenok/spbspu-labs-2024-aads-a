5f7034f769d6e8001ce512d255b5fb00a2a695b2
vktrchh <viskas6969@gmail.com>
2024-09-23 21:31:29 +0300

    pull big trouble(

--- vojuck.victoria/S1/main.cpp
+++ vojuck.victoria/S1/main.cpp
@@ -1,2 +1,10 @@
+#include "list.hpp"
+#include "input.hpp"
+
 int main()
-{}
+{
+  using namespace vojuck;
+  List< paired_list > lines;
+  inputLists(std::cin, lines);
+  std::cout << "NAVALITE_HELPA\n";
+}
