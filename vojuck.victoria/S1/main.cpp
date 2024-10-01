7ea0d7a5d6eed22885b32aa6aa8af4a08f73d599
vktrchh <viskas6969@gmail.com>
2024-10-01 21:17:24 +0300

    add main

--- vojuck.victoria/S1/main.cpp
+++ vojuck.victoria/S1/main.cpp
@@ -1,12 +1,64 @@
+#include <iostream>
+#include "list.hpp"
 #include "input_output.hpp"
 
 int main()
 {
-  using namespace vojuck;
-  List< paired_list > lines;
-  inputLists(std::cin, lines);
-  List< List <size_t > > result;
-  getOrderedLists(result, lines);
-  outputOrderedLists(result);
-  std::cout << "NAVALITE_HELPA\n";
+  vojuck::List< vojuck::paired_list > inputSequences;
+  vojuck::inputLists(std::cin, inputSequences);
+
+  if (inputSequences.empty())
+  {
+    std::cout << "0" << std::endl;
+    return 0;
+  }
+
+  auto it = inputSequences.cbegin();
+  auto end = inputSequences.cend();
+  while (it != end)
+  {
+    std::cout << it->first << " ";
+    ++it;
+  }
+  std::cout << '\n';
+
+  vojuck::List< vojuck::List< size_t > > orderedSequences;
+  vojuck::getOrderedLists(orderedSequences, inputSequences);
+
+  vojuck::outputOrderedLists(orderedSequences);
+
+  try
+  {
+    vojuck::List< size_t > sums;
+    auto seqIt = orderedSequences.cbegin();
+    auto seqEnd = orderedSequences.cend();
+    while (seqIt != seqEnd)
+    {
+      size_t sum = 0;
+      auto numIt = seqIt->cbegin();
+      auto numEnd = seqIt->cend();
+      while (numIt != numEnd)
+      {
+        sum += *numIt;
+        ++numIt;
+      }
+      sums.push_back(sum);
+      ++seqIt;
+    }
+    auto sumIt = sums.cbegin();
+    auto sumEnd = sums.cend();
+    while (sumIt != sumEnd)
+    {
+      std::cout << *sumIt << " ";
+      ++sumIt;
+    }
+    std::cout << std::endl;
+  }
+  catch (...)
+  {
+    std::cerr << "Error: cannot compute sums" << std::endl;
+    return 1;
+  }
+
+  return 0;
 }
