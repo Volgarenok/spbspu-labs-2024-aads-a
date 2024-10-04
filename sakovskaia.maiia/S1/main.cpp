6de10d3274eb2abd2b1830c321d88903e9db802c
MayaSakovskaya <zloyejik05@gmail.com>
2024-10-04 22:19:44 +0300

    overflow..

--- sakovskaia.maiia/S1/main.cpp
+++ sakovskaia.maiia/S1/main.cpp
@@ -8,32 +8,28 @@ int main()
   using namespace sakovskaia;
   List< std::pair< std::string, ullList > > sequences;
   input(std::cin, sequences);
-  bool overflow_detected = false;
   if (sequences.empty())
   {
     std::cout << "0" << "\n";
     return 0;
   }
   List< ullList > processedData = processData(sequences);
-  List< unsigned long long int > sums = calculateSums(processedData);
+  List< unsigned long long int > sums;
   try
   {
-    outputSequences(sequences);
-    if (isEmpty(sequences))
-    {
-      std::cout << "0" << "\n";
-      return 0;
-    }
+     sums = calculateSums(processedData);
   }
-  catch (const std::out_of_range & e)
+  catch (const std::overflow_error & e)
   {
     std::cerr << e.what() << "\n";
-    overflow_detected = true;
+    return 1;
+  }
+  outputSequences(sequences);
+  if (isEmpty(sequences))
+  {
+    std::cout << "0" << "\n";
+    return 0;
   }
   outputProcessedData(processedData);
-  if (!overflow_detected)
-  {
-    outputSums(sums);
-  }
-  return overflow_detected;
+  outputSums(sums);
 }
