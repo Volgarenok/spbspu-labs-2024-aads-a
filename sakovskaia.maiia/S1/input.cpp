3c55c414dd10c0ebc04fe9e3511ec31a567e4ad5
MayaSakovskaya <zloyejik05@gmail.com>
2024-10-04 16:20:32 +0300

    fixed input..

--- sakovskaia.maiia/S1/input.cpp
+++ sakovskaia.maiia/S1/input.cpp
@@ -3,21 +3,23 @@
 void sakovskaia::input(std::istream & input, List< std::pair< std::string, ullList > > & list)
 {
   std::string arg = "";
-  while (input >> arg)
+  input >> arg;
+  while (input)
   {
     ullList numbers;
     std::string token = "";
-    while (input >> token)
+    while (input >> token && token.find_first_not_of("01234567890"))
     {
       try
       {
-        numbers.push_front(std::stoull(token));
+        numbers.push_back(std::stoull(token));
       }
       catch (const std::invalid_argument &)
       {
         break;
       }
     }
-    list.push_front({arg, std::move(numbers)});
+    list.push_back({arg, std::move(numbers)});
+    arg = token;
   }
 }
