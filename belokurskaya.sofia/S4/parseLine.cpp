9cbfd80b258029c347cd35d7edd13105dbf844b7
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-10-06 17:16:44 +0300

    one more try

--- belokurskaya.sofia/S4/parseLine.cpp
+++ belokurskaya.sofia/S4/parseLine.cpp
@@ -3,18 +3,46 @@
 void belokurskaya::parseLine(char* line, std::string& dict_name, Dictionary& dict)
 {
   char* next_token = nullptr;
-  char* token = strtok_r(line, " ", &next_token);
+  char* token = splitString(line, " ", &next_token);
   if (token)
   {
     dict_name = token;
-    while ((token = strtok_r(nullptr, " ", &next_token)))
+    while ((token = splitString(nullptr, " ", &next_token)))
     {
       int key = std::atoi(token);
-      if ((token = strtok_r(nullptr, " ", &next_token)))
+      if ((token = splitString(nullptr, " ", &next_token)))
       {
         std::string value = token;
         dict.push(key, value);
       }
     }
   }
+}
+
+char* belokurskaya::splitString(char* str, const char* delimiters, char** context)
+{
+  if (str == NULL)
+  {
+    str = *context;
+  }
+  if (str == NULL || *str == '\0')
+  {
+    return NULL;
+  }
+  while (*str != '\0' && strchr(delimiters, *str) != NULL)
+  {
+    str++;
+  }
+  char* token = str;
+  while (*str != '\0' && strchr(delimiters, *str) == NULL)
+  {
+    str++;
+  }
+  if (*str != '\0')
+  {
+    *str = '\0';
+    str++;
+  }
+  *context = str;
+  return token;
 }
--- belokurskaya.sofia/S4/parseLine.hpp
+++ belokurskaya.sofia/S4/parseLine.hpp
@@ -8,6 +8,7 @@
 namespace belokurskaya
 {
   void parseLine(char* line, std::string& dict_name, Dictionary& dict);
+  char* splitString(char* str, const char* delimiters, char** context);
 }
 
 #endif
