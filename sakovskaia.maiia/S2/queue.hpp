fc1e238e2f9cfa035ecfa121519cd11a9719d840
MayaSakovskaya <zloyejik05@gmail.com>
2024-10-06 22:57:56 +0300

    improved queue and stack

--- sakovskaia.maiia/S2/queue.hpp
+++ sakovskaia.maiia/S2/queue.hpp
@@ -9,10 +9,17 @@ namespace sakovskaia
   {
   public:
     void push_back(const T & value);
+    void push(T && value);
+    T pop();
     T & front() noexcept;
     const T & front() const noexcept;
     bool empty() const noexcept;
     void pop_front();
+    Queue() = default;
+    Queue(const Queue & other) = default;
+    Queue(Queue && other) noexcept = default;
+    Queue & operator=(const Queue & other) = default;
+    Queue & operator=(Queue && other) noexcept = default;
   private:
     List< T > data;
   };
@@ -21,6 +28,24 @@ namespace sakovskaia
   void Queue< T >::push_back(const T & value)
   {
     data.push_back(value);
+  }
+
+  template< typename T >
+  void Queue< T >::push(T && value)
+  {
+    data.push_back(std::move(value));
+  }
+
+  template< typename T >
+  T Queue< T >::pop()
+  {
+    if (data.empty())
+    {
+      throw std::runtime_error("Queue is empty");
+    }
+    T value = std::move(data.front());
+    data.pop_front();
+    return value;
   }
 
   template< typename T >
--- sakovskaia.maiia/S2/stack.hpp
+++ sakovskaia.maiia/S2/stack.hpp
@@ -9,9 +9,16 @@ namespace sakovskaia
   {
   public:
     void push_back(const T & value);
+    void push(T && value);
+    T pop();
     T & front() noexcept;
     const T & front() const noexcept;
     bool empty() const noexcept;
+    Stack() = default;
+    Stack(const Stack & other) = default;
+    Stack(Stack && other) noexcept = default;
+    Stack & operator=(const Stack & other) = default;
+    Stack & operator=(Stack && other) noexcept = default;
   private:
     List< T > data;
   };
@@ -20,6 +27,24 @@ namespace sakovskaia
   void Stack< T >::push_back(const T & value)
   {
     data.push_back(value);
+  }
+
+  template< typename T >
+  void Stack< T >::push(T && value)
+  {
+    data.push_back(std::move(value));
+  }
+
+  template< typename T >
+  T Stack< T >::pop()
+  {
+    if (data.empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    T value = std::move(data.front());
+    data.pop_front();
+    return value;
   }
 
   template< typename T >
