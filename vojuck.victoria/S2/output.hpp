ff849ba1d4152d732ddb98d00bba78285c46e916
vktrchh <viskas6969@gmail.com>
2024-10-07 23:58:43 +0300

    one more function

--- vojuck.victoria/S2/output.hpp
+++ vojuck.victoria/S2/output.hpp
@@ -6,6 +6,7 @@ namespace vojuck
 int precedence(const std::string& op);
 bool isOperator(const std::string& token);
 int evaluatePostfix(const vojuck::Queue<std::string>& postfixQueue);
+vojuck::Queue<std::string> infixToPostfix(const std::string& expression);
 }
 
 #endif
