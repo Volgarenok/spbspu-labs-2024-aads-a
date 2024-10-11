48c034e76f9feddaabd394cdbf1bb676f5f083d1
puzz13 <puzz13one@gmail.com>
2024-10-11 20:19:17 +0300

    Node interface markup

--- kovtun.alexander/S1/node.hpp
+++ kovtun.alexander/S1/node.hpp
@@ -1,23 +1,33 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
+#include <stddef.h>
+
 namespace kovtun
 {
   template<typename T>
   struct Node
   {
-    Node(Node *prev, Node *next, T val) :
-        prev(prev),
-        next(next),
-        val(val)
-    {};
-
+    Node(Node * prev, Node * next, T val);
+    Node(const T & val);
     ~Node() = default;
 
-    Node *prev;
-    Node *next;
+    Node * prev;
+    Node * next;
     T val;
   };
+
+  template< typename T >
+  Node< T >::Node(Node<T> *prev, Node<T> *next, T val) :
+      prev(prev),
+      next(next),
+      val(val)
+  {}
+
+  template< typename T >
+  Node< T >::Node(const T & val) :
+      Node(nullptr, nullptr, val)
+  {}
 }
 
 #endif
