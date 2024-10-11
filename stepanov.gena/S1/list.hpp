57ba410d8fc20f3b51001476646326443761566f
fncg <stepanov.gr@edu.spbstu.ru>
2024-10-12 02:31:08 +0300

    Реалиован конструктор по умолачанию для List. Добавлена реализация методов List (front empty push_front push_back pop_front clear swap)

--- stepanov.gena/S1/list.hpp
+++ stepanov.gena/S1/list.hpp
@@ -1,12 +1,13 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include <utility>
 #include <cstddef>
 #include "node.hpp"
 
 namespace stepanov
 {
-  template <typename T>
+  template < typename T >
   class List
   {
   public:
@@ -34,9 +35,111 @@ namespace stepanov
     void assign(size_t n, const T & value);
 
   private:
-    Node<T> * head;
-    Node<T> * tail;
+    Node<T> * head_;
+    Node<T> * tail_;
   };
+
+  template < typename T >
+  List< T >::List():
+    head_(nullptr),
+    tail_(nullptr)
+  {}
+
+  template <  typename T >
+  T & List< T >::front()
+  {
+    return head_->data_;
+  }
+
+  template < typename T >
+  const T & List< T >::front() const
+  {
+    return head_->data_;
+  }
+
+  template < typename T >
+  bool List< T >::empty() const noexcept
+  {
+    return head_ == nullptr;
+  }
+
+  template < typename T >
+  void List< T >::push_front(const T & value)
+  {
+    Node< T > * newnode = new Node< T >(value);
+    newnode->next_ = head_;
+    head_ = newnode;
+    if (tail_ == nullptr)
+    {
+      tail_ = newnode;
+    }
+  }
+
+  template < typename T >
+  void List< T >::push_front(T && value)
+  {
+    Node< T > * newnode = new Node< T >(std::move(value));
+    newnode->next_ = head_;
+    head_ = newnode;
+    if (tail_ == nullptr)
+    {
+      tail_ = newnode;
+    }
+  }
+
+  template < typename T >
+  void List< T >::push_back(const T & value)
+  {
+    Node< T > * newnode = new Node< T >(value);
+    if (empty())
+    {
+      head_ = newnode;
+      tail_ = newnode;
+      return;
+    }
+    tail_->next_ = newnode;
+    tail_ = newnode;
+  }
+
+  template < typename T >
+  void List< T >::pop_front()
+  {
+    if (head_ == nullptr)
+    {
+      return;
+    }
+    if (head_ == tail_)
+    {
+      delete head_;
+      head_ = nullptr;
+      tail_ = nullptr;
+      return;
+    }
+    Node< T > * newhead = head_->next_;
+    delete head_;
+    head_ = newhead;
+  }
+
+  template < typename T >
+  void List< T >::clear() noexcept
+  {
+    while (!empty())
+    {
+      pop_front();
+    }
+  }
+
+  template < typename T >
+  void List< T >::swap(List< T > & list)
+  {
+    Node< T > * subhead = head_;
+    Node< T > * subtail = tail_;
+    head_ = list.head_;
+    tail_ = list.tail_;
+    list.head_ = subhead;
+    list.tail_ = subtail;
+  }
+
 }
 
 
