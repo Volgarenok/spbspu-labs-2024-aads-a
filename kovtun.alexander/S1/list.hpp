228fe1f8bd6778bb77840b3251f8f22b1d63409a
puzz13 <puzz13one@gmail.com>
2024-10-11 19:40:01 +0300

    List interface markup

--- kovtun.alexander/S1/list.hpp
+++ kovtun.alexander/S1/list.hpp
@@ -9,18 +9,34 @@ namespace kovtun
   class List
   {
   public:
-    List() :
-        head_(nullptr),
-        tail_(nullptr),
-        size_(0)
-    {};
+    List();
     ~List() = default;
+
+    ConstIterator< T > cbegin() const;
+    ConstIterator< T > cend() const;
+
+    bool empty();
+
+    void push_front(const T &);
+    void push_back(const T &);
+    void pop_front();
+    void pop_back();
+    void clear();
+    void swap();
 
   private:
     Node< T > * head_;
     Node< T > * tail_;
+    
     size_t size_;
   };
+
+  template< typename T >
+  List< T >::List() :
+      head_(nullptr),
+      tail_(nullptr),
+      size_(0)
+  {}
 }
 
 #endif
