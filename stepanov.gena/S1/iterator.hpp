b809124131b2168ed4f0018e1d252983d1704002
fncg <stepanov.gr@edu.spbstu.ru>
2024-10-12 06:27:47 +0300

    Iterator: Конструктор по умолучанию,деструктор, конструктор с параметром

--- stepanov.gena/S1/iterator.hpp
+++ stepanov.gena/S1/iterator.hpp
@@ -1,10 +1,29 @@
 #ifndef ITERATOR_HPP
 #define ITERATOR_HPP
 
+#include <iterator>
+#include "node.hpp"
+
 namespace stepanov
 {
   template < typename T>
   class List;
+
+  template < typename T>
+  class Iterator
+  {
+    public:
+      Iterator():
+        node_(nullptr)
+      {}
+      Iterator(Node< T > * node):
+        node_(node)
+      {}
+      ~Iterator() = default;
+
+    private:
+      Node * node_;
+  }
 }
 
 
