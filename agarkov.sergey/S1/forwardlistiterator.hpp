b09b58a13e1ac733fbaf8172e76e01629c5d16ea
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-14 23:21:18 +0300

    ForwardListIterator(details::List< T >* rhs)

--- agarkov.sergey/S1/forwardlistiterator.hpp
+++ agarkov.sergey/S1/forwardlistiterator.hpp
@@ -10,9 +10,25 @@ namespace agarkov
   {
   public:
     ForwardListIterator();
+    ~ForwardListIterator() = default;
+    explicit ForwardListIterator(details::List< T >* rhs);
   private:
     details::List< T >* ptr_;
   };
+
+  template< typename T >
+  ForwardListIterator< T >::ForwardListIterator():
+    ptr_(nullptr)
+  {
+  }
+  
+  template< typename T >
+  ForwardListIterator< T >::ForwardListIterator(details::List< T >* rhs):
+    ptr_(rhs)
+  {
+  }
+
+
 
 }
 #endif
