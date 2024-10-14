1240123d6723a799e66a47c8e31cf16c28a58294
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-14 23:17:16 +0300

    iterator field and construct

--- agarkov.sergey/S1/forwardlistiterator.hpp
+++ agarkov.sergey/S1/forwardlistiterator.hpp
@@ -1,1 +1,18 @@
- 
+#ifndef FORWARDLISTITERATOR
+#define FORWARDLISTITERATOR
+
+#include <list.hpp>
+
+namespace agarkov
+{
+  template< typename T >
+  class ForwardListIterator
+  {
+  public:
+    ForwardListIterator();
+  private:
+    details::List< T >* ptr_;
+  };
+
+}
+#endif
