fa223830b2afaed5f99db3db363e1478a638bc95
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-15 00:12:24 +0300

    const iter constructor

--- agarkov.sergey/S1/forwardlistconstiterator.hpp
+++ agarkov.sergey/S1/forwardlistconstiterator.hpp
@@ -1,1 +1,35 @@
- 
+#ifndef FORWARDLISTCONSTITERATOR_HPP
+#define FORWARDLISTCONSTITERATOR_HPP
+
+#include <list.hpp>
+
+namespace agarkov
+{
+  class ForwardListConstIterator
+  {
+  friend class ForwardList< T >;
+  public:
+    ForwardListConstIterator();
+    ~ForwardListConstIterator() = default;
+    explicit ForwardListConstIterator(const details::List< T >* rhs);
+  private:
+    const details::List< T >* ptr_;
+  };
+
+  template< typename T >
+  ForwardListConstIterator< T >::ForwardListConstIterator():
+    ptr_(nullptr)
+  {
+  }
+
+  template< typename T >
+  ForwardListConstIterator< T >::ForwardListConstIterator(const details::List< T >* rhs):
+    ptr_(rhs)
+  {
+  }
+
+
+}
+
+
+#endif
