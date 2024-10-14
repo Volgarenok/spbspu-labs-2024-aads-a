5d0d328b0c68c8eb54fff2a8fc67a242a85bfbba
fncg <stepanov.gr@edu.spbstu.ru>
2024-10-14 12:04:53 +0300

    Создание Queue && Stack

---
+++ stepanov.gena/S2/Queue.h
@@ -0,0 +1,16 @@
+#ifndef QUEUE_H
+#define QUEUE_h
+
+namespace stepanov
+{
+  template < typename T >
+  class Queue
+  {
+  public:
+    Queue() = default;
+    ~Queue() = default;
+  private:
+  };
+}
+
+#endif
---
+++ stepanov.gena/S2/Stack.h
@@ -0,0 +1,16 @@
+#ifndef STACK_H
+#define STACK_H
+
+namespace stepanov
+{
+  template < typename T >
+  class Stack
+  {
+  public:
+    Stack() = default;
+    ~Stack() = default;
+  private:
+  };
+}
+
+#endif
