19d43db9a739eccdc47335f70e011992718f626e
fncg <stepanov.gr@edu.spbstu.ru>
2024-10-16 01:10:37 +0300

    Начало реализации input(console || file)

--- stepanov.gena/S2/main.cpp
+++ stepanov.gena/S2/main.cpp
@@ -1,4 +1,23 @@
-int main()
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main(int argc, char* argv[])
 {
+  std::string strInput = "";
+
+  if (argc == 1)
+  {}
+  else if (argc == 2)
+  {
+    std::ifstream fileName;
+    fileName.open(argv[1]);
+    if (!fileName.is_open())
+    {
+      std::cerr << "No such file in directory" << "\n";
+      return 1;
+    }
+  }
   return 0;
 }
