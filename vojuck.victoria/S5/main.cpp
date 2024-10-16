123d1b409d820dae51ae0403bef5af550ff67277
vktrchh <viskas6969@gmail.com>
2024-10-16 12:25:00 +0300

    some translation unit

--- vojuck.victoria/S5/main.cpp
+++ vojuck.victoria/S5/main.cpp
@@ -8,35 +8,8 @@
 #include <functional>
 #include <cstdlib>
 #include "tree.hpp"
-
+#include "traversalresult.hpp"
 using namespace vojuck;
-
-
-struct TraversalResult
-{
-  long long sum;
-  std::string values;
-
-  TraversalResult() : sum(0), values("") {}
-
-  void operator()(const std::pair<const int, std::string>& key_value)
-  {
-      // Проверка на переполнение при добавлении
-    if ((key_value.first > 0 && sum > std::numeric_limits<long long>::max() - key_value.first) ||
-        (key_value.first < 0 && sum < std::numeric_limits<long long>::min() - key_value.first))
-    {
-      throw std::overflow_error("overflow sum.");
-    }
-    sum += key_value.first;
-
-    if (!values.empty())
-    {
-      values += " ";
-    }
-      values += key_value.second;
-  }
-};
-
 int main(int argc, char* argv[])
 {
   if (argc != 3)
