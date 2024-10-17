09121cb43961a3543ade9965e8b39a419b85c535
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-18 01:41:32 +0300

    start calculate postfix

---
+++ agarkov.sergey/S2/calcucalepostfix.hpp
@@ -0,0 +1,12 @@
+#ifndef CALCULATEPOSTFIX_HPP
+#define CALCULATEPOSTFIX_HPP
+#include <string>
+#include "queue.hpp"
+
+namespace agarkov
+{
+  long long calculatePostfix(Queue< std::string >& postfixQueue);
+}
+
+#endif
+ 
