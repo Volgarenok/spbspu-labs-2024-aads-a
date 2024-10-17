1e66cb3a79e5742c3f6652ed9090472d242a9b6c
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-18 00:38:02 +0300

    try create covert infix to postfix

--- agarkov.sergey/S2/main.cpp
+++ agarkov.sergey/S2/main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <string>
+#include "convertstringtoinfix.hpp"
  
+int main()
+{
+  std::string line;
+  std::getline(std::cin, line);
+  agarkov::Queue< std::string > infix = agarkov::convertStringToInfix(line);
+}
