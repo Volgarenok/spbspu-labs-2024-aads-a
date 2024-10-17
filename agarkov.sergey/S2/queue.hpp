65936efd1919df61854d38c7b97334d7a70392b2
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-17 14:38:22 +0300

    queue()

--- agarkov.sergey/S2/queue.hpp
+++ agarkov.sergey/S2/queue.hpp
@@ -6,8 +6,18 @@ namespace agarkov
   template< typename T >
   class Queue
   {
-
+    public:
+      Queue();
+    private:
+      details::List< T >* begin_;
+      details::List< T >* end_;
   };
+
+  template< typename T >
+  Queue< T >::Queue():
+    begin_(nullptr),
+    end_(nullptr)
+  {}
 }
 
 #endif
