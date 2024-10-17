45d0708f8b5696f4b0c5d937bf89f520d68e62a7
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-17 15:13:42 +0300

    stack()

--- agarkov.sergey/S2/stack.hpp
+++ agarkov.sergey/S2/stack.hpp
@@ -4,4 +4,22 @@
 #include <stdexcept>
 #include <list.hpp>
 
+namespace agarkov
+{
+  template< typename T >
+  class Stack
+  {
+    public:
+      Stack();
+    private:
+      details::List< T >* top_;
+  };
+
+  template< typename T >
+  Stack< T >::Stack():
+    top_(nullptr)
+  {}
+
+}
+
 #endif
