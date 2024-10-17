d388297e0fde2cf69b010e018d43b85bbf36f94d
deadshumz <shumzz@bk.ru>
2024-10-17 13:06:57 +0300

    remove trailing spaces

--- petuhov.kirill/S2/applyOperation.cpp
+++ petuhov.kirill/S2/applyOperation.cpp
@@ -4,8 +4,8 @@
 
 int petuhov::applyOperation(long long a, long long b, char op) {
   switch (op) {
-    case '+': 
-      if (a > 0 && b > 0 && a > std::numeric_limits<long long>::max() - b) 
+    case '+':
+      if (a > 0 && b > 0 && a > std::numeric_limits<long long>::max() - b)
         throw std::overflow_error("Overflow in addition");
       return a + b;
     case '-':
