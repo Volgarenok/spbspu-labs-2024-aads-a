40ff0badd55c2b5df08311ea9331e72c72f51e9a
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-18 01:20:09 +0300

    start arihmet oper

---
+++ agarkov.sergey/S2/arithmetic.hpp
@@ -0,0 +1,13 @@
+#ifndef ARITHMETIC_HPP
+#define ARITHMETIC_HPP
+
+namespace agarkov
+{
+  long long sum(long long a, long long b);
+  long long subtract(long long a, long long b);
+  long long multiply(long long a, long long b);
+  long long division(long long a, long long b);
+  long long remaind(long long a, long long b);
+}
+
+#endif
