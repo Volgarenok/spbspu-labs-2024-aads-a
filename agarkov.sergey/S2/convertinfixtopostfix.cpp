fdb5207925f1bcf5532454955149320764cde42a
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-18 01:17:46 +0300

    finish and test infix to postfix

--- agarkov.sergey/S2/convertinfixtopostfix.cpp
+++ agarkov.sergey/S2/convertinfixtopostfix.cpp
@@ -41,6 +41,60 @@ agarkov::Queue< std::string > agarkov::convertInfixToPostfix(agarkov::Queue< std
 {
   agarkov::Queue< std::string > postfixQueue;
   agarkov::Stack< std::string > stack;
+  while (!infixQueue.isEmpty())
+  {
+    std::string token = infixQueue.get();
+    infixQueue.pop();
+
+    if (std::isdigit(token[0]))
+    {
+      postfixQueue.push(token);
+    }
+    else if (isOperator(token))
+    {
+      if (token == ")")
+      {
+        while (!stack.isEmpty() && (stack.get() != "("))
+        {
+          postfixQueue.push(stack.get());
+          stack.pop();
+        }
+        if (stack.isEmpty())
+        {
+          throw std::logic_error("Mismatched parentheses");
+        }
+        stack.pop();
+      }
+      else if (token == "(")
+      {
+        stack.push(token);
+      }
+      else
+      {
+        while (!stack.isEmpty() && (isHigherPriority(stack.get(),token)))
+        {
+          postfixQueue.push(stack.get());
+          stack.pop();
+        }
+        stack.push(token);
+      }
+    }
+    else
+    {
+      throw std::logic_error("Invalid token");
+    }
+  }
+
+  while (!stack.isEmpty())
+  {
+    std::string op = stack.get();
+    stack.pop();
+    if (op == "(")
+    {
+      throw std::logic_error("Mismatched parentheses");
+    }
+    postfixQueue.push(op);
+  }
 
   return postfixQueue;
 }
--- agarkov.sergey/S2/main.cpp
+++ agarkov.sergey/S2/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include "convertstringtoinfix.hpp"
+#include "convertinfixtopostfix.hpp"
  
 int main()
 {
   std::string line;
   std::getline(std::cin, line);
   agarkov::Queue< std::string > infix = agarkov::convertStringToInfix(line);
+  convertInfixToPostfix(infix);
 }
